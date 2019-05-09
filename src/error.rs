c61a17e1c54f38db09f5b8e3d1d9f629275a33ff
Saghm Rossi <saghmrossi@gmail.com>
2019-05-09 13:29:20 -0400

    error api

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,37 @@
-pub struct Error {}
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+    }
 
-pub type Result<T> = std::result::Result<T, Error>;
+    errors {
+        /// A malformed or invalid argument was passed to the driver.
+        ArgumentError(msg: String) {
+            description("An invalid argument was provided to a database operation")
+            display("An invalid arugment was provided to a database operation: {}", msg)
+        }
+
+        /// The driver was unable to send or receive a message to the server.
+        OperationError(msg: String) {
+            description("A database operation failed to send or receive a reply")
+            display("A database operation failed to send or receive a reply: {}", msg)
+        }
+
+       /// The response the driver received from the server was not in the form expected.
+        ResponseError(msg: String) {
+            description("A database operation returned an invalid reply")
+            display("A database operation returned an invalid reply: {}", msg)
+        }
+
+        /// The server encountered an error when executing the operation.
+        ServerError(operation: String, msg: String) {
+            description("The server returned an error for an operation")
+            display("{} operation failed: {}", operation, msg)
+        }
+
+        /// An error occurred during server selection.
+        ServerSelectionError(msg: String) {
+            description("An error occurred during server selection")
+            display("An error occured during server selection: {}", msg)
+        }
+    }
+}
