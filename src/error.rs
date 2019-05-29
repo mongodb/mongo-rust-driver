2c3adfd423edf3a22661afe37b5cc9dcaa480962
Saghm Rossi <saghmrossi@gmail.com>
2019-05-29 14:48:06 -0400

    split server error type

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,7 @@
+use std::fmt;
+
+use bson::Document;
+
 error_chain! {
     foreign_links {
         Io(std::io::Error);
@@ -8,6 +12,12 @@ error_chain! {
         ArgumentError(msg: String) {
             description("An invalid argument was provided to a database operation")
             display("An invalid arugment was provided to a database operation: {}", msg)
+        }
+
+        /// The server encountered an error when executing the operation.
+        CommandError(code: u32, code_name: String, msg: String, labels: Vec<String>) {
+            description("An error occurred when executing a command")
+            display("Command failed ({}): {}", code_name, msg)
         }
 
         /// The driver was unable to send or receive a message to the server.
@@ -22,16 +32,55 @@ error_chain! {
             display("A database operation returned an invalid reply: {}", msg)
         }
 
-        /// The server encountered an error when executing the operation.
-        ServerError(operation: String, msg: String) {
-            description("The server returned an error for an operation")
-            display("{} operation failed: {}", operation, msg)
-        }
-
         /// An error occurred during server selection.
         ServerSelectionError(msg: String) {
             description("An error occurred during server selection")
             display("An error occured during server selection: {}", msg)
         }
+
+        /// An error occurred when trying to execute a write operation.
+        WriteError(inner: WriteFailure) {
+            description("An error occurred when trying to execute a write operation:")
+            display("{}", inner)
+        }
+    }
+}
+
+/// An error that occurred due to not being able to satisfy a write concern.
+#[derive(Debug)]
+pub struct WriteConcernError {
+    /// Identifies the type of write concern error.
+    pub code: i32,
+    /// The name associated with the error code.
+    pub code_name: String,
+    /// A description of the error that occurred.
+    pub message: String,
+}
+
+/// An error that occurred duringn a write operation that wasn't due to being unable to satisfy a
+/// write concern.
+#[derive(Debug)]
+pub struct WriteError {
+    /// Identifies the type of write concern error.
+    pub code: i32,
+    /// The name associated with the error code.
+    ///
+    /// Note that the server will not return this in some cases, hence `code_name` being an
+    /// `Option`.
+    pub code_name: Option<String>,
+    /// A description of the error that occurred.
+    pub message: String,
+}
+
+/// An error that occurred when trying to execute a write operation.
+#[derive(Debug)]
+pub enum WriteFailure {
+    WriteConcernError(WriteConcernError),
+    WriteError(WriteError),
+}
+
+impl fmt::Display for WriteFailure {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        unimplemented!()
     }
 }
