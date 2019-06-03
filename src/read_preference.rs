50c905d4d837bc9fa5455b2db98663202ca755aa
Saghm Rossi <saghmrossi@gmail.com>
2019-06-03 12:20:59 -0400

    read preference api (#6)

--- src/read_preference.rs
+++ src/read_preference.rs
@@ -1,2 +1,40 @@
-#[derive(Debug)]
-pub struct ReadPreference {}
+use std::{collections::HashMap, time::Duration};
+
+/// Specifies how the driver should route a read operation to members of a replica set.
+///
+/// If applicable, `tag_sets` can be used to target specific nodes in a replica set, and
+/// `max_staleness` specifies the maximum lag behind the primary that a secondary can be to remain
+/// eligible for the operation. The max staleness value maps to the `maxStalenessSeconds` MongoDB
+/// option and will be sent to the server as an integer number of seconds.
+///
+/// See the [MongoDB docs](https://docs.mongodb.com/manual/core/read-preference) for more details.
+#[derive(Clone, Debug, PartialEq)]
+pub enum ReadPreference {
+    /// Only route this operation to the primary.
+    Primary,
+    /// Only route this operation to a secondary.
+    Secondary {
+        tag_sets: Option<Vec<TagSet>>,
+        max_staleness: Option<Duration>,
+    },
+    /// Route this operation to the primary if it's available, but fall back to the secondaries if
+    /// not.
+    PrimaryPreferred {
+        tag_sets: Option<Vec<TagSet>>,
+        max_staleness: Option<Duration>,
+    },
+    /// Route this operation to a secondary if one is available, but fall back to the primary if
+    /// not.
+    SecondaryPreferred {
+        tag_sets: Option<Vec<TagSet>>,
+        max_staleness: Option<Duration>,
+    },
+    /// Route this operation to the node with the least network latency regardless of whether it's
+    /// the primary or a secondary.
+    Nearest {
+        tag_sets: Option<Vec<TagSet>>,
+        max_staleness: Option<Duration>,
+    },
+}
+
+pub type TagSet = HashMap<String, String>;
