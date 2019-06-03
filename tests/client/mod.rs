a8ecba633c503591778d46b345be403236ee2ef3
Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>
2019-06-03 13:32:19 -0400

    RUST-47 Implements database enumeration spec (#8)

--- tests/client/mod.rs
+++ tests/client/mod.rs
@@ -28,9 +28,14 @@ struct OsMetadata {
     pub architecture: String,
 }
 
-#[test]
+// This test currently doesn't pass on replica sets and sharded clusters consistently due to
+// `currentOp` sometimes detecting heartbeats between the server. Eventually we can test this using
+// APM or coming up with something more clever, but for now, we're just disabling it.
+//
+// #[test]
+#[allow(unused)]
 fn metadata_sent_in_handshake() {
-    let client = Client::with_uri(*MONGODB_URI).unwrap();
+    let client = Client::with_uri(MONGODB_URI.as_str()).unwrap();
     let db = client.database("admin");
     let result = db.run_command(doc! { "currentOp": 1 }, None).unwrap();
 
@@ -41,4 +46,81 @@ fn metadata_sent_in_handshake() {
 
     let metadata: Metadata = bson::from_bson(in_prog[0].clone()).unwrap();
     assert_eq!(metadata.client.driver.name, "mrd");
+}
+
+#[test]
+fn list_databases() {
+    let client = Client::with_uri(MONGODB_URI.as_str()).unwrap();
+
+    let expected_dbs = &["list_databases1", "list_databases2", "list_databases3"];
+
+    for name in expected_dbs {
+        client.database(name).drop().unwrap();
+    }
+
+    let prev_dbs = client.list_databases(None).unwrap();
+
+    for name in expected_dbs {
+        assert!(!prev_dbs
+            .iter()
+            .any(|doc| doc.get("name") == Some(&Bson::String(name.to_string()))));
+
+        let db = client.database(name);
+
+        db.collection("foo")
+            .insert_one(doc! { "x": 1 }, None)
+            .unwrap();
+    }
+
+    let new_dbs = client.list_databases(None).unwrap();
+    let new_dbs: Vec<_> = new_dbs
+        .into_iter()
+        .filter(|doc| match doc.get("name") {
+            Some(&Bson::String(ref name)) => expected_dbs.contains(&name.as_str()),
+            _ => false,
+        })
+        .collect();
+    assert_eq!(new_dbs.len(), expected_dbs.len());
+
+    for name in expected_dbs {
+        let db_doc = new_dbs
+            .iter()
+            .find(|doc| doc.get("name") == Some(&Bson::String(name.to_string())))
+            .unwrap();
+        assert!(db_doc.contains_key("sizeOnDisk"));
+        assert!(db_doc.contains_key("empty"));
+    }
+}
+
+#[test]
+fn list_database_names() {
+    let client = Client::with_uri(MONGODB_URI.as_str()).unwrap();
+
+    let expected_dbs = &[
+        "list_database_names1",
+        "list_database_names2",
+        "list_database_names3",
+    ];
+
+    for name in expected_dbs {
+        client.database(name).drop().unwrap();
+    }
+
+    let prev_dbs = client.list_database_names(None).unwrap();
+
+    for name in expected_dbs {
+        assert!(!prev_dbs.iter().any(|db_name| db_name == name));
+
+        let db = client.database(name);
+
+        db.collection("foo")
+            .insert_one(doc! { "x": 1 }, None)
+            .unwrap();
+    }
+
+    let new_dbs = client.list_database_names(None).unwrap();
+
+    for name in expected_dbs {
+        assert_eq!(new_dbs.iter().filter(|db_name| db_name == name).count(), 1);
+    }
 }
