a56eef86ad683753ca296bd1c3073b9c25a243e9
Saghm Rossi <saghmrossi@gmail.com>
2019-06-11 16:36:51 -0400

    add failure docs

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -13,6 +13,11 @@ impl HandlerId {
     }
 }
 
+/// In order to easily send events to multiple handlers, we need to be able to copy the events.
+/// Certain events (e.g. `CommandFailedEvent`) need to contain information about an error that a
+/// driver returned, but due to some external errors not implementing Clone (e.g. `std::io::Error`),
+/// we define `Failure` as a type that contains all the same information as our error type but that
+/// can be properly cloned and sent to each handler.
 #[derive(Clone, Debug)]
 pub enum Failure {
     Argument(String),
@@ -22,4 +27,9 @@ pub enum Failure {
     ResponseError(String),
     ServerSelectionError(String),
     WriteError(WriteFailure),
+
+    // Error-chain uses a `__Nonexhaustive` variant that's hidden from the docs to force users to
+    // match the wildcard pattern when checking the error kind, so we do the same thing here.
+    #[doc(hidden)]
+    __Nonexhaustive,
 }
