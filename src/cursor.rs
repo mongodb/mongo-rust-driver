ea610c8b2b79de98b8eb624d208c90118b3f89af
Saghm Rossi <saghmrossi@gmail.com>
2019-07-02 16:14:23 -0400

    Tailable cursor support (#24)

--- src/cursor.rs
+++ src/cursor.rs
@@ -60,6 +60,63 @@ use crate::{
 /// # Ok(())
 /// # }
 /// ```
+///
+/// One subtlety of `Cursor` implementing `Iterator` is that many of the common ways to use a
+/// `Cursor`, including using a `for` loop and methods such as `map` and `fold` take ownership of
+/// `self`, meaning that the `Cursor` will be unusable afterwards. Normally, this isn't an issue,
+/// since by default, the server will close a cursor once all the results have been sent to the
+/// client. However, a [tailable cursor](https://docs.mongodb.com/manual/core/tailable-cursors/)
+/// will remain open even after the results have been exhausted and may return additional results
+/// later. In order to facilitate using the full `Iterator` API with tailable cursors, `Cursor`
+/// implements a method called `tail`. `Cursor::tail` returns a `Tail` struct that also implements
+/// `Iterator`, which can then be freely used in ways that require ownership of `self` without
+/// rendering the original `Cursor` unusable:
+///
+/// ```rust
+/// # use mongodb::{Client, error::Result, options::{CursorType, FindOptions}};
+/// #
+/// # fn do_stuff() -> Result<()> {
+/// # let client = Client::with_uri_str("mongodb://example.com")?;
+/// # let coll = client.database("foo").collection("bar");
+/// let options = FindOptions::builder()
+///     .cursor_type(CursorType::Tailable)
+///     .build();
+///
+/// let mut cursor = coll.find(None, Some(options))?;
+///
+/// loop {
+///     for doc in cursor.tail() {
+///         println!("{}", doc?);
+///     }
+///
+///     // Sleep for a second to wait for new results.
+///     std::thread::sleep(std::time::Duration::from_secs(1));
+/// }
+/// # Ok(())
+/// # }
+/// ```
+///
+/// Note that until the `Tail` is dropped, the original `Cursor` cannot be iterated, and an
+/// additional `Tail` cannot be created.
+///
+/// ```compile_fail
+/// # use mongodb::{Client, error::Result, options::{CursorType, FindOptions}};
+/// #
+/// # fn do_stuff() -> Result<()> {
+/// # let client = Client::with_uri_str("mongodb://example.com")?;
+/// # let coll = client.database("foo").collection("bar");
+/// let options = FindOptions::builder()
+///     .cursor_type(CursorType::Tailable)
+///     .build();
+///
+/// let mut cursor = coll.find(None, Some(options))?;
+/// let mut tail = cursor.tail();
+///
+/// // This won't compile, since `tail` can't be dropped until after the call to `Tail::next`.
+/// cursor.next();
+/// tail.next();
+/// # Ok(())
+/// ```
 #[allow(dead_code)]
 pub struct Cursor {
     coll: Collection,
@@ -67,6 +124,16 @@ pub struct Cursor {
     cursor_id: i64,
     batch_size: Option<i32>,
     buffer: VecDeque<Document>,
+}
+
+/// A `Tail` is a temporary `Iterator` created from `Cursor::tail` to facilitate using methods that
+/// require ownership of `self` (such as `map` and `fold`) while still being able to use the
+/// `Cursor` afterwards.
+///
+/// The only way to create a `Tail` is with `Cursor::tail`. See the `Cursor` type documentation for
+/// more details on how to use a `Tail`.
+pub struct Tail<'a> {
+    cursor: &'a mut Cursor,
 }
 
 impl Cursor {
@@ -108,19 +175,42 @@ impl Cursor {
 
         Ok(())
     }
+
+    /// Create a new `Tail` to facilitate using functionality that requires ownership of `self`
+    /// (such as `Iterator::map` and `Iterator::fold`) and still use the `Cursor` afterwards.
+    pub fn tail(&mut self) -> Tail {
+        Tail { cursor: self }
+    }
 }
 
 impl Iterator for Cursor {
     type Item = Result<Document>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        // Return the next document from the current batch if one is available.
+        //
+        // Otherwise, if the cursor ID is 0, then the server has closed the cursor, so there are no
+        // more results.
         match self.buffer.pop_front() {
-            Some(doc) => Some(Ok(doc)),
-            None if self.cursor_id == 0 => None,
-            None => match self.next_batch() {
-                Ok(()) => self.next(),
-                Err(e) => Some(Err(e)),
-            },
+            Some(doc) => return Some(Ok(doc)),
+            None if self.cursor_id == 0 => return None,
+            None => {}
+        };
+
+        // Fetch the next batch, returning an error if one occurs.
+        if let Err(err) = self.next_batch() {
+            return Some(Err(err));
         }
+
+        // Return the first document from the next batch if it's available.
+        self.buffer.pop_front().map(Ok)
+    }
+}
+
+impl<'a> Iterator for Tail<'a> {
+    type Item = Result<Document>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.cursor.next()
     }
 }
