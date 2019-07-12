f08106e686d44903cd781693ff748a559aac8ffb
Saghm Rossi <saghmrossi@gmail.com>
2019-07-12 15:38:57 -0400

    minor: enable lint CI on benchmarks

--- .evergreen/check-clippy.sh
+++ .evergreen/check-clippy.sh
@@ -4,3 +4,6 @@ set -o errexit
 
 . ~/.cargo/env
 cargo clippy --all-targets --all-features -p mongodb -- -D warnings
+
+cd benchmarks
+cargo clippy --all-targets --all-features -p mongodb -- -D warnings
