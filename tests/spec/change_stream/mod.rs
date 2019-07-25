a6837d5ccd8881c0d5a935dc3523fb0d50bed86f
Benji Rewis <ben.rewis98@gmail.com>
2019-07-25 17:32:19 -0400

    Continue implementing spec test harness

--- tests/spec/change_stream/mod.rs
+++ tests/spec/change_stream/mod.rs
@@ -1,60 +1,160 @@
-use bson::Document;
-use mongodb::{options::collation::Collation, Client};
+use bson::{Bson, Document};
+use mongodb::{
+    change_stream::document::ChangeStreamDocument, error::Result, options::ChangeStreamOptions,
+    Client,
+};
 
 #[derive(Debug, Deserialize)]
-pub struct TestFile {
-    pub data: Vec<Document>,
-    pub tests: Vec<TestCase>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct TestCase {
-    pub description: String,
-    pub operation: Operation,
-    pub outcome: Document,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Operation {
-    name: String,
-    arguments: Document,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Outcome<R> {
-    pub result: R,
-    pub collection: Option<CollectionOutcome>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct CollectionOutcome {
-    pub name: Option<String>,
-    pub data: Vec<Document>,
+struct TestFile {
+    collection_name: String,
+    database_name: String,
+    collection2_name: String,
+    database2_name: String,
+    tests: Vec<TestCase>,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Arguments {
-    pub pipeline: Vec<Document>,
-    pub batch_size: Option<i32>,
-    pub collation: Option<Collation>,
+struct TestCase {
+    description: String,
+    min_server_version: String,
+    max_server_version: Option<String>,
+    fail_point: Option<Document>,
+    target: Target,
+    topology: Topology,
+    change_stream_pipeline: Vec<Document>,
+    change_stream_options: ChangeStreamOptions,
+    operations: Vec<Document>,
+    expectations: Vec<Document>,
+    result: Outcome,
 }
 
-fn matches(expected: Document, actual: Document) -> Result<(), &'static str> {
-    unimplemented!();
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+enum Target {
+    Collection,
+    Database,
+    Client,
 }
 
-#[function_name]
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+enum Topology {
+    Single,
+    ReplicaSet,
+    Sharded,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+enum Outcome {
+    Error(i32),
+    Success(Vec<ChangeStreamDocument>),
+}
+
+fn matches(expected: impl Into<Bson>, actual: impl Into<Bson>) -> bool {
+    let mut expected_bson = expected.into();
+    let actual_bson = actual.into();
+
+    if expected_bson == bson::to_bson("42").unwrap() {
+        return true;
+    }
+
+    expected_bson == actual_bson
+}
+
+fn run_test(
+    mut test_case: TestCase,
+    test_file: TestFile,
+    global_client: Client,
+) -> Result<Vec<Document>> {
+    test_case.description = test_case.description.replace('$', "%");
+
+    // TODO: Change to check actual topology
+    if test_case.topology != Topology::Single {
+        return Ok(Vec::new());
+    }
+
+    global_client.database(&test_file.database_name).drop()?;
+    global_client.database(&test_file.database2_name).drop()?;
+
+    let db = global_client.database(&test_file.database_name);
+    let db2 = global_client.database(&test_file.database2_name);
+    db.create_collection(&test_file.collection_name, None)?;
+    db2.create_collection(&test_file.collection2_name, None)?;
+    let coll = db.collection(&test_file.collection_name);
+    let coll2 = db.collection(&test_file.collection2_name);
+
+    if let Some(fail_point) = test_case.fail_point {
+        let admin_db = global_client.database("admin");
+        admin_db.run_command(fail_point, None)?;
+    }
+
+    let client = Client::with_uri_str("mongodb://localhost:27017")?;
+
+    // TODO: Begin monitoring all APM events for client
+
+    // TODO: Change to correct collection and database targets
+    let change_stream = match test_case.target {
+        Collection => coll.watch(
+            test_case.change_stream_pipeline,
+            Some(test_case.change_stream_options),
+        )?,
+        Database => db.watch(
+            test_case.change_stream_pipeline,
+            Some(test_case.change_stream_options),
+        )?,
+        Client => client.watch(
+            test_case.change_stream_pipeline,
+            Some(test_case.change_stream_options),
+        )?,
+    };
+
+    for operation in test_case.operations {
+        global_client
+            .database("admin")
+            .run_command(operation, None)?;
+    }
+
+    let changes = Vec::new();
+    match test_case.result {
+        Outcome::Error(_) => {
+            change_stream.next().transpose()?;
+        }
+        Outcome::Success(_) => {
+            for change in change_stream {
+                changes.push(change?);
+            }
+        }
+    }
+
+    Ok(changes)
+}
+
 fn run_change_stream_test(test_file: TestFile) {
-    let data = test_file.data;
-
-    let global_client = Client::with_uri_str("mongodb://localhost:27017");
+    let global_client = Client::with_uri_str("mongodb://localhost:27017").unwrap();
 
     for mut test_case in test_file.tests {
-        test_case.description = test_case.description.replace('$', "%");
+        match run_test(test_case, test_file, global_client) {
+            Err(e) => match test_case.result {
+                Outcome::Error(code) => assert!(matches(code, e.code)),
+                Outcome::Success(_) => panic!(&test_case.description),
+            },
+            Ok(changes) => match test_case.result {
+                Outcome::Error(_) => panic!(&test_case.description),
+                Outcome::Success(docs) => {
+                    for pair in docs.iter().zip(changes.iter()) {
+                        assert!(matches(bson::to_bson(pair.0).unwrap(), pair.1));
+                    }
+                }
+            },
+        }
 
-        match data.get("topology".to_string()) {}
+        // TODO: Assert expectations
     }
+
+    global_client.database(&test_file.database_name).drop();
+    global_client.database(&test_file.database2_name).drop();
 }
 
 #[test]
