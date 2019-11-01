06c06513c7875fdeb99d47d03653d28fc92bd5d0
Patrick Freed <patrick.freed@mongodb.com>
2019-11-01 17:31:10 -0400

    move execute_operation to executor submodule as per the design

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,4 +1,5 @@
 pub mod auth;
+mod executor;
 pub mod options;
 
 use std::sync::{Arc, RwLock};
@@ -153,33 +154,5 @@ impl Client {
         self.inner.topology.read().unwrap().select_server(
             criteria.unwrap_or_else(|| &SelectionCriteria::ReadPreference(ReadPreference::Primary)),
         )
-    }
-
-    /// Execute the given operation, optionally specifying a connection used to do so.
-    /// If no connection is provided, server selection will performed using the criteria specified
-    /// on the operation, if any.
-    #[allow(dead_code)]
-    pub(crate) fn execute_operation<T: Operation>(
-        &self,
-        op: &T,
-        connection: Option<&mut Connection>,
-    ) -> Result<T::O> {
-        // if no connection provided, select one.
-        let mut selected_connection = match connection {
-            Some(_) => None,
-            None => {
-                let server = self.select_server(op.selection_criteria())?;
-                Some(server.checkout_connection()?)
-            }
-        };
-
-        // get a reference to the selected connection if it exists, otherwise get it from the
-        // provided connection. exactly one of these will be non-null, so the final unwrap
-        // will always succeed.
-        let connection_ref = selected_connection.as_mut().unwrap_or(connection.unwrap());
-
-        let cmd = op.build(connection_ref.stream_description()?)?;
-        let response = connection_ref.send_command(cmd)?;
-        op.handle_response(response)
     }
 }
