d7aec1b0114f0572962076e04415cbe45d48ffb9
Sophie Saskin <sophie.saskin@gmail.com>
2019-11-04 15:02:55 -0500

    factor out to helper function

--- tests/read_concern/mod.rs
+++ tests/read_concern/mod.rs
@@ -2,38 +2,49 @@ use bson::{bson, doc, Bson};
 use function_name::named;
 use mongodb::{
     concern::ReadConcern,
+    event::command::CommandStartedEvent,
     options::{
         AggregateOptions, CountOptions, DistinctOptions, EstimatedDocumentCountOptions, FindOptions,
     },
+    Collection,
 };
 
 use crate::util::EventClient;
 
-#[test]
-#[named]
-fn test_count_with_read_concern() {
+fn helper(
+    name: &str,
+    command_name: &str,
+    function: impl FnOnce(Collection),
+) -> Vec<CommandStartedEvent> {
     let client = EventClient::new();
-    let collection = client
-        .database(function_name!())
-        .collection(function_name!());
-    collection
-        .count_documents(
-            None,
-            Some(
-                CountOptions::builder()
-                    .read_concern(ReadConcern::Local)
-                    .build(),
-            ),
-        )
-        .unwrap();
+    let collection = client.database(name).collection(name);
+    function(collection);
     let events: Vec<_> = client
         .events
         .write()
         .unwrap()
         .drain(..)
-        .filter(|event| event.command_name == "count_documents")
+        .filter(|event| event.command_name == command_name)
         .collect();
     assert_eq!(events.len(), 1);
+    events
+}
+
+#[test]
+#[named]
+fn test_count_with_read_concern() {
+    let events = helper(function_name!(), "count_documents", |collection| {
+        collection
+            .count_documents(
+                None,
+                Some(
+                    CountOptions::builder()
+                        .read_concern(ReadConcern::Local)
+                        .build(),
+                ),
+            )
+            .unwrap();
+    });
     let read_concern = events[0].command.get("readConcern").unwrap();
     assert_eq!(read_concern, &Bson::Document(doc! {"level" : "local"}));
 }
@@ -41,44 +52,25 @@ fn test_count_with_read_concern() {
 #[test]
 #[named]
 fn test_count_without_read_concern() {
-    let client = EventClient::new();
-    let collection = client
-        .database(function_name!())
-        .collection(function_name!());
-    collection.count_documents(None, None).unwrap();
-    let events: Vec<_> = client
-        .events
-        .write()
-        .unwrap()
-        .drain(..)
-        .filter(|event| event.command_name == "count_documents")
-        .collect();
-    assert_eq!(events.len(), 1);
+    let events = helper(function_name!(), "count_documents", |collection| {
+        collection.count_documents(None, None).unwrap();
+    });
     assert!(!events[0].command.contains_key("readConcern"));
 }
 
 #[test]
 #[named]
 fn test_estimated_count_documents_with_read_concern() {
-    let client = EventClient::new();
-    let collection = client
-        .database(function_name!())
-        .collection(function_name!());
-    collection
-        .estimated_document_count(Some(
-            EstimatedDocumentCountOptions::builder()
-                .read_concern(ReadConcern::Local)
-                .build(),
-        ))
-        .unwrap();
-    let events: Vec<_> = client
-        .events
-        .write()
-        .unwrap()
-        .drain(..)
-        .filter(|event| event.command_name == "estimated_document_count")
-        .collect();
-    assert_eq!(events.len(), 1);
+    let events = helper(function_name!(), "estimated_document_count", |collection| {
+        collection
+            .estimated_document_count(Some(
+                EstimatedDocumentCountOptions::builder()
+                    .read_concern(ReadConcern::Local)
+                    .build(),
+            ))
+            .unwrap();
+    });
+
     let read_concern = events[0].command.get("readConcern").unwrap();
     assert_eq!(read_concern, &Bson::Document(doc! {"level" : "local"}));
 }
@@ -86,47 +78,28 @@ fn test_estimated_count_documents_with_read_concern() {
 #[test]
 #[named]
 fn test_estimated_count_documents_without_read_concern() {
-    let client = EventClient::new();
-    let collection = client
-        .database(function_name!())
-        .collection(function_name!());
-    collection.estimated_document_count(None).unwrap();
-    let events: Vec<_> = client
-        .events
-        .write()
-        .unwrap()
-        .drain(..)
-        .filter(|event| event.command_name == "estimated_document_count")
-        .collect();
-    assert_eq!(events.len(), 1);
+    let events = helper(function_name!(), "estimated_document_count", |collection| {
+        collection.estimated_document_count(None).unwrap();
+    });
     assert!(!events[0].command.contains_key("readConcern"));
 }
 
 #[test]
 #[named]
 fn test_aggregate_with_read_concern() {
-    let client = EventClient::new();
-    let collection = client
-        .database(function_name!())
-        .collection(function_name!());
-    collection
-        .aggregate(
-            None,
-            Some(
-                AggregateOptions::builder()
-                    .read_concern(ReadConcern::Local)
-                    .build(),
-            ),
-        )
-        .unwrap();
-    let events: Vec<_> = client
-        .events
-        .write()
-        .unwrap()
-        .drain(..)
-        .filter(|event| event.command_name == "aggregate")
-        .collect();
-    assert_eq!(events.len(), 1);
+    let events = helper(function_name!(), "aggregate", |collection| {
+        collection
+            .aggregate(
+                None,
+                Some(
+                    AggregateOptions::builder()
+                        .read_concern(ReadConcern::Local)
+                        .build(),
+                ),
+            )
+            .unwrap();
+    });
+
     let read_concern = events[0].command.get("readConcern").unwrap();
     assert_eq!(read_concern, &Bson::Document(doc! {"level" : "local"}));
 }
@@ -134,48 +107,28 @@ fn test_aggregate_with_read_concern() {
 #[test]
 #[named]
 fn test_aggregate_without_read_concern() {
-    let client = EventClient::new();
-    let collection = client
-        .database(function_name!())
-        .collection(function_name!());
-    collection.aggregate(None, None).unwrap();
-    let events: Vec<_> = client
-        .events
-        .write()
-        .unwrap()
-        .drain(..)
-        .filter(|event| event.command_name == "aggregate")
-        .collect();
-    assert_eq!(events.len(), 1);
+    let events = helper(function_name!(), "aggregate", |collection| {
+        collection.aggregate(None, None).unwrap();
+    });
     assert!(!events[0].command.contains_key("readConcern"));
 }
 
 #[test]
 #[named]
 fn test_distinct_with_read_concern() {
-    let client = EventClient::new();
-    let collection = client
-        .database(function_name!())
-        .collection(function_name!());
-    collection
-        .distinct(
-            "",
-            None,
-            Some(
-                DistinctOptions::builder()
-                    .read_concern(ReadConcern::Local)
-                    .build(),
-            ),
-        )
-        .unwrap();
-    let events: Vec<_> = client
-        .events
-        .write()
-        .unwrap()
-        .drain(..)
-        .filter(|event| event.command_name == "distinct")
-        .collect();
-    assert_eq!(events.len(), 1);
+    let events = helper(function_name!(), "distinct", |collection| {
+        collection
+            .distinct(
+                "",
+                None,
+                Some(
+                    DistinctOptions::builder()
+                        .read_concern(ReadConcern::Local)
+                        .build(),
+                ),
+            )
+            .unwrap();
+    });
     let read_concern = events[0].command.get("readConcern").unwrap();
     assert_eq!(read_concern, &Bson::Document(doc! {"level" : "local"}));
 }
@@ -183,65 +136,37 @@ fn test_distinct_with_read_concern() {
 #[test]
 #[named]
 fn test_distinct_without_read_concern() {
-    let client = EventClient::new();
-    let collection = client
-        .database(function_name!())
-        .collection(function_name!());
-    collection.distinct("", None, None).unwrap();
-    let events: Vec<_> = client
-        .events
-        .write()
-        .unwrap()
-        .drain(..)
-        .filter(|event| event.command_name == "distinct")
-        .collect();
-    assert_eq!(events.len(), 1);
+    let events = helper(function_name!(), "distinct", |collection| {
+        collection.distinct("", None, None).unwrap();
+    });
     assert!(!events[0].command.contains_key("readConcern"));
 }
 
 #[test]
 #[named]
 fn test_find_with_read_concern() {
-    let client = EventClient::new();
-    let collection = client
-        .database(function_name!())
-        .collection(function_name!());
-    collection.find(None, None).unwrap();
-    let events: Vec<_> = client
-        .events
-        .write()
-        .unwrap()
-        .drain(..)
-        .filter(|event| event.command_name == "find")
-        .collect();
-    assert_eq!(events.len(), 1);
+    let events = helper(function_name!(), "find", |collection| {
+        collection
+            .find(
+                None,
+                Some(
+                    FindOptions::builder()
+                        .read_concern(ReadConcern::Local)
+                        .build(),
+                ),
+            )
+            .unwrap();
+    });
+
     assert!(!events[0].command.contains_key("readConcern"));
 }
 
 #[test]
 #[named]
 fn test_find_without_read_concern() {
-    let client = EventClient::new();
-    let collection = client
-        .database(function_name!())
-        .collection(function_name!());
-    collection
-        .find(
-            None,
-            Some(
-                FindOptions::builder()
-                    .read_concern(ReadConcern::Local)
-                    .build(),
-            ),
-        )
-        .unwrap();
-    let events: Vec<_> = client
-        .events
-        .write()
-        .unwrap()
-        .drain(..)
-        .filter(|event| event.command_name == "find")
-        .collect();
-    assert_eq!(events.len(), 1);
+    let events = helper(function_name!(), "find", |collection| {
+        collection.find(None, None).unwrap();
+    });
+
     assert!(!events[0].command.contains_key("readConcern"));
 }
