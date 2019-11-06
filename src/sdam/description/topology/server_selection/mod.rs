a4975f6bfb280af07255eb53ab8f39c154f15907
Saghm Rossi <saghmrossi@gmail.com>
2019-11-06 11:04:38 -0500

    RUST-233 Add topology helper to update command with read preferen… (#59)

--- src/sdam/description/topology/server_selection/mod.rs
+++ src/sdam/description/topology/server_selection/mod.rs
@@ -1,16 +1,29 @@
+use derivative::Derivative;
 use rand::seq::IteratorRandom;
 
 use super::TopologyDescription;
 use crate::{read_preference::ReadPreference, sdam::description::server::ServerDescription};
 
 /// Describes which servers are suitable for a given operation.
+#[derive(Derivative)]
+#[derivative(Debug)]
 pub(crate) enum SelectionCriteria {
     /// A read preference that describes the suitable servers based on the server type, max
     /// staleness, and server tags.
     ReadPreference(ReadPreference),
+
     /// A predicate used to filter servers that are considered suitable. A `server` will be
     /// considered suitable by a `predicate` if `predicate(server)` returns true.
-    Predicate(Box<dyn Fn(&ServerDescription) -> bool>),
+    Predicate(#[derivative(Debug = "ignore")] Box<dyn Fn(&ServerDescription) -> bool>),
+}
+
+impl SelectionCriteria {
+    pub(crate) fn as_read_pref(&self) -> Option<&ReadPreference> {
+        match self {
+            Self::ReadPreference(ref read_pref) => Some(read_pref),
+            Self::Predicate(..) => None,
+        }
+    }
 }
 
 impl TopologyDescription {
