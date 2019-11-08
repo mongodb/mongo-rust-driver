8e3a14b2e18e9bd0b8a87ddcf2701069aa50ed14
Patrick Freed <patrick.freed@mongodb.com>
2019-11-08 16:47:25 -0500

    move tests to their own files

---
+++ src/operation/delete/test.rs
@@ -0,0 +1,185 @@
+use bson::{bson, doc};
+
+use crate::{
+    bson_util,
+    cmap::{CommandResponse, StreamDescription},
+    concern::{Acknowledgment, WriteConcern},
+    error::{ErrorKind, WriteConcernError, WriteError, WriteFailure},
+    operation::{test, Delete, Operation},
+    Namespace,
+};
+
+#[test]
+fn build_many() {
+    let ns = Namespace {
+        db: "test_db".to_string(),
+        coll: "test_coll".to_string(),
+    };
+    let filter = doc! { "x": { "$gt": 1 } };
+
+    let wc = WriteConcern {
+        w: Some(Acknowledgment::Majority),
+        ..Default::default()
+    };
+
+    let op = Delete::new(ns, filter.clone(), None, Some(wc), None);
+
+    let description = StreamDescription::new_testing();
+    let mut cmd = op.build(&description).unwrap();
+
+    assert_eq!(cmd.name.as_str(), "delete");
+    assert_eq!(cmd.target_db.as_str(), "test_db");
+    assert_eq!(cmd.read_pref.as_ref(), None);
+
+    let mut expected_body = doc! {
+        "delete": "test_coll",
+        "deletes": [
+            {
+                "q": filter,
+                "limit": 0,
+            }
+        ],
+        "writeConcern": {
+            "w": "majority"
+        },
+    };
+
+    bson_util::sort_document(&mut cmd.body);
+    bson_util::sort_document(&mut expected_body);
+
+    assert_eq!(cmd.body, expected_body);
+}
+
+#[test]
+fn build_one() {
+    let ns = Namespace {
+        db: "test_db".to_string(),
+        coll: "test_coll".to_string(),
+    };
+    let filter = doc! { "x": { "$gt": 1 } };
+
+    let wc = WriteConcern {
+        w: Some(Acknowledgment::Majority),
+        ..Default::default()
+    };
+
+    let op = Delete::new(ns, filter.clone(), Some(1), Some(wc), None);
+
+    let description = StreamDescription::new_testing();
+    let mut cmd = op.build(&description).unwrap();
+
+    assert_eq!(cmd.name.as_str(), "delete");
+    assert_eq!(cmd.target_db.as_str(), "test_db");
+    assert_eq!(cmd.read_pref.as_ref(), None);
+
+    let mut expected_body = doc! {
+        "delete": "test_coll",
+        "deletes": [
+            {
+                "q": filter,
+                "limit": 1,
+            }
+        ],
+        "writeConcern": {
+            "w": "majority"
+        },
+    };
+
+    bson_util::sort_document(&mut cmd.body);
+    bson_util::sort_document(&mut expected_body);
+
+    assert_eq!(cmd.body, expected_body);
+}
+
+#[test]
+fn handle_success() {
+    let op = Delete::empty();
+
+    let ok_response = CommandResponse::from_document(doc! {
+        "ok": 1.0,
+        "n": 3,
+    });
+
+    let ok_result = op.handle_response(ok_response);
+    assert!(ok_result.is_ok());
+
+    let delete_result = ok_result.unwrap();
+    assert_eq!(delete_result.deleted_count, 3);
+}
+
+#[test]
+fn handle_invalid_response() {
+    let op = Delete::empty();
+
+    let invalid_response = CommandResponse::from_document(doc! { "ok": 1.0, "asdfadsf": 123123 });
+    assert!(op.handle_response(invalid_response).is_err());
+}
+
+#[test]
+fn handle_command_error() {
+    test::handle_command_error(Delete::empty())
+}
+
+#[test]
+fn handle_write_failure() {
+    let op = Delete::empty();
+
+    let write_error_response = CommandResponse::from_document(doc! {
+        "ok": 1.0,
+        "n": 0,
+        "writeErrors": [
+            {
+                "index": 0,
+                "code": 1234,
+                "errmsg": "my error string"
+            }
+        ]
+    });
+    let write_error_result = op.handle_response(write_error_response);
+    assert!(write_error_result.is_err());
+    match *write_error_result.unwrap_err().kind {
+        ErrorKind::WriteError {
+            inner: WriteFailure::WriteError(ref error),
+        } => {
+            let expected_err = WriteError {
+                code: 1234,
+                code_name: None,
+                message: "my error string".to_string(),
+            };
+            assert_eq!(error, &expected_err);
+        }
+        ref e => panic!("expected write error, got {:?}", e),
+    };
+}
+
+#[test]
+fn handle_write_concern_failure() {
+    let op = Delete::empty();
+
+    let wc_error_response = CommandResponse::from_document(doc! {
+        "ok": 1.0,
+        "n": 0,
+        "writeConcernError": {
+            "code": 456,
+            "codeName": "wcError",
+            "errmsg": "some message"
+        }
+    });
+
+    let wc_error_result = op.handle_response(wc_error_response);
+    assert!(wc_error_result.is_err());
+
+    match *wc_error_result.unwrap_err().kind {
+        ErrorKind::WriteError {
+            inner: WriteFailure::WriteConcernError(ref wc_error),
+        } => {
+            let expected_wc_err = WriteConcernError {
+                code: 456,
+                code_name: "wcError".to_string(),
+                message: "some message".to_string(),
+            };
+            assert_eq!(wc_error, &expected_wc_err);
+        }
+        ref e => panic!("expected write concern error, got {:?}", e),
+    }
+}
---
+++ src/operation/drop/test.rs
@@ -0,0 +1,80 @@
+use bson::{bson, doc};
+
+use crate::{
+    cmap::{CommandResponse, StreamDescription},
+    concern::{Acknowledgment, WriteConcern},
+    error::{ErrorKind, WriteFailure},
+    operation::{test, Drop, Operation},
+    Namespace,
+};
+
+#[test]
+fn build() {
+    let op = Drop::new(
+        Namespace {
+            db: "test_db".to_string(),
+            coll: "test_coll".to_string(),
+        },
+        Some(WriteConcern {
+            w: Some(Acknowledgment::Tag("abc".to_string())),
+            ..Default::default()
+        }),
+    );
+
+    let description = StreamDescription::new_testing();
+    let cmd = op.build(&description).unwrap();
+
+    assert_eq!(cmd.name.as_str(), "drop");
+    assert_eq!(cmd.target_db.as_str(), "test_db");
+    assert_eq!(cmd.read_pref.as_ref(), None);
+    assert_eq!(
+        cmd.body,
+        doc! {
+            "drop": "test_coll",
+            "writeConcern": { "w": "abc" }
+        }
+    );
+}
+
+#[test]
+fn handle_success() {
+    let op = Drop::empty();
+
+    let ok_response = CommandResponse::from_document(doc! { "ok": 1.0 });
+    assert!(op.handle_response(ok_response).is_ok());
+    let ok_extra = CommandResponse::from_document(doc! { "ok": 1.0, "hello": "world" });
+    assert!(op.handle_response(ok_extra).is_ok());
+}
+
+#[test]
+fn handle_command_error() {
+    test::handle_command_error(Drop::empty());
+}
+
+#[test]
+fn handle_write_concern_error() {
+    let op = Drop::empty();
+
+    let response = CommandResponse::from_document(doc! {
+        "writeConcernError": {
+            "code": 100,
+            "codeName": "hello world",
+            "errmsg": "12345"
+        },
+        "ok": 1
+    });
+
+    let result = op.handle_response(response);
+    assert!(result.is_err());
+
+    match *result.unwrap_err().kind {
+        ErrorKind::WriteError {
+            inner: WriteFailure::WriteConcernError(ref wc_err),
+        } => {
+            assert_eq!(wc_err.code, 100);
+            assert_eq!(wc_err.code_name, "hello world");
+            assert_eq!(wc_err.message, "12345");
+        }
+        ref e => panic!("expected write concern error, got {:?}", e),
+    }
+}
