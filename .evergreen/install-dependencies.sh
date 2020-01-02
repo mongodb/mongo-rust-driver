a1437551a980f7bfc9811471664f3fb4ba2422a3
Patrick Freed <patrick.freed@mongodb.com>
2020-01-02 14:49:43 -0500

    minor: fix macOS evergreen failures (#111)

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -3,5 +3,6 @@
 rm -rf ~/.rustup
 curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
 
+echo "export CARGO_NET_GIT_FETCH_WITH_CLI=true" >> ~/.cargo/env
 . ~/.cargo/env
 rustup toolchain install nightly -c rustfmt
