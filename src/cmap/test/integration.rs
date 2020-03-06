70f08b2d7cf894e1fd23861b1514eac48802a8db
Patrick Freed <patrick.freed@mongodb.com>
2020-03-06 14:01:20 -0500

    remove unneccessary block_on

--- src/cmap/test/integration.rs
+++ src/cmap/test/integration.rs
@@ -5,7 +5,6 @@ use crate::{
     cmap::{options::ConnectionPoolOptions, Command, ConnectionPool},
     selection_criteria::ReadPreference,
     test::{CLIENT, LOCK},
-    RUNTIME,
 };
 
 #[derive(Debug, Deserialize)]
@@ -27,7 +26,7 @@ async fn acquire_connection_and_send_command() {
     let pool_options = ConnectionPoolOptions::from_client_options(&client_options);
 
     let pool = ConnectionPool::new(client_options.hosts[0].clone(), Some(pool_options));
-    let mut connection = RUNTIME.block_on(pool.check_out()).unwrap();
+    let mut connection = pool.check_out().await.unwrap();
 
     let body = doc! { "listDatabases": 1 };
     let read_pref = ReadPreference::PrimaryPreferred {
