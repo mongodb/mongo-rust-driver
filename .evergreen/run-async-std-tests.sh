512d03d2ab69497644c775be2da343b274c73411
Patrick Freed <patrick.freed@mongodb.com>
2020-04-10 17:17:40 -0400

    only run sync doc tests in sync mode

--- .evergreen/run-async-std-tests.sh
+++ .evergreen/run-async-std-tests.sh
@@ -5,4 +5,4 @@ set -o errexit
 . ~/.cargo/env
 RUST_BACKTRACE=1 cargo test --no-default-features --features async-std-runtime
 RUST_BACKTRACE=1 cargo test sync --no-default-features --features sync
-RUST_BACKTRACE=1 cargo test --doc --no-default-features --features sync
+RUST_BACKTRACE=1 cargo test --doc sync --no-default-features --features sync
