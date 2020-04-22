1a0c8af2afdf25ac7e02cbe1ffd20044ffc22bd8
Saghm Rossi <saghmrossi@gmail.com>
2020-04-22 12:12:59 -0400

    minor: run macos CI tests single-threaded

--- .evergreen/run-async-std-tests.sh
+++ .evergreen/run-async-std-tests.sh
@@ -3,6 +3,13 @@
 set -o errexit
 
 . ~/.cargo/env
-RUST_BACKTRACE=1 cargo test --no-default-features --features async-std-runtime
-RUST_BACKTRACE=1 cargo test sync --no-default-features --features sync
-RUST_BACKTRACE=1 cargo test --doc sync --no-default-features --features sync
+
+if [ "$SINGLE_THREAD" = true ]; then
+	OPTIONS="-- --test-threads=1"
+fi
+
+echo "cargo test options: ${OPTIONS}"
+
+RUST_BACKTRACE=1 cargo test --no-default-features --features async-std-runtime $OPTIONS
+RUST_BACKTRACE=1 cargo test sync --no-default-features --features sync $OPTIONS
+RUST_BACKTRACE=1 cargo test --doc sync --no-default-features --features sync $OPTIONS
