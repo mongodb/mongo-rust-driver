3f198f0d1d5e9732fe128810af3203254abfb00c
Isabel Atkinson <isabel.atkinson@mongodb.com>
2020-05-26 11:56:34 -0500

    remove json files

---
+++ src/test/spec/runner/test_event.rs
@@ -0,0 +1,95 @@
+use crate::test::{CommandEvent, Matchable};
+use bson::Document;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum TestEvent {
+    CommandStartedEvent {
+        command_name: Option<String>,
+        database_name: Option<String>,
+        command: Document,
+    },
+
+    CommandSucceededEvent {
+        command_name: String,
+        reply: Document,
+    },
+
+    CommandFailedEvent {
+        command_name: String,
+    },
+}
+
+impl Matchable for TestEvent {
+    fn content_matches(&self, actual: &TestEvent) -> bool {
+        match (self, actual) {
+            (
+                TestEvent::CommandStartedEvent {
+                    command_name: actual_command_name,
+                    database_name: actual_database_name,
+                    command: actual_command,
+                },
+                TestEvent::CommandStartedEvent {
+                    command_name: expected_command_name,
+                    database_name: expected_database_name,
+                    command: expected_command,
+                },
+            ) => {
+                if actual_command_name.is_some()
+                    && expected_command_name.is_some()
+                    && actual_command_name != expected_command_name
+                {
+                    return false;
+                }
+                if actual_database_name.is_some()
+                    && expected_database_name.is_some()
+                    && actual_database_name != expected_database_name
+                {
+                    return false;
+                }
+                actual_command.matches(expected_command)
+            }
+            (
+                TestEvent::CommandSucceededEvent {
+                    command_name: actual_command_name,
+                    reply: actual_reply,
+                },
+                TestEvent::CommandSucceededEvent {
+                    command_name: expected_command_name,
+                    reply: expected_reply,
+                },
+            ) => {
+                actual_command_name == expected_command_name && actual_reply.matches(expected_reply)
+            }
+            (
+                TestEvent::CommandFailedEvent {
+                    command_name: actual_command_name,
+                },
+                TestEvent::CommandFailedEvent {
+                    command_name: expected_command_name,
+                },
+            ) => actual_command_name == expected_command_name,
+            _ => false,
+        }
+    }
+}
+
+impl From<CommandEvent> for TestEvent {
+    fn from(event: CommandEvent) -> Self {
+        match event {
+            CommandEvent::CommandStartedEvent(event) => TestEvent::CommandStartedEvent {
+                command_name: Some(event.command_name),
+                database_name: Some(event.db),
+                command: event.command,
+            },
+            CommandEvent::CommandFailedEvent(event) => TestEvent::CommandFailedEvent {
+                command_name: event.command_name,
+            },
+            CommandEvent::CommandSucceededEvent(event) => TestEvent::CommandSucceededEvent {
+                command_name: event.command_name,
+                reply: event.reply,
+            },
+        }
+    }
+}
---
+++ src/test/spec/runner/test_file.rs
@@ -0,0 +1,95 @@
+use crate::test::{TestEvent, util::{parse_version, EventClient}};
+use bson::{doc, Bson, Document};
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize)]
+pub struct TestFile {
+    #[serde(rename = "runOn")]
+    pub run_on: Option<Vec<RunOn>>,
+    pub database_name: Option<String>,
+    pub collection_name: Option<String>,
+    pub bucket_name: Option<String>,
+    pub data: Option<TestData>,
+    pub tests: Vec<TestCase>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum TestData {
+    Single(Vec<Document>),
+    Many(HashMap<String, Vec<Document>>),
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RunOn {
+    pub min_server_version: Option<String>,
+    pub max_server_version: Option<String>,
+    pub topology: Option<Vec<String>>,
+}
+
+impl RunOn {
+    pub fn can_run_on(&self, client: &EventClient) -> bool {
+        if let Some(ref min_server_version) = self.min_server_version {
+            let (major, minor) = parse_version(min_server_version);
+            if client.server_version_lt(major, minor) {
+                return false;
+            }
+        }
+        if let Some(ref max_server_version) = self.max_server_version {
+            let (major, minor) = parse_version(max_server_version);
+            if client.server_version_gt(major, minor) {
+                return false;
+            }
+        }
+        if let Some(ref topology) = self.topology {
+            if !topology.contains(&client.topology()) {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TestCase {
+    pub description: String,
+    pub client_options: Option<Document>,
+    pub use_multiple_mongoses: Option<bool>,
+    pub skip_reason: Option<String>,
+    pub fail_point: Option<Document>,
+    pub operations: Vec<Operation>,
+    pub outcome: Option<Document>,
+    pub expectations: Option<Vec<TestEvent>>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Operation {
+    pub name: String,
+    pub object: OperationObject,
+    pub arguments: Option<Bson>,
+    pub result: Option<Bson>,
+    pub error: Option<bool>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum OperationObject {
+    Database,
+    Collection,
+    Client,
+    GridfsBucket,
+}
+
+impl Operation {
+    pub fn as_document(&self) -> Document {
+        let mut doc = Document::new();
+        doc.insert("name", self.name.clone());
+        if let Some(ref arguments) = self.arguments {
+            doc.insert("arguments", arguments);
+        }
+        doc
+    }
+}
