4ac336e57905adff720022858740687f06014e77
Isabel Atkinson <isabel.atkinson@mongodb.com>
2020-06-01 09:42:36 -0500

    reverse branches

--- src/client/executor.rs
+++ src/client/executor.rs
@@ -145,10 +145,10 @@ impl Client {
                     .topology
                     .handle_post_handshake_error(err.clone(), conn, server)
                     .await;
-                if !(err.is_server_error() || err.is_read_retryable()) {
-                    Err(first_error)
-                } else {
+                if err.is_server_error() || err.is_read_retryable() {
                     Err(err)
+                } else {
+                    Err(first_error)
                 }
             }
         }
