e8aa1db68f94a8c93284efa5c69a5640568bf0d1
Isabel Atkinson <isabel.atkinson@mongodb.com>
2020-07-24 13:11:33 -0500

    RUST-478 add connection snippet examples

---
+++ examples/connection_snippets.rs
@@ -0,0 +1,45 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+extern crate mongodb;
+
+use mongodb::error::Result;
+
+const CONNECTION_URI: &str =
+    "mongodb+srv://<username>:<password>@<cluster-url>/<dbname>?w=majority";
+
+#[cfg(feature = "sync")]
+fn main() -> Result<()> {
+    use mongodb::sync::Client;
+    let client = Client::with_uri_str(CONNECTION_URI)?;
+    let database = client.database("test");
+    Ok(())
+}
+
+#[cfg(feature = "tokio-runtime")]
+#[tokio::main]
+async fn main() -> Result<()> {
+    use mongodb::{options::ClientOptions, Client};
+    let client_options = ClientOptions::parse(CONNECTION_URI).await?;
+    let client = Client::with_options(client_options)?;
+    let database = client.database("test");
+    Ok(())
+}
+
+#[cfg(feature = "async-std-runtime")]
+#[async_std::main]
+async fn main() -> Result<()> {
+    use mongodb::{options::ClientOptions, Client};
+    let client_options = ClientOptions::parse(CONNECTION_URI).await?;
+    let client = Client::with_options(client_options)?;
+    let database = client.database("test");
+    Ok(())
+}
+
+#[cfg(all(
+    not(feature = "sync"),
+    not(feature = "tokio-runtime"),
+    not(feature = "async-std-runtime")
+))]
+fn main() {}
