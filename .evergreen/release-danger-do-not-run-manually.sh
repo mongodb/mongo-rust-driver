c2e5e74e86b465635a6afaef22662ef5ee0c344c
Saghm Rossi <git@saghm.com>
2020-10-14 17:18:18 -0400

    test on fake release

---
+++ .evergreen/release-danger-do-not-run-manually.sh
@@ -0,0 +1,31 @@
+# ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢
+# # Danger!
+#
+# This script is used to publish a release of the driver to crates.io.
+#
+# Do not run it manually!
+# ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠ ☢ ☠
+
+# Disable tracing
+# set +x
+
+set -x
+
+set -o errexit
+
+if [[ -z "$TAG" ]]; then
+	>&2 echo "\$TAG must be set to the git tag of the release"
+	exit 1
+fi
+
+if [[ -z "$TOKEN" ]]; then
+	>&2 echo "\$TOKEN must be set to the crates.io authentication token"
+	exit 1
+fi
+
+git fetch origin $TAG
+git checkout $TAG
+
+. ~/.cargo/env
+
+cargo package
