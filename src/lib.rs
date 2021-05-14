dfb484ba96e409f5e880a82fd88a6a066966f8a2
Patrick Freed <patrick.freed@mongodb.com>
2021-05-14 17:41:52 -0400

    release v2.0.0-beta

--- src/lib.rs
+++ src/lib.rs
@@ -83,7 +83,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(test, type_length_limit = "80000000")]
 
-#![doc(html_root_url = "https://docs.rs/mongodb/2.0.0-alpha.1")]
+#![doc(html_root_url = "https://docs.rs/mongodb/2.0.0-beta")]
 
 macro_rules! define_if_single_runtime_enabled {
     ( $( $def:item )+ ) => {
