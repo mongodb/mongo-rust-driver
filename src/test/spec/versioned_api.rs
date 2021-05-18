5fff3647358257b3911b8b067317ec3499325f7c
Isabel Atkinson <isabel.atkinson@mongodb.com>
2021-05-18 16:48:16 -0400

    bump min version

--- src/test/spec/versioned_api.rs
+++ src/test/spec/versioned_api.rs
@@ -3,7 +3,7 @@ use tokio::sync::{RwLockReadGuard, RwLockWriteGuard};
 use crate::{
     bson::doc,
     options::{ServerApi, ServerApiVersion},
-    test::{run_spec_test, EventClient, LOCK, CLIENT_OPTIONS},
+    test::{run_spec_test, EventClient, CLIENT_OPTIONS, LOCK},
 };
 
 use super::run_unified_format_test;
@@ -21,29 +21,40 @@ async fn run() {
 async fn transaction_handling() {
     let _guard: RwLockReadGuard<_> = LOCK.run_concurrently().await;
 
-    let version = ServerApi::builder().version(ServerApiVersion::Version1).build();
+    let version = ServerApi::builder()
+        .version(ServerApiVersion::Version1)
+        .build();
 
     let mut options = CLIENT_OPTIONS.clone();
     options.server_api = Some(version);
     let client = EventClient::with_options(options).await;
-    if !client.is_replica_set() || client.server_version_lt(4, 9) {
+    if !client.is_replica_set() || client.server_version_lt(5, 0) {
         return;
     }
 
     let mut session = client.start_session(None).await.unwrap();
     session.start_transaction(None).await.unwrap();
 
-    let coll = client.database(function_name!()).collection(function_name!());
+    let coll = client
+        .database(function_name!())
+        .collection(function_name!());
 
-    coll.insert_one_with_session(doc! { "x": 1 }, None, &mut session).await.unwrap();
+    coll.insert_one_with_session(doc! { "x": 1 }, None, &mut session)
+        .await
+        .unwrap();
     session.commit_transaction().await.unwrap();
     session.commit_transaction().await.unwrap();
 
     session.start_transaction(None).await.unwrap();
 
-    coll.insert_one_with_session(doc! { "y": 2 }, None, &mut session).await.unwrap();
+    coll.insert_one_with_session(doc! { "y": 2 }, None, &mut session)
+        .await
+        .unwrap();
     session.abort_transaction().await.unwrap();
-    session.abort_transaction().await.expect("aborting twice should fail");
+    session
+        .abort_transaction()
+        .await
+        .expect("aborting twice should fail");
 
     let events = client.get_all_command_started_events();
     for event in events {
