9cef3e6bbfe51bfada0162df2a5ff23b773490de
AsianIntel <gautam.abhyankar@gmail.com>
2021-06-21 17:36:31 +0530

    Drop reqwest in favor of hyper

--- src/runtime/http.rs
+++ src/runtime/http.rs
@@ -1,12 +1,34 @@
 #[cfg(feature = "tokio-runtime")]
-use reqwest::{Method, Response};
+use hyper::{
+    body::{self, Buf},
+    client::HttpConnector,
+    Body,
+    Client as HyperClient,
+    Error as HyperError,
+    Method,
+    Request,
+    Response,
+};
+#[cfg(feature = "tokio-runtime")]
+use hyper_rustls::HttpsConnector;
 #[cfg(feature = "tokio-runtime")]
 use serde::Deserialize;
+#[cfg(feature = "tokio-runtime")]
+use serde_json::Error as SerdeError;
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug)]
 pub(crate) struct HttpClient {
     #[cfg(feature = "tokio-runtime")]
-    inner: reqwest::Client,
+    inner: HyperClient<HttpsConnector<HttpConnector>>,
+}
+
+#[cfg(feature = "tokio-runtime")]
+#[derive(Debug)]
+pub(crate) enum HttpError {
+    BuildingRequest,
+    Request(HyperError),
+    InvalidUTF8,
+    Parsing(SerdeError),
 }
 
 #[cfg(feature = "tokio-runtime")]
@@ -16,17 +38,18 @@ impl HttpClient {
         &self,
         uri: &str,
         headers: impl IntoIterator<Item = &'a (&'a str, &'a str)>,
-    ) -> reqwest::Result<T>
+    ) -> Result<T, HttpError>
     where
         T: for<'de> Deserialize<'de>,
     {
-        let value = self
-            .request(Method::GET, uri, headers)
-            .await?
-            .json()
-            .await?;
+        let res = self.request(Method::GET, uri, headers).await?;
 
-        Ok(value)
+        let mut buf = body::aggregate(res.into_body()).await?;
+        let mut bytes = vec![0; buf.remaining()];
+        buf.copy_to_slice(&mut bytes);
+
+        let result = serde_json::from_slice(&bytes)?;
+        Ok(result)
     }
 
     /// Executes an HTTP GET request and returns the response body as a string.
@@ -34,7 +57,7 @@ impl HttpClient {
         &self,
         uri: &str,
         headers: impl IntoIterator<Item = &'a (&'a str, &'a str)>,
-    ) -> reqwest::Result<String> {
+    ) -> Result<String, HttpError> {
         self.request_and_read_string(Method::GET, uri, headers)
             .await
     }
@@ -44,7 +67,7 @@ impl HttpClient {
         &self,
         uri: &str,
         headers: impl IntoIterator<Item = &'a (&'a str, &'a str)>,
-    ) -> reqwest::Result<String> {
+    ) -> Result<String, HttpError> {
         self.request_and_read_string(Method::PUT, uri, headers)
             .await
     }
@@ -55,9 +78,14 @@ impl HttpClient {
         method: Method,
         uri: &str,
         headers: impl IntoIterator<Item = &'a (&'a str, &'a str)>,
-    ) -> reqwest::Result<String> {
-        let text = self.request(method, uri, headers).await?.text().await?;
+    ) -> Result<String, HttpError> {
+        let res = self.request(method, uri, headers).await?;
 
+        let mut buf = body::aggregate(res.into_body()).await?;
+        let mut bytes = vec![0; buf.remaining()];
+        buf.copy_to_slice(&mut bytes);
+
+        let text = String::from_utf8(bytes)?;
         Ok(text)
     }
 
@@ -67,15 +95,57 @@ impl HttpClient {
         method: Method,
         uri: &str,
         headers: impl IntoIterator<Item = &'a (&'a str, &'a str)>,
-    ) -> reqwest::Result<Response> {
-        let response = headers
-            .into_iter()
-            .fold(self.inner.request(method, uri), |request, (k, v)| {
-                request.header(*k, *v)
-            })
-            .send()
-            .await?;
+    ) -> Result<Response<Body>, HttpError> {
+        let mut request = Request::builder().uri(uri).method(method);
+
+        for header in headers {
+            request = request.header(header.0, header.1);
+        }
+
+        let request = request.body(Body::empty())?;
+        let response = self.inner.request(request).await?;
 
         Ok(response)
+    }
+}
+
+impl Default for HttpClient {
+    fn default() -> Self {
+        #[cfg(feature = "tokio-runtime")]
+        let connector = hyper_rustls::HttpsConnector::with_webpki_roots();
+        #[cfg(feature = "tokio-runtime")]
+        let client = HyperClient::builder().build(connector);
+        Self {
+            #[cfg(feature = "tokio-runtime")]
+            inner: client,
+        }
+    }
+}
+
+#[cfg(feature = "tokio-runtime")]
+impl From<hyper::http::Error> for HttpError {
+    fn from(_err: hyper::http::Error) -> Self {
+        Self::BuildingRequest
+    }
+}
+
+#[cfg(feature = "tokio-runtime")]
+impl From<HyperError> for HttpError {
+    fn from(err: HyperError) -> Self {
+        Self::Request(err)
+    }
+}
+
+#[cfg(feature = "tokio-runtime")]
+impl From<SerdeError> for HttpError {
+    fn from(err: SerdeError) -> Self {
+        Self::Parsing(err)
+    }
+}
+
+#[cfg(feature = "tokio-runtime")]
+impl From<std::string::FromUtf8Error> for HttpError {
+    fn from(_err: std::string::FromUtf8Error) -> Self {
+        Self::InvalidUTF8
     }
 }
