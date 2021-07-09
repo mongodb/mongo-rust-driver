45fb940618b391c10c0f572fcf30b5dbfee05463
Abraham Egnor <abraham.egnor@mongodb.com>
2021-07-09 12:25:24 -0400

    print event mismatch

--- src/test/spec/unified_runner/mod.rs
+++ src/test/spec/unified_runner/mod.rs
@@ -231,7 +231,10 @@ pub async fn run_unified_format_test(test_file: TestFile) {
                 assert_eq!(actual_events.len(), expected_events.len());
 
                 for (actual, expected) in actual_events.iter().zip(expected_events) {
-                    assert!(events_match(actual, expected, Some(&test_runner.entities)));
+                    assert!(
+                        events_match(actual, expected, Some(&test_runner.entities)),
+                        "event mismatch: expected = {:#?}, actual = {:#?}", expected, actual,
+                    );
                 }
             }
         }
