eb7347eda01a92870e94fecf0d32f909a3761a7e
Abraham Egnor <abraham.egnor@mongodb.com>
2021-07-12 16:04:18 -0400

    rename option and tweak comment

--- src/coll/options.rs
+++ src/coll/options.rs
@@ -491,13 +491,14 @@ pub struct AggregateOptions {
     /// be used.
     pub write_concern: Option<WriteConcern>,
 
-    /// Any amount of parameter names, each followed by definitions of constants in the MQL
-    /// Aggregate Expression language.  Each parameter name is then usable to access the value of
-    /// the corresponding MQL Expression with the "$$" syntax within Aggregate Expression contexts.
+    /// A document with any amount of parameter names, each followed by definitions of constants in
+    /// the MQL Aggregate Expression language.  Each parameter name is then usable to access the
+    /// value of the corresponding MQL Expression with the "$$" syntax within Aggregate Expression
+    /// contexts.
     ///
     /// This feature is only available on server versions 5.0 and above.
     #[serde(rename = "let")]
-    pub bindings: Option<Document>,
+    pub let_vars: Option<Document>,
 }
 
 /// Specifies the options to a
