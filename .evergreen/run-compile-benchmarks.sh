97495d2cad83a80a5611e1bb33a7d4095ac56f32
Patrick Freed <patrick.freed@mongodb.com>
2021-08-16 14:40:37 -0400

    fix compilation benchmark

--- .evergreen/run-compile-benchmarks.sh
+++ .evergreen/run-compile-benchmarks.sh
@@ -15,7 +15,8 @@ else
     exit 1
 fi
 
-RUST_VERSION=$(rustc --version)
+rustc --version
+
 SECONDS=0
 cargo build --release
 DURATION_IN_SECONDS="$SECONDS"
@@ -25,13 +26,11 @@ cat > benchmark-results.json <<-EOF
   {
     "info": {
       "test_name": "Compile Time",
-      "args": {
-        "Rust Version": "$RUST_VERSION"
-      }
+      "args": {}
     },
     "metrics": [
       {
-        "name": "Compile Time (s)",
+        "name": "compile_time_seconds",
         "value": $DURATION_IN_SECONDS
       }
     ]
