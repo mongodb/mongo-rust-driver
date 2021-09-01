cd4f2d0385fbe81ab772aa6f6d4fb8d9743d7e83
Patrick Freed <patrick.freed@mongodb.com>
2021-09-01 14:41:36 -0400

    revert to old check-rustdoc.sh

--- .evergreen/check-rustdoc.sh
+++ .evergreen/check-rustdoc.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 . ~/.cargo/env
-cargo +nightly rustdoc --features aws-auth -- -D warnings
-cargo +nightly rustdoc --no-default-features --features async-std-runtime -- -D warnings
-cargo +nightly rustdoc --no-default-features --features sync -- -D warnings
+cargo rustdoc -- -D warnings
+cargo rustdoc --no-default-features --features async-std-runtime -- -D warnings
+cargo rustdoc --no-default-features --features sync -- -D warnings
