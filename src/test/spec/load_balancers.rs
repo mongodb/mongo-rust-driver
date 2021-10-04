6c5cfd67f1895f3771236718f4a38e530924c095
Abraham Egnor <abraham.egnor@mongodb.com>
2021-10-04 11:59:57 -0400

    skip some tests

--- src/test/spec/load_balancers.rs
+++ src/test/spec/load_balancers.rs
@@ -10,10 +10,17 @@ use super::run_unified_format_test_filtered;
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
 async fn run() {
     async fn run_test(path: PathBuf, test_file: TestFile) {
-        if !path.ends_with("cursors.json") { return; }
-        run_unified_format_test_filtered(test_file, |tc|
-            tc.description == "listIndexes pins the cursor to a connection"
-        ).await;
+        // The Rust driver doesn't support wait queue timeouts.
+        if path.ends_with("wait-queue-timeouts.json") {
+            return;
+        }
+        run_unified_format_test_filtered(test_file, |tc| {
+            // TODO RUST-142 unskip this when change streams are implemented.
+            if tc.description == "change streams pin to a connection" {
+                return false;
+            }
+            true
+        }).await;
     }
     let _guard: RwLockWriteGuard<()> = LOCK.run_exclusively().await;
     run_spec_test_with_path(&["load-balancers"], run_test).await;
