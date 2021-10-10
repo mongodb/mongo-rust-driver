b8c409eee0a4e1e8bd0726a28fb668251442a972
Andrew Witten <andrew.witten@mongodb.com>
2021-10-10 14:56:18 -0400

    fix bash script

--- .evergreen/compile-only-tokio.sh
+++ .evergreen/compile-only-tokio.sh
@@ -5,11 +5,11 @@ set -o errexit
 . ~/.cargo/env
 
 # Enable snappy, zlib unconditionally
-FEATURE_FLAGS = "snappy-compression,zlib-compression"
+FEATURE_FLAGS=snappy-compression,zlib-compression
 
 # Zstd requires Rust version 1.54
 if [[ $RUST_VERSION == "nightly" ]]; then
-    FEATURE_FLAGS = "${FEATURE_FLAGS},zstd-compression"
+    FEATURE_FLAGS=$FEATURE_FLAGS,zstd-compression
 fi
 
 rustup run $RUST_VERSION cargo build --features $FEATURE_FLAGS
