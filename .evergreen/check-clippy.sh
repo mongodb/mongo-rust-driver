dd6ac57b2ccf74c35b431e227a0fc345d4fa3610
Andrew Witten <andrew.witten@mongodb.com>
2021-10-11 08:48:48 -0400

    add compressors to check clippy

--- .evergreen/check-clippy.sh
+++ .evergreen/check-clippy.sh
@@ -2,7 +2,14 @@
 
 set -o errexit
 
+FEATURE_FLAGS=snappy-compression,zlib-compression
+
+if [[ $RUST_VERSION == "nightly" ]]; then
+    FEATURE_FLAGS=$FEATURE_FLAGS,zstd-compression
+fi
+
 . ~/.cargo/env
 cargo clippy --all-targets -p mongodb -- -D warnings
+cargo clippy --all-targets -p mongodb --features $FEATURE_FLAGS -- -D warnings
 cargo clippy --all-targets --no-default-features --features async-std-runtime -p mongodb -- -D warnings
 cargo clippy --all-targets --no-default-features --features sync -p mongodb -- -D warnings
