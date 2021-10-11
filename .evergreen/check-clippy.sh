92f0f42a8294f757f149c89fc360b835b632dd3f
Andrew Witten <andrew.witten@mongodb.com>
2021-10-11 10:45:54 -0400

    update check clippy

--- .evergreen/check-clippy.sh
+++ .evergreen/check-clippy.sh
@@ -2,10 +2,9 @@
 
 set -o errexit
 
-FEATURE_FLAGS=zstd-compression,snappy-compression,zlib-compression
-
 . ~/.cargo/env
 cargo clippy --all-targets -p mongodb -- -D warnings
-cargo clippy --all-targets -p mongodb --features $FEATURE_FLAGS -- -D warnings
+# check clippy with compressors separately
+cargo clippy --all-targets -p mongodb --features zstd-compression,snappy-compression,zlib-compression -- -D warnings
 cargo clippy --all-targets --no-default-features --features async-std-runtime -p mongodb -- -D warnings
 cargo clippy --all-targets --no-default-features --features sync -p mongodb -- -D warnings
