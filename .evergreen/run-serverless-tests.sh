0cc16d1ec23d88b61cce2210230ca71e1f0b7f23
Abraham Egnor <abraham.egnor@mongodb.com>
2021-10-19 10:37:54 -0400

    evergreen config wip

---
+++ .evergreen/run-serverless-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -o errexit
+
+FEATURE_FLAGS="zstd-compression,snappy-compression,zlib-compression"
+DEFAULT_FEATURES=""
+
+if [ "$ASYNC_RUNTIME" = "async-std" ]; then
+    FEATURE_FLAGS="${FEATURE_FLAGS},async-std-runtime"
+    DEFAULT_FEATURES="--no-default-features"
+elif [ "$ASYNC_RUNTIME" != "tokio" ]; then
+    echo "invalid async runtime: ${ASYNC_RUNTIME}" >&2
+    exit 1
+fi
+
+. ~/.cargo/env
+
+if [ "$SINGLE_THREAD" = true ]; then
+	OPTIONS="-- --test-threads=1"
+fi
+
+echo "cargo test options: ${DEFAULT_FEATURES} --features $FEATURE_FLAGS ${OPTIONS}"
+
+RUST_BACKTRACE=1 cargo test ${DEFAULT_FEATURES} --features $FEATURE_FLAGS $OPTIONS
