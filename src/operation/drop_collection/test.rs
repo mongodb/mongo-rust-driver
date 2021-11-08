8c0f3301cc99554137318c3f274f9eb241099564
Karmen Liang <karmen.liang@mongodb.com>
2021-11-08 12:41:58 -0800

    add drop_collection test

--- src/operation/drop_collection/test.rs
+++ src/operation/drop_collection/test.rs
@@ -52,6 +52,35 @@ async fn build() {
 
 #[cfg_attr(feature = "tokio-runtime", tokio::test)]
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
+async fn build_no_write_concern() {
+    let options = DropCollectionOptions {
+        write_concern: Some(WriteConcern {
+            ..Default::default()
+        }),
+    };
+
+    let ns = Namespace {
+        db: "test_db".to_string(),
+        coll: "test_coll".to_string(),
+    };
+
+    let mut op = DropCollection::new(ns.clone(), Some(options));
+
+    let description = StreamDescription::new_testing();
+    let cmd = op.build(&description).expect("build should succeed");
+
+    assert_eq!(cmd.name.as_str(), "drop");
+    assert_eq!(cmd.target_db.as_str(), "test_db");
+    assert_eq!(
+        cmd.body,
+        doc! {
+            "drop": "test_coll",
+        }
+    );
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
 async fn handle_success() {
     let op = DropCollection::empty();
 
