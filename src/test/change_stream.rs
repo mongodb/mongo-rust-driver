0190ab0bf7b3a5c1752262651ccba86453634a18
Abraham Egnor <abraham.egnor@mongodb.com>
2022-01-13 10:24:25 -0500

    prose test 2

--- src/test/change_stream.rs
+++ src/test/change_stream.rs
@@ -11,22 +11,23 @@ use super::{LOCK, EventClient};
 /// Prose test 1: ChangeStream must continuously track the last seen resumeToken
 #[cfg_attr(feature = "tokio-runtime", tokio::test)]
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
-async fn track_resume_token() -> Result<(), Box<dyn std::error::Error>> {
+async fn tracks_resume_token() -> Result<(), Box<dyn std::error::Error>> {
     // TODO aegnor: restrict to replica sets and sharded clusters
     let _guard: RwLockReadGuard<()> = LOCK.run_concurrently().await;
 
     let client = EventClient::new().await;
     let db = client.database("change_stream_tests");
     let coll = db.collection::<Document>("track_resume_token");
-    let mut changes = client.watch(None, None).await?;
+    let mut stream = coll.watch(None, None).await?;
     let mut tokens = vec![];
-    tokens.push(changes.resume_token().cloned().unwrap().parsed()?);
+    tokens.push(stream.resume_token().cloned().unwrap().parsed()?);
     for _ in 0..3 {
         coll.insert_one(doc! {}, None).await?;
-        changes.next().await.transpose()?;
-        tokens.push(changes.resume_token().cloned().unwrap().parsed()?);
+        stream.next().await.transpose()?;
+        tokens.push(stream.resume_token().cloned().unwrap().parsed()?);
     }
-    let events: Vec<_> = client.get_command_events(&["aggregate", "getMore"])
+
+    let expected_tokens: Vec<_> = client.get_command_events(&["aggregate", "getMore"])
         .into_iter()
         .filter_map(|ev| {
             match ev {
@@ -34,14 +35,6 @@ async fn track_resume_token() -> Result<(), Box<dyn std::error::Error>> {
                 _ => None,
             }
         })
-        .collect();
-
-    assert_eq!(events.len(), 4);
-    assert_eq!(events[0].command_name, "aggregate");
-    for n in 1..4 {
-        assert_eq!(events[n].command_name, "getMore");
-    }
-    let expected_tokens: Vec<_> = events.iter()
         .map(expected_token)
         .collect();
     assert_eq!(tokens, expected_tokens);
@@ -49,7 +42,7 @@ async fn track_resume_token() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn expected_token(ev: &CommandSucceededEvent) -> Bson {
+fn expected_token(ev: CommandSucceededEvent) -> Bson {
     let cursor = ev.reply.get_document("cursor").unwrap();
     if let Some(token) = cursor.get("postBatchResumeToken") {
         token.clone()
@@ -63,4 +56,23 @@ fn expected_token(ev: &CommandSucceededEvent) -> Bson {
             .unwrap()
             .clone()
     }
+}
+
+/// Prose test 2: ChangeStream will throw an exception if the server response is missing the resume token
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+async fn errors_on_missing_token() -> Result<(), Box<dyn std::error::Error>> {
+    // TODO aegnor: restrict to replica sets and sharded clusters
+    let _guard: RwLockReadGuard<()> = LOCK.run_concurrently().await;
+
+    let client = EventClient::new().await;
+    let db = client.database("change_stream_tests");
+    let coll = db.collection::<Document>("errors_on_missing_token");
+    let mut stream = coll.watch(vec![
+        doc! { "$project": { "_id": 0 } },
+    ], None).await?;
+    coll.insert_one(doc! {}, None).await?;
+    assert!(stream.next().await.transpose().is_err());
+
+    Ok(())
 }
