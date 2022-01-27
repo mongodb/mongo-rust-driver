027fcec8279416c1151ee9ee3176290ac357c8ad
Abraham Egnor <abraham.egnor@mongodb.com>
2022-01-27 09:09:20 -0500

    watch wip

--- src/sync/client/mod.rs
+++ src/sync/client/mod.rs
@@ -1,6 +1,6 @@
 pub mod session;
 
-use super::{ClientSession, Database};
+use super::{ClientSession, Database, ChangeStream};
 use crate::{
     bson::Document,
     concern::{ReadConcern, WriteConcern},
@@ -14,7 +14,7 @@ use crate::{
     },
     results::DatabaseSpecification,
     Client as AsyncClient,
-    RUNTIME,
+    RUNTIME, change_stream::{options::ChangeStreamOptions, event::ChangeStreamEvent},
 };
 
 /// This is the main entry point for the synchronous API. A `Client` is used to connect to a MongoDB
@@ -156,5 +156,34 @@ impl Client {
         RUNTIME
             .block_on(self.async_client.start_session(options))
             .map(Into::into)
+    }
+
+    /// Starts a new [`ChangeStream`] that receives events for all changes in the cluster. The
+    /// stream does not observe changes from system collections or the "config", "local" or
+    /// "admin" databases. Note that this method (`watch` on a cluster) is only supported in
+    /// MongoDB 4.0 or greater.
+    ///
+    /// See the documentation [here](https://docs.mongodb.com/manual/changeStreams/) on change
+    /// streams.
+    ///
+    /// Change streams require either a "majority" read concern or no read
+    /// concern. Anything else will cause a server error.
+    ///
+    /// Note that using a `$project` stage to remove any of the `_id` `operationType` or `ns` fields
+    /// will cause an error. The driver requires these fields to support resumability. For
+    /// more information on resumability, see the documentation for
+    /// [`ChangeStream`](change_stream/struct.ChangeStream.html)
+    ///
+    /// If the pipeline alters the structure of the returned events, the parsed type will need to be
+    /// changed via [`ChangeStream::with_type`].
+    #[allow(unused)]
+    pub(crate) async fn watch(
+        &self,
+        pipeline: impl IntoIterator<Item = Document>,
+        options: impl Into<Option<ChangeStreamOptions>>,
+    ) -> Result<ChangeStream<ChangeStreamEvent<Document>>> {
+        RUNTIME
+            .block_on(self.async_client.watch(pipeline, options))
+            .map(ChangeStream::new)
     }
 }
