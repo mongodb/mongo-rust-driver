036588cbc54cfa7b97015b54432d0708a8f1fc11
Abraham Egnor <abraham.egnor@mongodb.com>
2022-02-03 17:12:44 -0500

    remove filtering

--- src/test/spec/change_streams.rs
+++ src/test/spec/change_streams.rs
@@ -1,25 +1,13 @@
 use crate::test::{
-    spec::unified_runner::TestCase,
-    run_spec_test_with_path,
+    run_spec_test,
     LOCK,
 };
 
-use super::run_unified_format_test_filtered;
+use super::run_unified_format_test;
 
 #[cfg_attr(feature = "tokio-runtime", tokio::test(flavor = "multi_thread"))] // multi_thread required for FailPoint
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
 async fn run() {
     let _guard = LOCK.run_exclusively().await;
-    run_spec_test_with_path(&["change-streams", "unified"], |path, t| {
-        async move {
-            if !path.ends_with("change-streams-resume-errorLabels.json") {
-                return;
-            }
-            run_unified_format_test_filtered(t, test_filter).await
-        }
-    }).await;
-}
-
-fn test_filter(_test: &TestCase) -> bool {
-    true
+    run_spec_test(&["change-streams", "unified"], run_unified_format_test).await;
 }
