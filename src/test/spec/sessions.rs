985964098f25e8309ea73db29f4b430b69d796f7
Isabel Atkinson <isabel.atkinson@mongodb.com>
2022-02-28 11:44:39 -0500

    RUST-1186 Convert sessions tests to unified test format

--- src/test/spec/sessions.rs
+++ src/test/spec/sessions.rs
@@ -6,20 +6,13 @@ use crate::{
     test::{run_spec_test, TestClient, LOCK},
 };
 
-use super::{run_unified_format_test, run_v2_test};
+use super::run_unified_format_test;
 
 #[cfg_attr(feature = "tokio-runtime", tokio::test(flavor = "multi_thread"))]
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
 async fn run_unified() {
     let _guard: RwLockWriteGuard<()> = LOCK.run_exclusively().await;
-    run_spec_test(&["sessions", "unified"], run_unified_format_test).await;
-}
-
-#[cfg_attr(feature = "tokio-runtime", tokio::test(flavor = "multi_thread"))]
-#[cfg_attr(feature = "async-std-runtime", async_std::test)]
-async fn run_legacy() {
-    let _guard: RwLockWriteGuard<()> = LOCK.run_exclusively().await;
-    run_spec_test(&["sessions", "legacy"], run_v2_test).await;
+    run_spec_test(&["sessions"], run_unified_format_test).await;
 }
 
 #[cfg_attr(feature = "tokio-runtime", tokio::test(flavor = "multi_thread"))]
