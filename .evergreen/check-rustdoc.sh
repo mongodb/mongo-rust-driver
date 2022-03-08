f9557b6df47a0f921782b17136b31c64d94906c3
Isabel Atkinson <isabel.atkinson@mongodb.com>
2022-03-08 15:15:33 -0500

    fix rustdoc

--- .evergreen/check-rustdoc.sh
+++ .evergreen/check-rustdoc.sh
@@ -5,5 +5,5 @@ set -o errexit
 . ~/.cargo/env
 cargo +nightly rustdoc -- -D warnings --cfg docsrs
 cargo +nightly rustdoc --no-default-features --features async-std-runtime -- -D warnings --cfg docsrs
-cargo +nightly rustdoc --features sync -- -D warnings --cfg docsrs
+cargo +nightly rustdoc --no-default-features --features sync -- -D warnings --cfg docsrs
 cargo +nightly rustdoc --features tokio-sync -- -D warnings --cfg docsrs
