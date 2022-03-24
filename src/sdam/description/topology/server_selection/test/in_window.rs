b46d8d5b76cee49b0ee8216e6ce1643e5e99cd88
Patrick Freed <patrick.freed@mongodb.com>
2022-03-24 17:37:21 -0400

    undo long sleep

--- src/sdam/description/topology/server_selection/test/in_window.rs
+++ src/sdam/description/topology/server_selection/test/in_window.rs
@@ -256,7 +256,7 @@ async fn load_balancing_test() {
                 client
                     .database("load_balancing_test")
                     .collection::<Document>("load_balancing_test")
-                    .find(doc! { "$where": "sleep(1000) && true" }, options)
+                    .find(doc! { "$where": "sleep(500) && true" }, options)
                     .await
                     .unwrap();
             });
