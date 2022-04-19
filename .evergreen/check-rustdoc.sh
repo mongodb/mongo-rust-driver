a7b8daa70223deefed182ae20f77223686d42132
Kaitlin Mahar <kaitlinmahar@gmail.com>
2022-04-19 15:07:03 -0400

    modify cargo registry perms for rustdoc check

--- .evergreen/check-rustdoc.sh
+++ .evergreen/check-rustdoc.sh
@@ -3,6 +3,16 @@
 set -o errexit
 
 . ~/.cargo/env
+
+# docs.rs builds the driver on a read-only file system. to create a more realistic environment, we first
+# build the driver to ensure we have all the deps already in src, and then limit the permissions on that directory.
+# this is to help us avoid introducing problems like those described here 
+# https://docs.rs/about/builds#read-only-directories where we or a dependency modify source code during the
+# build process.
+cargo +nightly build
+cargo clean
+chmod -R 555 ~/.cargo/registry/src
+
 cargo +nightly rustdoc -- -D warnings --cfg docsrs
 cargo +nightly rustdoc --no-default-features --features async-std-runtime -- -D warnings --cfg docsrs
 cargo +nightly rustdoc --no-default-features --features sync -- -D warnings --cfg docsrs
