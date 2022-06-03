c11735009d06e87349b3cb551f1569a5b171abaf
Kaitlin Mahar <kaitlinmahar@gmail.com>
2022-06-02 22:42:30 -0400

    fix MSRV compilation with async-std

--- .evergreen/compile-only-async-std.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-source ./.evergreen/configure-rust.sh
-
-rustup run $RUST_VERSION cargo build --no-default-features --features async-std-runtime
-rustup run $RUST_VERSION cargo build --no-default-features --features sync
