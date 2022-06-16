f14eae760fd2effeddfe191a02d9720b9f60e3ed
Isabel Atkinson <isabel.atkinson@mongodb.com>
2022-06-16 10:24:46 -0400

    update script

--- .evergreen/check-clippy.sh
+++ .evergreen/check-clippy.sh
@@ -3,11 +3,13 @@
 set -o errexit
 
 source ./.evergreen/configure-rust.sh
-# Pin clippy to the lastest version. This should be updated when new versions of Rust are released.
-rustup default 1.61.0
-
 source ./.evergreen/feature-combinations.sh
 
+# Pin clippy to the latest version. This should be updated when new versions of Rust are released.
+CLIPPY_VERSION=1.61.0
+
+rustup install $CLIPPY_VERSION
+
 for ((i = 0; i < ${#FEATURE_COMBINATIONS[@]}; i++)); do
-    cargo clippy --all-targets ${FEATURE_COMBINATIONS[$i]}  -p mongodb -- -D warnings
+    cargo +$CLIPPY_VERSION clippy --all-targets ${FEATURE_COMBINATIONS[$i]}  -p mongodb -- -D warnings
 done
