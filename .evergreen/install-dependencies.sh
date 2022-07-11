9f33cbdfe26cbbacf8d97b3d806b1a9d5c14de39
Kaitlin Mahar <kaitlinmahar@gmail.com>
2022-07-11 14:24:40 -0400

    refactor deps script to accept arguments for what to install

--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# this script accepts one or more of the following arguments: `rust`, `rustfmt`, `mdbook`, `junit-dependencies`.
+# the corresponding dependencies will be installed in the order they are specified.
+
 set -o xtrace
 set -o errexit
 
@@ -14,39 +17,50 @@ if [ "Windows_NT" == "$OS" ]; then
     export CARGO_HOME=$(cygpath ${CARGO_HOME} --windows)
 fi
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path $DEFAULT_HOST_OPTIONS
+for arg; do
+    if [ $arg == "rust" ]; then
+        curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path $DEFAULT_HOST_OPTIONS
 
-# This file is not created by default on Windows
-echo 'export PATH="$PATH:${CARGO_HOME}/bin"' >> ${CARGO_HOME}/env
-echo "export CARGO_NET_GIT_FETCH_WITH_CLI=true" >> ${CARGO_HOME}/env
+        # This file is not created by default on Windows
+        echo 'export PATH="$PATH:${CARGO_HOME}/bin"' >> ${CARGO_HOME}/env
+        echo "export CARGO_NET_GIT_FETCH_WITH_CLI=true" >> ${CARGO_HOME}/env
+    elif [ $arg == "rustfmt" ]; then
+        source ${CARGO_HOME}/env
+        # Install nightly rustfmt
+        rustup toolchain install nightly -c rustfmt
+    elif [ $arg == "mdbook" ]; then
+        source ${CARGO_HOME}/env
+        # Install the manual rendering tool
+        cargo install mdbook
+    elif [ $arg == "junit-dependencies" ]; then
+        source ${CARGO_HOME}/env
+        # Install tool for converting cargo test output to junit
+        cargo install cargo2junit
 
-source ${CARGO_HOME}/env
+        # install npm/node
+        ./.evergreen/install-node.sh
 
-# Install nightly rustfmt
-rustup toolchain install nightly -c rustfmt
+        source ./.evergreen/env.sh
 
-# Install tool for converting cargo test output to junit
-cargo install cargo2junit
+        # Install tool for merging different junit reports into a single one
+        set +o errexit
+        set -o pipefail
 
-# Install the manual rendering tool
-cargo install mdbook
+        npm install -g junit-report-merger --cache $(mktemp -d) 2>&1 | tee npm-install-output
+        RESULT=$?
+        MATCH=$(grep -o '/\S*-debug.log' npm-install-output)
+        if [[ $MATCH != "" ]]; then
+            echo ===== BEGIN NPM LOG =====
+            cat $MATCH
+            echo ===== END NPM LOG =====
+        fi
 
-# install npm/node
-./.evergreen/install-node.sh
-
-source ./.evergreen/env.sh
-
-# Install tool for merging different junit reports into a single one
-set +o errexit
-set -o pipefail
-
-npm install -g junit-report-merger --cache $(mktemp -d) 2>&1 | tee npm-install-output
-RESULT=$?
-MATCH=$(grep -o '/\S*-debug.log' npm-install-output)
-if [[ $MATCH != "" ]]; then
-    echo ===== BEGIN NPM LOG =====
-    cat $MATCH
-    echo ===== END NPM LOG =====
-fi
-
-exit $RESULT
+        set -o errexit
+        if [ $RESULT -ne 0 ]; then
+            exit $RESULT
+        fi
+    else
+        echo Missing/unknown install option: "$arg"
+        exit 1
+    fi
+done
