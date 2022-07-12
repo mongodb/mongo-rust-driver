e88913b1cd24be519ffe3a184b2a307cc2504811
sanav33 <sanyaverma0303@gmail.com>
2022-07-12 16:39:46 -0400

    convert generic tfileid to bson

--- src/gridfs.rs
+++ src/gridfs.rs
@@ -1,3 +1,5 @@
+pub mod options;
+
 use core::task::{Context, Poll};
 use std::{
     io::{self, Result},
@@ -8,104 +10,19 @@ use std::{
 use crate::{
     concern::{ReadConcern, WriteConcern},
     cursor::Cursor,
-    selection_criteria::{ReadPreference, SelectionCriteria},
+    selection_criteria::SelectionCriteria,
     Database,
 };
 
-use bson::{oid::ObjectId, DateTime, Document};
-use serde::Deserialize;
-use typed_builder::TypedBuilder;
+use options::*;
 
+use bson::{oid::ObjectId, DateTime, Document, Bson};
 use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
 
-/// Contains the options for creating a [`GridFsBucket`].
-#[derive(Clone, Debug, Default, Deserialize, TypedBuilder)]
-#[builder(field_defaults(setter(into)))]
-#[non_exhaustive]
-pub struct GridFsBucketOptions {
-    /// The bucket name. Defaults to 'fs'.
-    pub bucket_name: Option<String>,
-
-    /// The chunk size in bytes. Defaults to 255 KiB.
-    pub chunk_size_bytes: Option<i32>,
-
-    /// The write concern. Defaults to the write concern of the database.
-    pub write_concern: Option<WriteConcern>,
-
-    /// The read concern. Defaults to the read concern of the database.
-    pub read_concern: Option<ReadConcern>,
-
-    /// The read preference. Defaults to the read preference of the database.
-    pub read_preference: Option<ReadPreference>,
-}
-
-/// Contains the options for creating a [`GridFsStream`] to upload a file to a
-/// [`GridFsBucket`].
-#[derive(Clone, Debug, Default, Deserialize, TypedBuilder)]
-#[builder(field_defaults(setter(into)))]
-#[non_exhaustive]
-pub struct GridFsUploadOptions {
-    /// The number of bytes per chunk of this file. Defaults to the chunk_size_bytes in the
-    /// GridFsBucketOptions.
-    pub chunk_size_bytes: Option<i32>,
-
-    /// User data for the 'metadata' field of the files collection document.
-    pub metadata: Option<Document>,
-}
-
-/// Contains the options for creating [`GridFsStream`] to retrieve a stored file
-/// from a [`GridFsBucket`].
-#[derive(Clone, Debug, Default, Deserialize, TypedBuilder)]
-#[builder(field_defaults(setter(into)))]
-#[non_exhaustive]
-pub struct GridFsDownloadByNameOptions {
-    /// Which revision (documents with the same filename and different `upload_date`)
-    /// of the file to retrieve. Defaults to -1 (the most recent revision).
-    ///
-    /// Revision numbers are defined as follows:
-    /// 0 = the original stored file
-    /// 1 = the first revision
-    /// 2 = the second revision
-    /// etc...
-    /// -2 = the second most recent revision
-    /// -1 = the most recent revision
-    pub revision: Option<i32>,
-}
-
-/// Contains the options for performing a find operation on a files collection.  
-#[derive(Clone, Debug, Default, Deserialize, TypedBuilder)]
-#[builder(field_defaults(setter(into)))]
-#[non_exhaustive]
-pub struct GridFsFindOptions {
-    /// Enables writing to temporary files on the server. When set to true, the
-    /// server can write temporary data to disk while executing the find operation
-    /// on the files collection.
-    pub allow_disk_use: Option<bool>,
-
-    /// The number of documents to return per batch.
-    pub batch_size: Option<i32>,
-
-    /// The maximum number of documents to return.
-    pub limit: Option<i32>,
-
-    /// The maximum amount of time to allow the query to run.
-    pub max_time_ms: Option<i64>,
-
-    /// The server normally times out idle cursors after an inactivity period
-    /// (10 minutes) to prevent excess memory use. Set this option to prevent that.
-    pub no_cursor_timeout: Option<bool>,
-
-    /// The number of documents to skip before returning.
-    pub skip: i32,
-
-    /// The order by which to sort results. Defaults to not sorting.
-    pub sort: Option<Document>,
-}
-
 // Contained in a "chunks" collection for each user file
-struct Chunk<T: Eq + Copy> {
+struct Chunk {
     id: ObjectId,
-    files_id: T,
+    files_id: Bson,
     n: i32,
     // default size is 255 KiB
     data: Vec<u8>,
@@ -113,8 +30,8 @@ struct Chunk<T: Eq + Copy> {
 
 // A collection in which information about stored files is stored. There will be one files
 // collection document per stored file.
-struct FilesCollectionDocument<T: Eq + PartialEq + Copy> {
-    id: T,
+pub struct FilesCollectionDocument {
+    id: Bson,
     length: i64,
     chunk_size: i32,
     upload_date: DateTime,
@@ -129,7 +46,9 @@ pub struct GridFsBucket {
     pub(crate) options: Option<GridFsBucketOptions>,
 }
 
+// TODO: RUST-1399 Add documentation and example code for this struct.
 pub struct GridFsStream {
+    pub id: Bson,
     _pin: PhantomPinned,
 }
 
@@ -179,46 +98,64 @@ impl GridFsBucket {
     }
 
     /// Gets the read preference of the [`GridFsBucket`].
-    pub fn read_preference(&self) -> Option<&ReadPreference> {
+    pub fn read_preference(&self) -> Option<&SelectionCriteria> {
         if let Some(options) = &self.options {
-            if let Some(ref rp) = options.read_preference {
-                return Some(rp);
+            if options.read_preference.is_some() {
+                return options.read_preference.as_ref();
             }
         }
-        if let Some(SelectionCriteria::ReadPreference(ref rp)) = self.db.selection_criteria() {
-            Some(rp)
-        } else {
-            None
-        }
+        self.db.selection_criteria()
     }
 
     /// Opens a [`GridFsStream`] that the application can write the contents of the file to.
     /// The application provides a custom file id.
     ///
     /// Returns a [`GridFsStream`] to which the application will write the contents.
-    pub fn open_upload_stream_with_id<T>(
+    pub fn open_upload_stream_with_id(
         &self,
-        id: T,
+        id: Bson,
         filename: String,
-        options: GridFsUploadOptions,
+        options: impl Into<GridFsUploadOptions>,
     ) -> GridFsStream {
         todo!()
     }
 
-    /// Uploads a user file to a GridFS bucket. The application supplies a custom file id.
-    pub fn upload_from_stream_with_id<T>(
+    /// Opens a [`GridFsStream`] that the application can write the contents of the file to.
+    /// The driver generates a unique [`Bson::ObjectId`] for the file id.
+    ///
+    /// Returns a [`GridFsStream`] to which the application will write the contents.
+    pub fn open_upload_stream(
         &self,
-        id: T,
+        filename: String,
+        options: impl Into<GridFsUploadOptions>,
+    ) -> GridFsStream {
+        self.open_upload_stream_with_id(Bson::ObjectId(ObjectId::new()), filename, options)
+    }
+
+    /// Uploads a user file to a GridFS bucket. The application supplies a custom file id.
+    pub fn upload_from_stream_with_id(
+        &self,
+        id: Bson,
         filename: String,
         source: GridFsStream,
-        option: GridFsUploadOptions,
+        options: impl Into<GridFsUploadOptions>,
     ) {
         todo!()
     }
 
+    /// Uploads a user file to a GridFS bucket. The driver generates a unique [`Bson::ObjectId`] for the file id.
+    pub fn upload_from_stream(
+        &self,
+        filename: String,
+        source: GridFsStream,
+        options: impl Into<GridFsUploadOptions>,
+    ) {
+        self.upload_from_stream_with_id(Bson::ObjectId(ObjectId::new()), filename, source, options)
+    }
+
     /// Opens and returns a [`GridFsStream`] from which the application can read
     /// the contents of the stored file specified by `id`.
-    pub fn open_download_stream<T>(&self, id: T) -> GridFsStream {
+    pub fn open_download_stream(&self, id: Bson) -> GridFsStream {
         todo!()
     }
 
@@ -228,14 +165,14 @@ impl GridFsBucket {
     pub fn open_download_stream_by_name(
         &self,
         filename: String,
-        options: GridFsDownloadByNameOptions,
+        options: impl Into<GridFsDownloadByNameOptions>,
     ) -> GridFsStream {
         todo!()
     }
 
     /// Downloads the contents of the stored file specified by `id` and writes
     /// the contents to the destination [`GridFsStream`].
-    pub fn download_to_stream<T>(&self, id: T, destination: GridFsStream) {
+    pub fn download_to_stream<T>(&self, id: Bson, destination: GridFsStream) {
         todo!()
     }
 
@@ -246,24 +183,24 @@ impl GridFsBucket {
         &self,
         filename: String,
         destination: GridFsStream,
-        options: GridFsDownloadByNameOptions,
+        options: impl Into<GridFsDownloadByNameOptions>,
     ) {
         todo!()
     }
 
     /// Given an `id`, deletes the stored file's files collection document and
     /// associated chunks from a [`GridFsBucket`].
-    pub fn delete<T>(&self, id: T) {
+    pub fn delete(&self, id: Bson) {
         todo!()
     }
 
     /// Finds and returns the files collection documents that match the filter.
-    pub fn find<T>(&self, filter: Document, options: GridFsBucketOptions) -> Result<Cursor<T>> {
+    pub fn find(&self, filter: Document, options: impl Into<GridFsBucketOptions>) -> Result<Cursor<FilesCollectionDocument>> {
         todo!()
     }
 
     /// Renames the stored file with the specified `id`.
-    pub fn rename<T>(&self, id: T, new_filename: String) {
+    pub fn rename(&self, id: Bson, new_filename: String) {
         todo!()
     }
 
