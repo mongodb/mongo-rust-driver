f425deefbff61f5c33b20ee14d63027bf25b65e5
Kaitlin Mahar <kaitlinmahar@gmail.com>
2022-07-18 11:57:24 -0400

    update TODO ticket number

--- src/test/spec/change_streams.rs
+++ src/test/spec/change_streams.rs
@@ -7,7 +7,7 @@ use super::{run_unified_format_test_filtered, unified_runner::TestCase};
 async fn run() {
     let _guard = LOCK.run_exclusively().await;
     let client = TestClient::new().await;
-    // TODO DRIVERS-2387: remove this skip.
+    // TODO RUST-1412: remove this skip.
     let is_gte_6_1 = client.server_version_gte(6, 1);
     let pred = |tc: &TestCase| {
         !(is_gte_6_1 && tc.description == "change stream resumes after StaleShardVersion")
