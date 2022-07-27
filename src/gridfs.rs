62bf4fb1097868a94b76a7ed115be1b341eb7448
sanav33 <sanyaverma0303@gmail.com>
2022-07-27 14:36:15 -0400

    add abort method for upload stream

--- src/gridfs.rs
+++ src/gridfs.rs
@@ -213,6 +213,11 @@ impl GridFsUploadStream {
     pub fn finish(self) {
         todo!()
     }
+
+    /// Aborts the upload and discards the upload stream.
+    pub fn abort(self) {
+        todo!()
+    }
 }
 
 <<<<<<< HEAD
