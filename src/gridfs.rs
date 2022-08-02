81672d4140424b97b5f01d99ff0f0d2617906ef2
sanav33 <sanyaverma0303@gmail.com>
2022-08-02 17:14:32 -0400

    change helpers for bucket

--- src/gridfs.rs
+++ src/gridfs.rs
@@ -112,28 +112,18 @@ impl tokio::io::AsyncRead for GridFsDownloadStream {
 impl GridFsBucket {
     /// Gets the read concern of the [`GridFsBucket`].
     pub fn read_concern(&self) -> Option<&ReadConcern> {
-        if self.options.read_concern.is_some() {
-            self.options.read_concern.as_ref()
-        } else {
-            self.db.read_concern()
-        }
+        self.options.read_concern.as_ref()
     }
 
     /// Gets the write concern of the [`GridFsBucket`].
     pub fn write_concern(&self) -> Option<&WriteConcern> {
-        if self.options.write_concern.is_some() {
-            self.options.write_concern.as_ref()
-        } else {
-            self.db.write_concern()
-        }    }
+        self.options.write_concern.as_ref()
+    }
 
     /// Gets the selection criteria of the [`GridFsBucket`].
     pub fn selection_criteria(&self) -> Option<&SelectionCriteria> {
-        if self.options.selection_criteria.is_some() {
-            self.options.selection_criteria.as_ref()
-        } else {
-            self.db.selection_criteria()
-        }    }
+        self.options.selection_criteria.as_ref()
+    }
 
     /// Opens a [`GridFsUploadStream`] that the application can write the contents of the file to.
     /// The application provides a custom file id.
