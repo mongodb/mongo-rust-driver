mongodb/mongo-rust-driver

commit 223cee358826af7d3aa8fdea0c21f020ba2768a9

RUST-360 Streaming monitoring protocol (#721)
This also fixes RUST-1464

src/cmap/establish/mod.rs
@@ -1,96 +1,141 @@
 pub(super) mod handshake;
-#[cfg(test)]
-mod test;
 
-use self::handshake::Handshaker;
+use self::handshake::{Handshaker, HandshakerOptions};
 use super::{
-    conn::{ConnectionGeneration, PendingConnection},
-    options::ConnectionPoolOptions,
+    conn::{ConnectionGeneration, LoadBalancedGeneration, PendingConnection},
     Connection,
     PoolGeneration,
 };
 use crate::{
-    client::{auth::Credential, options::ServerApi},
-    error::{Error as MongoError, ErrorKind},
-    runtime::HttpClient,
+    client::{
+        auth::Credential,
+        options::{ClientOptions, ServerAddress, TlsOptions},
+    },
+    error::{Error as MongoError, ErrorKind, Result},
+    hello::HelloReply,
+    runtime::{AsyncStream, HttpClient, TlsConfig},
     sdam::HandshakePhase,
 };
 
 /// Contains the logic to establish a connection, including handshaking, authenticating, and
 /// potentially more.
-#[derive(Clone, Debug)]
-pub(super) struct ConnectionEstablisher {
+#[derive(Clone)]
+pub(crate) struct ConnectionEstablisher {
     /// Contains the logic for handshaking a connection.
     handshaker: Handshaker,
-    http_client: HttpClient,
-    credential: Option<Credential>,
-    server_api: Option<ServerApi>,
+
+    /// Cached configuration needed to create TLS connections, if needed.
+    tls_config: Option<TlsConfig>,
+}
+
+pub(crate) struct EstablisherOptions {
+    handshake_options: HandshakerOptions,
+    tls_options: Option<TlsOptions>,
+}
+
+impl EstablisherOptions {
+    pub(crate) fn from_client_options(opts: &ClientOptions) -> Self {
+        Self {
+            handshake_options: HandshakerOptions {
+                app_name: opts.app_name.clone(),
+                compressors: opts.compressors.clone(),
+                driver_info: opts.driver_info.clone(),
+                server_api: opts.server_api.clone(),
+                load_balanced: opts.load_balanced.unwrap_or(false),
+            },
+            tls_options: opts.tls_options(),
+        }
+    }
 }
 
 impl ConnectionEstablisher {
     /// Creates a new ConnectionEstablisher from the given options.
-    pub(super) fn new(http_client: HttpClient, options: Option<&ConnectionPoolOptions>) -> Self {
-        let handshaker = Handshaker::new(options.cloned().map(Into::into));
+    pub(crate) fn new(http_client: HttpClient, options: EstablisherOptions) -> Result<Self> {
+        let handshaker = Handshaker::new(http_client, options.handshake_options);
 
-        Self {
+        let tls_config = if let Some(tls_options) = options.tls_options {
+            Some(TlsConfig::new(tls_options)?)
+        } else {
+            None
+        };
+
+        Ok(Self {
             handshaker,
-            http_client,
-            credential: options.and_then(|options| options.credential.clone()),
-            server_api: options.and_then(|options| options.server_api.clone()),
-        }
+            tls_config,
+        })
+    }
+
+    async fn make_stream(&self, address: ServerAddress) -> Result<AsyncStream> {
+        AsyncStream::connect(address, self.tls_config.as_ref()).await
     }
 
     /// Establishes a connection.
     pub(super) async fn establish_connection(
         &self,
         pending_connection: PendingConnection,
+        credential: Option<&Credential>,
     ) -> std::result::Result<Connection, EstablishError> {
         let pool_gen = pending_connection.generation.clone();
-        let mut connection = Connection::connect(pending_connection)
+        let address = pending_connection.address.clone();
+
+        let stream = self
+            .make_stream(address)
             .await
             .map_err(|e| EstablishError::pre_hello(e, pool_gen.clone()))?;
 
-        let handshake = self
-            .handshaker
-            .handshake(&mut connection, None, &None)
-            .await
-            .map_err(|e| EstablishError::pre_hello(e, pool_gen.clone()))?;
-        let service_id = handshake.hello_reply.command_response.service_id;
+        let mut connection = Connection::new_pooled(pending_connection, stream);
+        let handshake_result = self.handshaker.handshake(&mut connection, credential).await;
 
         // If the handshake response had a `serviceId` field, this is a connection to a load
         // balancer and must derive its generation from the service_generations map.
-        match (pool_gen, service_id) {
+        match (&pool_gen, connection.service_id()) {
             (PoolGeneration::Normal(_), _) => {}
             (PoolGeneration::LoadBalanced(gen_map), Some(service_id)) => {
-                connection.generation = ConnectionGeneration::LoadBalanced {
+                connection.generation = LoadBalancedGeneration {
                     generation: *gen_map.get(&service_id).unwrap_or(&0),
                     service_id,
-                };
+                }
+                .into();
             }
-            _ => {
-                return Err(EstablishError::post_hello(
-                    ErrorKind::Internal {
-                        message: "load-balanced mode mismatch".to_string(),
-                    }
-                    .into(),
-                    connection.generation.clone(),
-                ));
+            (PoolGeneration::LoadBalanced(_), None) => {
+                // If the handshake succeeded and there isn't a service id, return a special error.
+                // If the handshake failed, just return the error from that instead.
+                if handshake_result.is_ok() {
+                    return Err(EstablishError::post_hello(
+                        ErrorKind::IncompatibleServer {
+                            message: "Driver attempted to initialize in load balancing mode, but \
+                                      the server does not support this mode."
+                                .to_string(),
+                        }
+                        .into(),
+                        connection.generation,
+                    ));
+                }
             }
         }
 
-        if let Some(ref credential) = self.credential {
-            credential
-                .authenticate_stream(
-                    &mut connection,
-                    &self.http_client,
-                    self.server_api.as_ref(),
-                    handshake.first_round,
-                )
-                .await
-                .map_err(|e| EstablishError::post_hello(e, connection.generation.clone()))?
-        }
+        handshake_result.map_err(|e| {
+            if connection.stream_description.is_none() {
+                EstablishError::pre_hello(e, pool_gen)
+            } else {
+                EstablishError::post_hello(e, connection.generation)
+            }
+        })?;
 
         Ok(connection)
+    }
+
+    /// Establishes a monitoring connection.
+    pub(crate) async fn establish_monitoring_connection(
+        &self,
+        address: ServerAddress,
+    ) -> Result<(Connection, HelloReply)> {
+        let stream = self.make_stream(address.clone()).await?;
+        let mut connection = Connection::new_monitoring(address, stream);
+
+        let hello_reply = self.handshaker.handshake(&mut connection, None).await?;
+
+        Ok((connection, hello_reply))
     }
 }
 

src/runtime/mod.rs
@@ -6,7 +6,7 @@ mod join_handle;
 #[cfg(feature = "csfle")]
 mod process;
 mod resolver;
-mod stream;
+pub(crate) mod stream;
 mod sync_read_ext;
 #[cfg(feature = "openssl-tls")]
 mod tls_openssl;
@@ -36,6 +36,8 @@ use tls_openssl as tls;
 use tls_rustls as tls;
 #[cfg(feature = "tokio-runtime")]
 use tokio::time::Interval;
+
+pub(crate) use tls::TlsConfig;
 
 /// Spawn a task in the background to run a future.
 ///

src/client/mod.rs
@@ -409,12 +409,12 @@ impl Client {
             if let Some(server) = server_selection::attempt_to_select_server(
                 criteria,
                 &state.description,
-                &state.servers,
+                &state.servers(),
             )? {
                 return Ok(server);
             }
 
-            self.inner.topology.request_update();
+            watcher.request_immediate_check();
 
             let change_occurred = start_time.elapsed() < timeout
                 && watcher
@@ -422,9 +422,8 @@ impl Client {
                     .await;
             if !change_occurred {
                 return Err(ErrorKind::ServerSelection {
-                    message: self
-                        .inner
-                        .topology
+                    message: state
+                        .description
                         .server_selection_timeout_error_message(criteria),
                 }
                 .into());
@@ -433,12 +432,13 @@ impl Client {
     }
 
     #[cfg(all(test, not(feature = "sync"), not(feature = "tokio-sync")))]
-    pub(crate) async fn get_hosts(&self) -> Vec<String> {
+    pub(crate) fn get_hosts(&self) -> Vec<String> {
         let watcher = self.inner.topology.watch();
         let state = watcher.peek_latest();
 
-        let servers = state.servers.keys();
-        servers
+        state
+            .servers()
+            .keys()
             .map(|stream_address| format!("{}", stream_address))
             .collect()
     }
@@ -458,6 +458,11 @@ impl Client {
             .clone()
     }
 
+    #[cfg(test)]
+    pub(crate) fn topology(&self) -> &crate::sdam::Topology {
+        &self.inner.topology
+    }
+
     #[cfg(feature = "csfle")]
     pub(crate) fn weak(&self) -> WeakClient {
         WeakClient {
@@ -474,11 +479,6 @@ impl Client {
             csfle.as_ref().map(|cs| cs.opts())
         })
         .ok()
-    }
-
-    #[cfg(test)]
-    pub(crate) fn topology(&self) -> &Topology {
-        &self.inner.topology
     }
 }
 

src/client/options/mod.rs
@@ -33,7 +33,7 @@ use crate::{
     error::{Error, ErrorKind, Result},
     event::{cmap::CmapEventHandler, command::CommandEventHandler, sdam::SdamEventHandler},
     options::ReadConcernLevel,
-    sdam::{DEFAULT_HEARTBEAT_FREQUENCY, IDLE_WRITE_PERIOD, MIN_HEARTBEAT_FREQUENCY},
+    sdam::{verify_max_staleness, DEFAULT_HEARTBEAT_FREQUENCY, MIN_HEARTBEAT_FREQUENCY},
     selection_criteria::{ReadPreference, SelectionCriteria, TagSet},
     srv::{OriginalSrvInfo, SrvResolver},
 };
@@ -568,7 +568,7 @@ pub struct ClientOptions {
 #[derive(Debug, Clone, Default)]
 pub(crate) struct TestOptions {
     /// Override MIN_HEARTBEAT_FREQUENCY.
-    pub(crate) heartbeat_freq: Option<Duration>,
+    pub(crate) min_heartbeat_freq: Option<Duration>,
 
     /// Disable server and SRV-polling monitor threads.
     pub(crate) disable_monitoring_threads: bool,
@@ -1318,17 +1318,10 @@ impl ClientOptions {
 
         if let Some(SelectionCriteria::ReadPreference(ref rp)) = self.selection_criteria {
             if let Some(max_staleness) = rp.max_staleness() {
-                let smallest_max_staleness = std::cmp::max(
-                    Duration::from_secs(90),
-                    self.heartbeat_freq.unwrap_or(DEFAULT_HEARTBEAT_FREQUENCY) + IDLE_WRITE_PERIOD,
-                );
-
-                if max_staleness < smallest_max_staleness {
-                    return Err(Error::invalid_argument(format!(
-                        "invalid maxStaleness value: must be at least {} seconds",
-                        smallest_max_staleness.as_secs()
-                    )));
-                }
+                verify_max_staleness(
+                    max_staleness,
+                    self.heartbeat_freq.unwrap_or(DEFAULT_HEARTBEAT_FREQUENCY),
+                )?;
             }
         }
 

src/cmap/conn/mod.rs
@@ -9,16 +9,16 @@ use std::{
 
 use derivative::Derivative;
 use serde::Serialize;
-use tokio::sync::{mpsc, Mutex};
+use tokio::{
+    io::BufStream,
+    sync::{mpsc, Mutex},
+};
 
-use self::wire::Message;
+use self::wire::{Message, MessageFlags};
 use super::manager::PoolManager;
 use crate::{
     bson::oid::ObjectId,
-    cmap::{
-        options::{ConnectionOptions, StreamOptions},
-        PoolGeneration,
-    },
+    cmap::PoolGeneration,
     compression::Compressor,
     error::{load_balanced_mode_mismatch, Error, ErrorKind, Result},
     event::cmap::{
@@ -30,7 +30,7 @@ use crate::{
         ConnectionCreatedEvent,
         ConnectionReadyEvent,
     },
-    options::{ServerAddress, TlsOptions},
+    options::ServerAddress,
     runtime::AsyncStream,
 };
 pub(crate) use command::{Command, RawCommand, RawCommandResponse};
@@ -86,7 +86,13 @@ pub(crate) struct Connection {
     /// into a pool.
     error: bool,
 
-    stream: AsyncStream,
+    /// Whether the most recently received message included the moreToCome flag, indicating the
+    /// server may send more responses without any additional requests. Attempting to send new
+    /// messages on this connection while this value is true will return an error. This value
+    /// will remain true until a server response does not include the moreToComeFlag.
+    more_to_come: bool,
+
+    stream: BufStream<AsyncStream>,
 
     /// Compressor that the client will use before sending messages.
     /// This compressor does not get used to decompress server messages.
@@ -105,81 +111,53 @@ pub(crate) struct Connection {
 }
 
 impl Connection {
-    async fn new(
-        id: u32,
+    fn new(
         address: ServerAddress,
-        generation: u32,
-        options: Option<ConnectionOptions>,
-    ) -> Result<Self> {
-        let stream_options = StreamOptions {
-            address: address.clone(),
-            connect_timeout: options.as_ref().and_then(|opts| opts.connect_timeout),
-            tls_options: options.as_ref().and_then(|opts| opts.tls_options.clone()),
-        };
-
-        let conn = Self {
+        stream: AsyncStream,
+        id: u32,
+        generation: ConnectionGeneration,
+    ) -> Self {
+        Self {
             id,
             server_id: None,
-            generation: ConnectionGeneration::Normal(generation),
+            generation,
             pool_manager: None,
             command_executing: false,
             ready_and_available_time: None,
-            stream: AsyncStream::connect(stream_options).await?,
+            stream: BufStream::new(stream),
             address,
-            handler: options.and_then(|options| options.event_handler),
+            handler: None,
             stream_description: None,
             error: false,
             pinned_sender: None,
             compressor: None,
-        };
-
-        Ok(conn)
+            more_to_come: false,
+        }
     }
 
-    /// Constructs and connects a new connection.
-    pub(super) async fn connect(pending_connection: PendingConnection) -> Result<Self> {
+    /// Create a connection intended to be stored in a connection pool for operation execution.
+    /// TODO: RUST-1454 Remove this from `Connection`, instead wrap a `Connection` type in a
+    /// separate type specific to pool.
+    pub(crate) fn new_pooled(pending_connection: PendingConnection, stream: AsyncStream) -> Self {
         let generation = match pending_connection.generation {
-            PoolGeneration::Normal(gen) => gen,
-            PoolGeneration::LoadBalanced(_) => 0, /* Placeholder; will be overwritten in
-                                                   * `ConnectionEstablisher::
-                                                   * establish_connection`. */
+            PoolGeneration::Normal(gen) => ConnectionGeneration::Normal(gen),
+            PoolGeneration::LoadBalanced(_) => ConnectionGeneration::LoadBalanced(None),
         };
-        Self::new(
+        let mut conn = Self::new(
+            pending_connection.address,
+            stream,
             pending_connection.id,
-            pending_connection.address.clone(),
             generation,
-            pending_connection.options,
-        )
-        .await
+        );
+        conn.handler = pending_connection.event_handler;
+        conn
     }
 
-    /// Construct and connect a new connection used for monitoring.
-    pub(crate) async fn connect_monitoring(
-        address: ServerAddress,
-        connect_timeout: Option<Duration>,
-        tls_options: Option<TlsOptions>,
-    ) -> Result<Self> {
-        Self::new(
-            0,
-            address,
-            0,
-            Some(ConnectionOptions {
-                connect_timeout,
-                tls_options,
-                event_handler: None,
-            }),
-        )
-        .await
-    }
-
-    #[cfg(test)]
-    pub(crate) async fn new_testing(
-        id: u32,
-        address: ServerAddress,
-        generation: u32,
-        options: Option<ConnectionOptions>,
-    ) -> Result<Self> {
-        Self::new(id, address, generation, options).await
+    /// Create a connection intended for monitoring purposes.
+    /// TODO: RUST-1454 Rename this to just `new`, drop the pooling-specific data.
+    pub(crate) fn new_monitoring(address: ServerAddress, stream: AsyncStream) -> Self {
+        // Monitoring connections don't have IDs, so just use 0 as a placeholder here.
+        Self::new(address, stream, 0, ConnectionGeneration::Monitoring)
     }
 
     pub(crate) fn info(&self) -> ConnectionInfo {
@@ -191,7 +169,9 @@ impl Connection {
     }
 
     pub(crate) fn service_id(&self) -> Option<ObjectId> {
-        self.generation.service_id()
+        self.stream_description
+            .as_ref()
+            .and_then(|sd| sd.service_id)
     }
 
     pub(crate) fn address(&self) -> &ServerAddress {
@@ -272,6 +252,13 @@ impl Connection {
         message: Message,
         to_compress: bool,
     ) -> Result<RawCommandResponse> {
+        if self.more_to_come {
+            return Err(Error::internal(format!(
+                "attempted to send a new message to {} but moreToCome bit was set",
+                self.address()
+            )));
+        }
+
         self.command_executing = true;
 
         // If the client has agreed on a compressor with the server, and the command
@@ -298,7 +285,10 @@ impl Connection {
         self.command_executing = false;
         self.error = response_message_result.is_err();
 
-        RawCommandResponse::new(self.address.clone(), response_message_result?)
+        let response_message = response_message_result?;
+        self.more_to_come = response_message.flags.contains(MessageFlags::MORE_TO_COME);
+
+        RawCommandResponse::new(self.address.clone(), response_message)
     }
 
     /// Executes a `Command` and returns a `CommandResponse` containing the result from the server.
@@ -330,6 +320,34 @@ impl Connection {
         let to_compress = command.should_compress();
         let message = Message::with_raw_command(command, request_id.into());
         self.send_message(message, to_compress).await
+    }
+
+    /// Receive the next message from the connection.
+    /// This will return an error if the previous response on this connection did not include the
+    /// moreToCome flag.
+    pub(crate) async fn receive_message(&mut self) -> Result<RawCommandResponse> {
+        if !self.more_to_come {
+            return Err(Error::internal(format!(
+                "attempted to stream response from connection to {} but moreToCome bit was not set",
+                self.address()
+            )));
+        }
+
+        self.command_executing = true;
+        let response_message_result = Message::read_from(
+            &mut self.stream,
+            self.stream_description
+                .as_ref()
+                .map(|d| d.max_message_size_bytes),
+        )
+        .await;
+        self.command_executing = false;
+        self.error = response_message_result.is_err();
+
+        let response_message = response_message_result?;
+        self.more_to_come = response_message.flags.contains(MessageFlags::MORE_TO_COME);
+
+        RawCommandResponse::new(self.address.clone(), response_message)
     }
 
     /// Gets the connection's StreamDescription.
@@ -384,8 +402,8 @@ impl Connection {
             id: self.id,
             server_id: self.server_id,
             address: self.address.clone(),
-            generation: self.generation.clone(),
-            stream: std::mem::replace(&mut self.stream, AsyncStream::Null),
+            generation: self.generation,
+            stream: std::mem::replace(&mut self.stream, BufStream::new(AsyncStream::Null)),
             handler: self.handler.take(),
             stream_description: self.stream_description.take(),
             command_executing: self.command_executing,
@@ -394,7 +412,14 @@ impl Connection {
             ready_and_available_time: None,
             pinned_sender: self.pinned_sender.clone(),
             compressor: self.compressor.clone(),
+            more_to_come: false,
         }
+    }
+
+    /// Whether or not the previous command response indicated that the server may send
+    /// more responses without another request.
+    pub(crate) fn is_streaming(&self) -> bool {
+        self.more_to_come
     }
 }
 
@@ -490,35 +515,50 @@ impl PinnedConnectionHandle {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
+pub(crate) struct LoadBalancedGeneration {
+    pub(crate) generation: u32,
+    pub(crate) service_id: ObjectId,
+}
+
+/// TODO: RUST-1454 Once we have separate types for pooled and non-pooled connections, the
+/// monitoring case and the Option<> wrapper can be dropped from this.
+#[derive(Debug, Clone, Copy)]
 pub(crate) enum ConnectionGeneration {
+    Monitoring,
     Normal(u32),
-    LoadBalanced {
-        generation: u32,
-        service_id: ObjectId,
-    },
+    LoadBalanced(Option<LoadBalancedGeneration>),
 }
 
 impl ConnectionGeneration {
-    pub(crate) fn service_id(&self) -> Option<ObjectId> {
+    pub(crate) fn service_id(self) -> Option<ObjectId> {
         match self {
-            ConnectionGeneration::Normal(_) => None,
-            ConnectionGeneration::LoadBalanced { service_id, .. } => Some(*service_id),
+            ConnectionGeneration::LoadBalanced(Some(gen)) => Some(gen.service_id),
+            _ => None,
         }
     }
 
-    pub(crate) fn is_stale(&self, current_generation: &PoolGeneration) -> bool {
+    pub(crate) fn is_stale(self, current_generation: &PoolGeneration) -> bool {
         match (self, current_generation) {
-            (ConnectionGeneration::Normal(cgen), PoolGeneration::Normal(pgen)) => cgen != pgen,
-            (
-                ConnectionGeneration::LoadBalanced {
-                    generation: cgen,
-                    service_id,
-                },
-                PoolGeneration::LoadBalanced(gen_map),
-            ) => cgen != gen_map.get(service_id).unwrap_or(&0),
+            (ConnectionGeneration::Normal(cgen), PoolGeneration::Normal(pgen)) => cgen != *pgen,
+            (ConnectionGeneration::LoadBalanced(cgen), PoolGeneration::LoadBalanced(gen_map)) => {
+                if let Some(cgen) = cgen {
+                    cgen.generation != *gen_map.get(&cgen.service_id).unwrap_or(&0)
+                } else {
+                    // In the event that an error occurred during handshake and no serviceId was
+                    // returned, just ignore the error for SDAM purposes, since
+                    // we won't know which serviceId to clear for.
+                    false
+                }
+            }
             _ => load_balanced_mode_mismatch!(false),
         }
+    }
+}
+
+impl From<LoadBalancedGeneration> for ConnectionGeneration {
+    fn from(gen: LoadBalancedGeneration) -> Self {
+        ConnectionGeneration::LoadBalanced(Some(gen))
     }
 }
 
@@ -527,12 +567,11 @@ impl ConnectionGeneration {
 /// Creating a `PendingConnection` contributes towards the total connection count of a pool, despite
 /// not actually making a TCP connection to the pool's endpoint. This models a "pending" Connection
 /// from the CMAP specification.
-#[derive(Debug)]
-pub(super) struct PendingConnection {
-    pub(super) id: u32,
-    pub(super) address: ServerAddress,
-    pub(super) generation: PoolGeneration,
-    pub(super) options: Option<ConnectionOptions>,
+pub(crate) struct PendingConnection {
+    pub(crate) id: u32,
+    pub(crate) address: ServerAddress,
+    pub(crate) generation: PoolGeneration,
+    pub(crate) event_handler: Option<Arc<dyn CmapEventHandler>>,
 }
 
 impl PendingConnection {

src/cmap/conn/wire/mod.rs
@@ -2,4 +2,7 @@ mod header;
 mod message;
 mod util;
 
-pub(crate) use self::{message::Message, util::next_request_id};
+pub(crate) use self::{
+    message::{Message, MessageFlags},
+    util::next_request_id,
+};

commit 5fb9b5210737b2b6e8bf88f1480bfdfdac9dc6ad

RUST-360 Guard connection establishment by connectTimeoutMS (#743)
This fixes a regression that was introduced in #721.

src/cmap/establish/mod.rs
@@ -1,5 +1,7 @@
 pub(super) mod handshake;
 
+use std::time::Duration;
+
 use self::handshake::{Handshaker, HandshakerOptions};
 use super::{
     conn::{ConnectionGeneration, LoadBalancedGeneration, PendingConnection},
@@ -13,7 +15,7 @@ use crate::{
     },
     error::{Error as MongoError, ErrorKind, Result},
     hello::HelloReply,
-    runtime::{AsyncStream, HttpClient, TlsConfig},
+    runtime::{self, stream::DEFAULT_CONNECT_TIMEOUT, AsyncStream, HttpClient, TlsConfig},
     sdam::HandshakePhase,
 };
 
@@ -26,11 +28,14 @@ pub(crate) struct ConnectionEstablisher {
 
     /// Cached configuration needed to create TLS connections, if needed.
     tls_config: Option<TlsConfig>,
+
+    connect_timeout: Duration,
 }
 
 pub(crate) struct EstablisherOptions {
     handshake_options: HandshakerOptions,
     tls_options: Option<TlsOptions>,
+    connect_timeout: Option<Duration>,
 }
 
 impl EstablisherOptions {
@@ -44,6 +49,7 @@ impl EstablisherOptions {
                 load_balanced: opts.load_balanced.unwrap_or(false),
             },
             tls_options: opts.tls_options(),
+            connect_timeout: opts.connect_timeout,
         }
     }
 }
@@ -59,14 +65,25 @@ impl ConnectionEstablisher {
             None
         };
 
+        let connect_timeout = match options.connect_timeout {
+            Some(d) if d.is_zero() => Duration::MAX,
+            Some(d) => d,
+            None => DEFAULT_CONNECT_TIMEOUT,
+        };
+
         Ok(Self {
             handshaker,
             tls_config,
+            connect_timeout,
         })
     }
 
     async fn make_stream(&self, address: ServerAddress) -> Result<AsyncStream> {
-        AsyncStream::connect(address, self.tls_config.as_ref()).await
+        runtime::timeout(
+            self.connect_timeout,
+            AsyncStream::connect(address, self.tls_config.as_ref()),
+        )
+        .await?
     }
 
     /// Establishes a connection.

src/runtime/mod.rs
@@ -109,7 +109,13 @@ pub(crate) async fn delay_for(delay: Duration) {
 
     #[cfg(feature = "async-std-runtime")]
     {
-        async_std::task::sleep(delay).await
+        // This avoids a panic in async-std when the provided duration is too large.
+        // See: https://github.com/async-rs/async-std/issues/1037.
+        if delay == Duration::MAX {
+            std::future::pending().await
+        } else {
+            async_std::task::sleep(delay).await
+        }
     }
 }
 
@@ -124,9 +130,15 @@ pub(crate) async fn timeout<F: Future>(timeout: Duration, future: F) -> Result<F
 
     #[cfg(feature = "async-std-runtime")]
     {
-        async_std::future::timeout(timeout, future)
-            .await
-            .map_err(|_| std::io::ErrorKind::TimedOut.into())
+        // This avoids a panic on async-std when the provided duration is too large.
+        // See: https://github.com/async-rs/async-std/issues/1037.
+        if timeout == Duration::MAX {
+            Ok(future.await)
+        } else {
+            async_std::future::timeout(timeout, future)
+                .await
+                .map_err(|_| std::io::ErrorKind::TimedOut.into())
+        }
     }
 }
 

src/sdam/monitor.rs
@@ -1,5 +1,4 @@
 use std::{
-    future,
     sync::Arc,
     time::{Duration, Instant},
 };
@@ -185,7 +184,7 @@ impl Monitor {
         });
 
         let heartbeat_frequency = self.heartbeat_frequency();
-        let timeout = if self.connect_timeout().as_millis() == 0 {
+        let timeout = if self.connect_timeout().is_zero() {
             // If connectTimeoutMS = 0, then the socket timeout for monitoring is unlimited.
             Duration::MAX
         } else if self.topology_version.is_some() {
@@ -197,16 +196,6 @@ impl Monitor {
         } else {
             // Otherwise, just use connectTimeoutMS.
             self.connect_timeout()
-        };
-        let timeout_future = async {
-            // If timeout is infinite, don't bother creating a delay future.
-            // This also avoids a panic on async-std when the provided duration is too large.
-            // See: https://github.com/async-rs/async-std/issues/1037.
-            if timeout == Duration::MAX {
-                future::pending().await
-            } else {
-                runtime::delay_for(timeout).await
-            }
         };
 
         let execute_hello = async {
@@ -268,7 +257,7 @@ impl Monitor {
                 };
                 HelloResult::Cancelled { reason: reason_error }
             }
-            _ = timeout_future => {
+            _ = runtime::delay_for(timeout) => {
                 HelloResult::Err(Error::network_timeout())
             }
         };