c3528080b57e67ec7a3cd228550bfbca573b733e
Abraham Egnor <abraham.egnor@mongodb.com>
2022-10-04 14:45:38 -0400

    more evergreen noodling

---
+++ .evergreen/find-crypt_shared.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ "$1" = "" ]; then
+    echo "crypt_shared library directory required"
+    exit 1
+fi
+
+crypt_shared_glob = ("$1"/*)
+
+if [ "${#crypt_shared_glob[@]}" != "1" ]; then
+    echo "Wrong number of files found: ${crypt_shared_glob[@]}"
+    exit 1
+fi
+
+echo ${crypt_shared_glob[0]}
