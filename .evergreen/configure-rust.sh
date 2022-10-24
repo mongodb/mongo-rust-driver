923e5b60a10653b8eab6575b93cf14b70c4e7e8d
Abraham Egnor <abraham.egnor@mongodb.com>
2022-10-24 11:41:18 -0400

    closer to working on windws; start on disabling crypt_shared

--- .evergreen/configure-rust.sh
+++ .evergreen/configure-rust.sh
@@ -9,6 +9,7 @@ if [[ "Windows_NT" == "$OS" ]]; then
     # rustup/cargo need the native Windows paths; $PROJECT_DIRECTORY is a cygwin path
     export RUSTUP_HOME=$(cygpath ${RUSTUP_HOME} --windows)
     export CARGO_HOME=$(cygpath ${CARGO_HOME} --windows)
+    export MONGOCRYPT_LIB_DIR=$(cygpath ${MONGOCRYPT_LIB_DIR} --windows)
 fi
 
 . ${CARGO_HOME}/env
--- .evergreen/run-csfle-kmip-servers.sh
+++ .evergreen/run-csfle-kmip-servers.sh
@@ -5,7 +5,7 @@ cd ${DRIVERS_TOOLS}/.evergreen/csfle
 # TMPDIR is required to avoid "AF_UNIX path too long" errors.
 export TMPDIR="$(dirname ${DRIVERS_TOOLS})"
 
-kmstlsvenv/bin/python kms_kmip_server.py &
-kmstlsvenv/bin/python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/expired.pem --port 9000 &
-kmstlsvenv/bin/python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/wrong-host.pem --port 9001 &
-kmstlsvenv/bin/python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/server.pem --port 9002 --require_client_cert
+python kms_kmip_server.py &
+python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/expired.pem --port 9000 &
+python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/wrong-host.pem --port 9001 &
+python -u kms_http_server.py --ca_file ../x509gen/ca.pem --cert_file ../x509gen/server.pem --port 9002 --require_client_cert
