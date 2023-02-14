19cde38a6ed39c428db957b6c902f662fad8229d
Abraham Egnor <abraham.egnor@mongodb.com>
2023-02-14 10:50:12 -0500

    RUST-1567 Sync CLAM write concern error test

--- src/test/spec/command_monitoring/mod.rs
+++ src/test/spec/command_monitoring/mod.rs
@@ -2,7 +2,7 @@ use crate::test::LOCK;
 
 use super::{run_spec_test_with_path, run_unified_format_test_filtered, unified_runner::TestCase};
 
-#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "tokio-runtime", tokio::test(flavor = "multi_thread"))]
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
 async fn command_monitoring_unified() {
     let _guard = LOCK.run_exclusively().await;
