f714db34c3be40869d02024f200bd37dc7ade068
Abraham Egnor <abraham.egnor@mongodb.com>
2023-05-12 13:10:31 -0400

    working remote rust binary

--- .evergreen/run-azure-kms-test.sh
+++ .evergreen/run-azure-kms-test.sh
@@ -9,11 +9,18 @@ set -o xtrace
 
 AZUREKMS_TOOLS=$DRIVERS_TOOLS/.evergreen/csfle/azurekms/
 
+mkdir azurekms_remote
+cp -r $MONGOCRYPT_LIB_DIR azurekms_remote
+
 pushd .evergreen/azure-kms-test
 cargo build
-AZUREKMS_SRC=target/debug/azure-kms-test \
+popd
+cp .evergreen/azure-kms-test/target/debug/azure-kms-test azurekms_remote
+
+tar czf azurekms_remote.tgz azurekms_remote
+AZUREKMS_SRC=azurekms_remote.tgz \
     AZUREKMS_DST="." \
     $AZUREKMS_TOOLS/copy-file.sh
-popd
-
-AZUREKMS_CMD=azure-kms-test $AZUREKMS_TOOLS/run-command.sh
+AZUREKMS_CMD="tar xvf azurekms_remote.tgz" $AZUREKMS_TOOLS/run-command.sh
+AZUREKMS_CMD="LD_LIBRARY_PATH=./azurekms_remote/lib ./azurekms_remote/azure-kms-test" \
+    $AZUREKMS_TOOLS/run-command.sh
