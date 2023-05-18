b353c9e045a4b789cd71ad842cd8442ee082874f
Isabel Atkinson <isabel.atkinson@mongodb.com>
2023-05-18 12:51:28 -0600

    change streams

--- src/test/spec/change_streams.rs
+++ src/test/spec/change_streams.rs
@@ -4,5 +4,12 @@ use crate::test::{spec::unified_runner::run_unified_tests, LOCK};
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
 async fn run_unified() {
     let _guard = LOCK.run_exclusively().await;
-    run_unified_tests(&["change-streams", "unified"]).await;
+    run_unified_tests(&["change-streams", "unified"])
+        .skip_files(&[
+            // TODO RUST-1281: unskip this file
+            "change-streams-showExpandedEvents.json",
+            // TODO RUST-1423: unskip this file
+            "change-streams-disambiguatedPaths.json",
+        ])
+        .await;
 }
