419b9fafb5991868782baf0d1c55de7332ac7ba5
drshika <67125579+drshika@users.noreply.github.com>
2023-06-13 16:55:41 -0400

    RUST-1571 docs

--- src/cmap/conn/mod.rs
+++ src/cmap/conn/mod.rs
@@ -46,13 +46,14 @@ pub struct ConnectionInfo {
     pub id: u32,
 
     /// A server-generated identifier that uniquely identifies the connection. Available on server
-    /// versions 4.2+. This may be used to correlate driver connections with server logs. 
-    /// If the connection ID sent by the server is too large for an i32, this will be a truncated value.
+    /// versions 4.2+. This may be used to correlate driver connections with server logs.
+    /// If the connection ID sent by the server is too large for an i32, this will be a truncated
+    /// value.
     pub server_id: Option<i32>,
 
     /// A server-generated identifier that uniquely identifies the connection. Available on server
-    /// versions 4.2+. This may be used to correlate driver connections with server logs. This value
-    /// will not be truncated and should be used rather than `server_id`.
+    /// versions 4.2+. This may be used to correlate driver connections with server logs. This
+    /// value will not be truncated and should be used rather than `server_id`.
     pub server_id_i64: Option<i64>,
 
     /// The address that the connection is connected to.
