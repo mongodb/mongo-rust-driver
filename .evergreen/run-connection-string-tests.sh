f43edeb42226f2c9080d83bc297c3c23b1212564
Abraham Egnor <abraham.egnor@mongodb.com>
2023-06-15 16:26:57 -0400

    remove debugging

--- .evergreen/run-connection-string-tests.sh
+++ .evergreen/run-connection-string-tests.sh
@@ -11,13 +11,10 @@ FEATURE_FLAGS+=("aws-auth")
 
 set +o errexit
 
-echo spec
 cargo_test spec::auth > spec.xml
-echo options
 cargo_test uri_options > uri_options.xml
-echo string
 cargo_test connection_string > connection_string.xml
-echo merge
+
 junit-report-merger results.xml spec.xml uri_options.xml connection_string.xml
-echo done ${CARGO_RESULT}
+
 exit ${CARGO_RESULT}
