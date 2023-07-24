f80ba7aa9eca65e06cc429b432108d4ac2854e32
Abraham Egnor <abraham.egnor@mongodb.com>
2023-07-24 11:13:56 -0400

    remove async_resource

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,6 +1,4 @@
 mod acknowledged_message;
-#[allow(unused)]
-pub(crate) mod async_resource;
 #[cfg(feature = "reqwest")]
 mod http;
 #[cfg(feature = "async-std-runtime")]
@@ -84,13 +82,14 @@ where
     spawn(fut);
 }
 
+#[cfg(any(test, feature = "sync", feature = "tokio-sync"))]
 pub(crate) fn block_on<F, T>(fut: F) -> T
 where
     F: Future<Output = T>,
 {
     #[cfg(all(feature = "tokio-runtime", not(feature = "tokio-sync")))]
     {
-        tokio::task::block_in_place(|| futures_executor::block_on(fut))
+        tokio::task::block_in_place(|| futures::executor::block_on(fut))
     }
 
     #[cfg(feature = "tokio-sync")]
