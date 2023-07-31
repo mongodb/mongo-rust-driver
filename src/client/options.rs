e8f37f31f2db0f6fc9edbe661d3962c0fd02cfbd
Luis Osta <luisosta1999@gmail.com>
2023-07-31 11:15:59 -0500

    added maxConnecting support

--- src/client/options.rs
+++ src/client/options.rs
@@ -1297,6 +1297,18 @@ impl ClientOptions {
                 };
             }
         }
+
+        let max_connecting = if let Some(mc) = conn_str.max_connecting {
+            // maxConnecting must be greater than 0
+            if mc == 0 {
+                None
+            } else {
+                Some(mc)
+            }
+        } else {
+            None
+        };
+
         Self {
             hosts: vec![],
             app_name: conn_str.app_name,
@@ -1310,7 +1322,7 @@ impl ClientOptions {
             max_pool_size: conn_str.max_pool_size,
             min_pool_size: conn_str.min_pool_size,
             max_idle_time: conn_str.max_idle_time,
-            max_connecting: conn_str.max_connecting,
+            max_connecting: max_connecting,
             server_selection_timeout: conn_str.server_selection_timeout,
             compressors: conn_str.compressors,
             connect_timeout: conn_str.connect_timeout,
