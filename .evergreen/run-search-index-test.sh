5740236bf7dde8cc0a3a301c91ecdebdf945bd1e
Abraham Egnor <abraham.egnor@mongodb.com>
2023-11-14 15:16:57 -0500

    execution of tests

--- .evergreen/run-search-index-test.sh
+++ .evergreen/run-search-index-test.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "TODO"
+set -o errexit
+set -o pipefail
+
+source ./.evergreen/env.sh
+source .evergreen/cargo-test.sh
+
+set -o xtrace
+
+unset INDEX_MANAGEMENT_TEST_UNIFIED
+export INDEX_MANAGEMENT_TEST_PROSE=1
+
+set +o errexit
+
+cargo_test test::spec::index_management results.xml
+
+exit ${CARGO_RESULT}
