50c1006f7d8430aff1e3ac55aed47e414dcbb764
Abraham Egnor <abraham.egnor@mongodb.com>
2024-01-09 11:54:31 -0500

    flesh out handler options

--- src/client/event.rs
+++ src/client/event.rs
@@ -1,15 +1,25 @@
 use std::sync::Arc;
 
+use futures_core::future::BoxFuture;
+
 use crate::event::command::CommandEvent;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 #[non_exhaustive]
 pub enum EventHandler<T> {
+    Callback(Arc<dyn Fn(T) + Sync + Send>),
+    AsyncCallback(Arc<dyn Fn(T) -> BoxFuture<'static, ()> + Sync + Send>),
     TokioMpsc(tokio::sync::mpsc::Sender<T>),
     Legacy(LegacyEventHandler<T>),
 }
 
-impl<T: Send + Sync> From<tokio::sync::mpsc::Sender<T>> for EventHandler<T> {
+impl<T> std::fmt::Debug for EventHandler<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("EventHandler").finish()
+    }
+}
+
+impl<T> From<tokio::sync::mpsc::Sender<T>> for EventHandler<T> {
     fn from(value: tokio::sync::mpsc::Sender<T>) -> Self {
         Self::TokioMpsc(value)
     }
@@ -27,8 +37,19 @@ impl<T: crate::event::command::CommandEventHandler + 'static> From<Arc<T>> for E
 }
 
 impl<T> EventHandler<T> {
+    pub fn new_callback(f: impl Fn(T) -> BoxFuture<'static, ()> + Send + Sync + 'static) -> Self {
+        Self::AsyncCallback(Arc::new(f))
+    }
+
+    pub fn new_sync_callback(f: impl Fn(T) + Send + Sync + 'static) -> Self {
+        Self::Callback(Arc::new(f))
+    }
+
     pub(crate) async fn handle(&self, event: T) {
         match self {
+            // TODO RUST-1731 Use tokio's spawn_blocking
+            Self::Callback(cb) => (cb)(event),
+            Self::AsyncCallback(cb) => (cb)(event).await,
             Self::TokioMpsc(sender) => {
                 let _ = sender.send(event).await;
             }
@@ -39,9 +60,3 @@ impl<T> EventHandler<T> {
 
 #[derive(Clone)]
 pub struct LegacyEventHandler<T>(Arc<dyn Fn(T) + Send + Sync>);
-
-impl<T> std::fmt::Debug for LegacyEventHandler<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("LegacyEventHandler").finish()
-    }
-}
