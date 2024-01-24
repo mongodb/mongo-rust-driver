b31103545227b308d0a6587cd3374aaf653002ce
Isabel Atkinson <isabel.atkinson@mongodb.com>
2024-01-24 15:16:08 -0700

    RUST-1525 Update AWS tests to use new scripts

--- .evergreen/run-aws-tests.sh
+++ .evergreen/run-aws-tests.sh
@@ -3,43 +3,14 @@
 set -o xtrace
 set -o errexit # Exit the script with error if any of the commands fail
 
-############################################
-#            Main Program                  #
-############################################
-
-# Supported/used environment variables:
-#  MONGODB_URI    Set the URI, including an optional username/password to use
-#                 to connect to the server via MONGODB-AWS authentication
-#                 mechanism.
-
 echo "Running MONGODB-AWS authentication tests"
-# ensure no secrets are printed in log files
-set +x
 
-# load the script
-shopt -s expand_aliases # needed for `urlencode` alias
-[ -s "${PROJECT_DIRECTORY}/prepare_mongodb_aws.sh" ] && source "${PROJECT_DIRECTORY}/prepare_mongodb_aws.sh"
-
-MONGODB_URI=${MONGODB_URI:-"mongodb://localhost"}
-MONGODB_URI="${MONGODB_URI}/aws?authMechanism=MONGODB-AWS"
-if [[ -n ${SESSION_TOKEN} ]]; then
-  MONGODB_URI="${MONGODB_URI}&authMechanismProperties=AWS_SESSION_TOKEN:${SESSION_TOKEN}"
-fi
-
-export MONGODB_URI="$MONGODB_URI"
-
-if [ "$ASSERT_NO_URI_CREDS" = "true" ]; then
-  if echo "$MONGODB_URI" | grep -q "@"; then
-    echo "MONGODB_URI unexpectedly contains user credentials!"
-    exit 1
-  fi
-fi
-
-# show test output
-set -x
+cd $DRIVERS_TOOLS/.evergreen/auth_aws
+. aws_setup.sh $AWS_AUTH_TYPE
 
 set -o errexit
 
+cd ${PROJECT_DIRECTORY}
 source .evergreen/env.sh
 source .evergreen/cargo-test.sh
 
