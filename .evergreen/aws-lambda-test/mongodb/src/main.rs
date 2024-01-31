cada0de5f46d122cbfe39dc05e0e52fb546098e0
Abraham Egnor <abraham.egnor@mongodb.com>
2024-01-31 11:59:06 -0500

    mongodb connection

--- .evergreen/aws-lambda-test/mongodb/src/main.rs
+++ .evergreen/aws-lambda-test/mongodb/src/main.rs
@@ -1,50 +1,45 @@
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-
+use mongodb::Client;
 use serde::{Deserialize, Serialize};
+use tokio::sync::OnceCell;
 
-/// This is a made-up example. Requests come into the runtime as unicode
-/// strings in json format, which can map to any structure that implements `serde::Deserialize`
-/// The runtime pays no attention to the contents of the request payload.
+static MONGODB_CLIENT: OnceCell<Client> = OnceCell::const_new();
+
+async fn get_mongodb_client() -> &'static Client {
+    MONGODB_CLIENT
+        .get_or_init(|| async {
+            let uri = std::env::var("MONGODB_URI")
+                .expect("MONGODB_URI must be set to the URI of the MongoDB deployment");
+            Client::with_uri_str(uri)
+                .await
+                .expect("Failed to create MongoDB Client")
+        })
+        .await
+}
+
 #[derive(Deserialize)]
 struct Request {
 }
 
-/// This is a made-up example of what a response structure may look like.
-/// There is no restriction on what it can be. The runtime requires responses
-/// to be serialized into json. The runtime pays no attention
-/// to the contents of the response payload.
 #[derive(Serialize)]
 struct Response {
-    #[serde(rename = "camelCase")]
+    #[serde(rename = "statusCode")]
     status_code: i32,
     body: String,
 }
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
 async fn function_handler(_event: LambdaEvent<Request>) -> Result<Response, Error> {
-    // Prepare the response
+    let client = get_mongodb_client().await;
+    let names = client.list_database_names(None, None).await?;
     let resp = Response {
         status_code: 200,
-        body: "Hello World!".to_string(),
+        body: format!("{:?}", names),
     };
 
-    // Return `Response` (it will be serialized to JSON automatically by the runtime)
     Ok(resp)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
-        .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
-        .without_time()
-        .init();
-
     run(service_fn(function_handler)).await
 }
