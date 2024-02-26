058a50cae05981bd9c5cc45f4f983094c40b1b20
Abraham Egnor <abraham.egnor@mongodb.com>
2024-02-26 15:14:28 -0500

    convert create_encrypted_collection

--- src/action/csfle.rs
+++ src/action/csfle.rs
@@ -1,7 +1,9 @@
 //! Action builders for in-use encryption.
 
 mod create_data_key;
+mod create_encrypted_collection;
 pub(crate) mod encrypt;
 
 pub use create_data_key::{CreateDataKey, DataKeyOptions};
+pub use create_encrypted_collection::CreateEncryptedCollection;
 pub use encrypt::{Encrypt, EncryptOptions};
