67d4519a54c35664ae5369fa5dbd59cc46d87d8c
Abraham Egnor <abraham.egnor@mongodb.com>
2024-02-27 14:23:34 -0500

    remove runtime benchmark axis

--- .evergreen/run-driver-benchmarks.sh
+++ .evergreen/run-driver-benchmarks.sh
@@ -4,13 +4,9 @@ set -o errexit
 
 source ./.evergreen/env.sh
 
-FEATURES=""
-
 cd benchmarks
 cargo run \
   --release \
-  --no-default-features \
-  --features ${FEATURES} \
   -- --output="../benchmark-results.json" --driver
 
 cat ../benchmark-results.json
