c6e98e0267609e20f3738d74b1be7b3467dfde29
Patrick Meredith <pmeredit@gmail.com>
2024-03-11 12:56:02 -0400

    RUST-1663: Remove unnecessary top level function by cloning twice

--- src/test/spec/oidc.rs
+++ src/test/spec/oidc.rs
@@ -8,16 +8,6 @@ use crate::{
 };
 use std::sync::{Arc, Mutex};
 
-// simple_cb is a simple callback that increments the passed atomic integer and returns a static IdpServerResponse
-async fn simple_cb(call_count: Arc<Mutex<u32>>) -> crate::error::Result<oidc::IdpServerResponse> {
-    *call_count.lock().unwrap() += 1;
-    Ok(oidc::IdpServerResponse {
-        access_token: tokio::fs::read_to_string("/tmp/tokens/test_user1").await?,
-        expires: None,
-        refresh_token: None,
-    })
-}
-
 // Machine Callback tests
 // Prose test 1.1 Single Principal Implicit Username
 #[tokio::test]
@@ -38,7 +28,16 @@ async fn machine_single_principal_implicit_username() -> anyhow::Result<()> {
     opts.credential = Credential::builder()
         .mechanism(AuthMechanism::MongoDbOidc)
         .oidc_callback(oidc::Callback::machine(move |_| {
-            { simple_cb(cb_call_count.clone()) }.boxed()
+            let call_count = cb_call_count.clone();
+            async move {
+                *call_count.lock().unwrap() += 1;
+                Ok(oidc::IdpServerResponse {
+                    access_token: tokio::fs::read_to_string("/tmp/tokens/test_user1").await?,
+                    expires: None,
+                    refresh_token: None,
+                })
+            }
+            .boxed()
         }))
         .build()
         .into();
@@ -80,7 +79,16 @@ async fn human_single_principal_implicit_username() -> anyhow::Result<()> {
     opts.credential = Credential::builder()
         .mechanism(AuthMechanism::MongoDbOidc)
         .oidc_callback(oidc::Callback::human(move |_| {
-            { simple_cb(cb_call_count.clone()) }.boxed()
+            let call_count = cb_call_count.clone();
+            async move {
+                *call_count.lock().unwrap() += 1;
+                Ok(oidc::IdpServerResponse {
+                    access_token: tokio::fs::read_to_string("/tmp/tokens/test_user1").await?,
+                    expires: None,
+                    refresh_token: None,
+                })
+            }
+            .boxed()
         }))
         .build()
         .into();
