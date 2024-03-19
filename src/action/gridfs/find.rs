9b0648d30a23518b80c1e56fe48186607062b766
Abraham Egnor <abraham.egnor@mongodb.com>
2024-03-19 14:20:09 -0400

    convert find_one

--- src/action/gridfs/find.rs
+++ src/action/gridfs/find.rs
@@ -2,8 +2,8 @@ use std::time::Duration;
 
 use bson::Document;
 
-use crate::coll::options::FindOptions;
-use crate::gridfs::{FilesCollectionDocument, GridFsFindOptions};
+use crate::coll::options::{FindOneOptions, FindOptions};
+use crate::gridfs::{FilesCollectionDocument, GridFsFindOneOptions, GridFsFindOptions};
 use crate::{Cursor, GridFsBucket};
 use crate::action::{action_impl, option_setters};
 use crate::error::Result;
@@ -16,6 +16,14 @@ impl GridFsBucket {
     pub fn find_2(&self, filter: Document) -> Find {
         Find { bucket: self, filter, options: None }
     }
+
+    /// Finds and returns a single [`FilesCollectionDocument`] within this bucket that matches the
+    /// given filter.
+    /// 
+    /// `await` will return `Result<Option<FilesCollectionDocument>>`.
+    pub fn find_one_2(&self, filter: Document) -> FindOne {
+        FindOne { bucket: self, filter, options: None }
+    }
 }
 
 #[cfg(feature = "sync")]
@@ -27,8 +35,18 @@ impl crate::sync::gridfs::GridFsBucket {
     pub fn find_2(&self, filter: Document) -> Find {
         self.async_bucket.find_2(filter)
     }
+
+    /// Finds and returns a single [`FilesCollectionDocument`] within this bucket that matches the
+    /// given filter.
+    /// 
+    /// [`run`](FindOne::run) will return `Result<Option<FilesCollectionDocument>>`.
+    pub fn find_one_2(&self, filter: Document) -> FindOne {
+        self.async_bucket.find_one_2(filter)
+    }
 }
 
+/// Finds and returns the [`FilesCollectionDocument`]s within a bucket that match a given
+/// filter.  Construct with [`GridFsBucket::find`].
 #[must_use]
 pub struct Find<'a> {
     bucket: &'a GridFsBucket,
@@ -58,6 +76,38 @@ action_impl! {
 
         fn sync_wrap(out) -> Result<crate::sync::Cursor<FilesCollectionDocument>> {
             out.map(crate::sync::Cursor::new)
+        }
+    }
+}
+
+/// Finds and returns a single [`FilesCollectionDocument`] within a bucket that matches a
+/// given filter.  Construct with [`GridFsBucket::find_one`].
+#[must_use]
+pub struct FindOne<'a> {
+    bucket: &'a GridFsBucket,
+    filter: Document,
+    options: Option<GridFsFindOneOptions>,
+}
+
+impl<'a> FindOne<'a> {
+    option_setters! { options: GridFsFindOneOptions;
+        max_time: Duration,
+        skip: u64,
+        sort: Document,
+    }
+}
+
+action_impl! {
+    impl<'a> Action for FindOne<'a> {
+        type Future = FindOneFuture;
+
+        async fn execute(self) -> Result<Option<FilesCollectionDocument>> {
+            let find_options = self.options.map(FindOneOptions::from);
+            self.bucket
+                .files()
+                .find_one(self.filter)
+                .with_options(find_options)
+                .await
         }
     }
 }
