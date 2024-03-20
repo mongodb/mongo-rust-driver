e1f88642f6ae1e6a7f82031ec3d00e0261f12a73
Abraham Egnor <abraham.egnor@mongodb.com>
2024-03-20 11:07:54 -0400

    convert drop

--- src/action/gridfs.rs
+++ src/action/gridfs.rs
@@ -1,9 +1,11 @@
 //! Action builders for gridfs.
 
 mod delete;
+mod drop;
 mod find;
 mod rename;
 
 pub use delete::Delete;
+pub use drop::Drop;
 pub use find::{Find, FindOne};
 pub use rename::Rename;
