c0a45c57875a8dc3e29d19864b1a40bf83060212
Abraham Egnor <abraham.egnor@mongodb.com>
2024-04-16 13:47:10 -0400

    RUST-1775 Remove manual in favor of a link to the docs page

--- manual/deps/src/example.rs
+++
@@ -1,6 +0,0 @@
-pub mod automatic_queryable_encryption;
-pub mod explicit_encryption;
-pub mod explicit_encryption_auto_decryption;
-pub mod explicit_queryable_encryption;
-pub mod local_rules;
-pub mod server_side_enforcement;
