41194ab2652553c643255b6cbad97a94f0f8ef36
Isabel Atkinson <isabel.atkinson@mongodb.com>
2024-04-17 09:55:41 -0600

    don't use multiple mongoses

--- src/test/bulk_write.rs
+++ src/test/bulk_write.rs
@@ -270,8 +270,12 @@ async fn successful_cursor_iteration() {
 
 #[tokio::test(flavor = "multi_thread")]
 async fn failed_cursor_iteration() {
+    let mut options = get_client_options().await.clone();
+    options.hosts.drain(1..);
+
     let event_buffer = EventBuffer::new();
     let client = Client::test_builder()
+        .options(options)
         .event_buffer(event_buffer.clone())
         .build()
         .await;
