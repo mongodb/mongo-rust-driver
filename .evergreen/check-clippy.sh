6169c7e5e588b79d2e47b155f6bceebcf8e4f15d
Abraham Egnor <abraham.egnor@mongodb.com>
2024-04-25 11:11:17 -0400

    RUST-1920 Use semgrep to generate SARIF reports

--- .evergreen/check-clippy.sh
+++ .evergreen/check-clippy.sh
@@ -8,13 +8,9 @@ source ./.evergreen/env.sh
 CLIPPY_VERSION=1.75.0
 
 rustup install $CLIPPY_VERSION
-cargo install clippy-sarif
 
 # Check with default features.
 cargo +$CLIPPY_VERSION clippy --all-targets -p mongodb -- -D warnings
 
 # Check with all features.
 cargo +$CLIPPY_VERSION clippy --all-targets --all-features -p mongodb -- -D warnings
-
-# Produce a SARIF report.
-cargo +$CLIPPY_VERSION clippy --all-targets --all-features -p mongodb --message-format=json -- -D warnings | clippy-sarif > clippy.sarif.json
