e19ce9ed973ab993d23405660fa0549759d0399d
Patrick Meredith <pmeredit@gmail.com>
2024-04-25 22:09:43 -0400

    RUST-1562: Do not check file that will not work with the azure VM

--- .evergreen/run-mongodb-oidc-azure-test.sh
+++
@@ -1,8 +0,0 @@
-git add .
-git commit -m "add files"
-export AZUREOIDC_DRIVERS_TAR_FILE=/tmp/mongo-rust-driver.tgz
-git archive -o $AZUREOIDC_DRIVERS_TAR_FILE HEAD
-export AZUREOIDC_TEST_CMD="PROJECT_DIRECTORY='.' ./.evergreen/install-dependencies.sh rust\
-                           && PROJECT_DIRECTORY='.' .evergreen/install-dependencies.sh junit-dependencies\
-                           && PROJECT_DIRECTORY='.' OIDC_ENV=azure OIDC=oidc  ./.evergreen/run-mongodb-oidc-test.sh"
-bash $DRIVERS_TOOLS/.evergreen/auth_oidc/azure/run-driver-test.sh
