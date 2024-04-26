b06660c4f04e172d73cef3a4127e486358578752
Patrick Meredith <pmeredit@protonmail.com>
2024-04-26 15:37:21 -0400

    RUST-1562: Automatic token acquisition for Azure Identity Provider (#1087)

--- .evergreen/run-mongodb-oidc-test.sh
+++ .evergreen/run-mongodb-oidc-test.sh
@@ -10,6 +10,10 @@ echo "Running MONGODB-OIDC authentication tests"
 
 OIDC_ENV=${OIDC_ENV:-"test"}
 
+export TEST_AUTH_OIDC=1
+export COVERAGE=1
+export AUTH="auth"
+
 if [ $OIDC_ENV == "test" ]; then
     # Make sure DRIVERS_TOOLS is set.
     if [ -z "$DRIVERS_TOOLS" ]; then
@@ -18,20 +22,17 @@ if [ $OIDC_ENV == "test" ]; then
     fi
     source ${DRIVERS_TOOLS}/.evergreen/auth_oidc/secrets-export.sh
 
+    cargo nextest run test::spec::oidc::basic --no-capture --profile ci
+    RESULT=$?
 elif [ $OIDC_ENV == "azure" ]; then
     source ./env.sh
 
+    cargo nextest run test::spec::oidc::azure --no-capture --profile ci --features=azure-oidc
+    RESULT=$?
 else
     echo "Unrecognized OIDC_ENV $OIDC_ENV"
     exit 1
 fi
 
-export TEST_AUTH_OIDC=1
-export COVERAGE=1
-export AUTH="auth"
-export OIDC="oidc"
-
-cargo nextest run test::spec::oidc --profile ci
-RESULT=$?
 cp target/nextest/ci/junit.xml results.xml
 exit $RESULT
