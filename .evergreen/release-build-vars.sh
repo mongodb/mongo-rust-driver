c502b3c6d19962687e4ea888d7d646e7c3ff7b54
Abraham Egnor <abraham.egnor@mongodb.com>
2024-05-03 14:19:35 -0400

    RUST-1921 Sign crate on release

---
+++ .evergreen/release-build-vars.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+source ./.evergreen/env.sh
+
+set +x
+
+CRATE_VERSION=$(cargo metadata --format-version=1 --no-deps | jq --raw-output '.packages[0].version')
+
+. ${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh drivers/rust
+rm secrets-export.sh
+
+PAPERTRAIL_PRODUCT="rust-driver"
+if [[ "${DRY_RUN}" == "yes" ]]; then
+  PAPERTRAIL_PRODUCT="rust-driver-testing"
+fi
+
+cat <<EOT >release-expansion.yml
+CRATE_VERSION: "${CRATE_VERSION}"
+PAPERTRAIL_KEY_ID: "${PAPERTRAIL_KEY_ID}"
+PAPERTRAIL_SECRET_KEY: "${PAPERTRAIL_SECRET_KEY}"
+PAPERTRAIL_PRODUCT: "${PAPERTRAIL_PRODUCT}"
+ARTIFACTORY_USERNAME: "${ARTIFACTORY_USERNAME}"
+ARTIFACTORY_PASSWORD: "${ARTIFACTORY_PASSWORD}"
+GARASIGN_USERNAME: "${GARASIGN_USERNAME}"
+GARASIGN_PASSWORD: "${GARASIGN_PASSWORD}"
+EOT
---
+++ .evergreen/release-sign.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set +x
+
+echo "${ARTIFACTORY_PASSWORD}" | docker login --password-stdin --username ${ARTIFACTORY_USERNAME} artifactory.corp.mongodb.com
+
+echo "GRS_CONFIG_USER1_USERNAME=${GARASIGN_USERNAME}" >> "signing-envfile"
+echo "GRS_CONFIG_USER1_PASSWORD=${GARASIGN_PASSWORD}" >> "signing-envfile"
+
+docker run \
+  --env-file=signing-envfile \
+  --rm \
+  -v $(pwd):$(pwd) \
+  -w $(pwd) \
+  artifactory.corp.mongodb.com/release-tools-container-registry-local/garasign-gpg \
+  /bin/bash -c "gpgloader && gpg --yes -v --armor -o mongodb-${CRATE_VERSION}.sig --detach-sign target/package/mongodb-${CRATE_VERSION}.crate"
