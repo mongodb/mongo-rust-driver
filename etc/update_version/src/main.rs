7e0e9de60a003700b29bc459ca32bfab6ce9476b
Abraham Egnor <abraham.egnor@mongodb.com>
2024-05-21 13:55:44 -0400

    update version script

--- etc/update_version/src/main.rs
+++ etc/update_version/src/main.rs
@@ -74,33 +74,43 @@ struct Args {
 }
 
 fn main() {
+    let zero = std::env::current_exe().unwrap();
+    let self_dir = zero.parent().unwrap();
+    let main_dir = self_dir.join("../../../..");
+    std::env::set_current_dir(main_dir).unwrap();
+
     let version_locs = vec![
         Location::new(
-            "../../Cargo.toml",
+            "Cargo.toml",
             r#"name = "mongodb"\nversion = "(?<target>.*?)"\n"#,
         ),
-        Location::new("../../README.md", r#"mongodb = "(?<target>.*?)"\n"#),
         Location::new(
-            "../../README.md",
+            "Cargo.toml",
+            r#"mongodb-internal-macros = (?<target>\{ path = .* \})\n"#,
+        ),
+        Location::new(
+            "macros/Cargo.toml",
+            r#"name = "mongodb-internal-macros"\nversion = "(?<target>.*?)"\n"#,
+        ),
+        Location::new("README.md", r#"mongodb = "(?<target>.*?)"\n"#),
+        Location::new(
+            "README.md",
             r#"\[dependencies.mongodb\]\nversion = "(?<target>.*?)"\n"#,
         ),
-        Location::new("../../src/lib.rs", r#"//! mongodb = "(?<target>.*?)"\n"#),
-        Location::new("../../src/lib.rs", r#"//! version = "(?<target>.*?)"\n"#),
+        Location::new("src/lib.rs", r#"//! mongodb = "(?<target>.*?)"\n"#),
+        Location::new("src/lib.rs", r#"//! version = "(?<target>.*?)"\n"#),
         Location::new(
-            "../../src/lib.rs",
+            "src/lib.rs",
             r#"html_root_url = "https://docs.rs/mongodb/(?<target>.*?)""#,
         ),
         Location::new(
-            "../../manual/src/installation_features.md",
+            "manual/src/installation_features.md",
             r#"\[dependencies.mongodb\]\nversion = "(?<target>.*?)"\n"#,
         ),
     ];
-    let bson_version_loc =
-        Location::new("../../Cargo.toml", r#"bson = (?<target>\{ git = .*? \})\n"#);
-    let mongocrypt_version_loc = Location::new(
-        "../../Cargo.toml",
-        r#"mongocrypt = (?<target>\{ git = .*? \})\n"#,
-    );
+    let bson_version_loc = Location::new("Cargo.toml", r#"bson = (?<target>\{ git = .*? \})\n"#);
+    let mongocrypt_version_loc =
+        Location::new("Cargo.toml", r#"mongocrypt = (?<target>\{ git = .*? \})\n"#);
 
     let args: Args = argh::from_env();
 
@@ -112,7 +122,10 @@ fn main() {
         pending.apply(&bson_version_loc, &format!("{:?}", bson));
     }
     if let Some(mongocrypt) = args.mongocrypt {
-        pending.apply(&mongocrypt_version_loc, &format!("{{ version = {:?}, optional = true }}", mongocrypt));
+        pending.apply(
+            &mongocrypt_version_loc,
+            &format!("{{ version = {:?}, optional = true }}", mongocrypt),
+        );
     }
     pending.write();
 }
