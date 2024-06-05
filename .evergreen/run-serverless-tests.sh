98f6404112b827b37d2bedf6f05d06a8cf5d18c9
Abraham Egnor <abraham.egnor@mongodb.com>
2024-06-05 16:14:55 -0400

    RUST-1678 Switch junit merge tools (#1120)

--- .evergreen/run-serverless-tests.sh
+++ .evergreen/run-serverless-tests.sh
@@ -21,6 +21,6 @@ cargo_test test::cursor cursor.xml
 cargo_test test::spec::collection_management coll.xml
 cargo_test test::spec::command_monitoring_unified monitoring.xml
 
-junit-report-merger results.xml crud.xml retryable_reads.xml retryable_writes.xml versioned_api.xml sessions.xml transactions.xml load_balancers.xml cursor.xml coll.xml monitoring.xml
+merge-junit -o results.xml crud.xml retryable_reads.xml retryable_writes.xml versioned_api.xml sessions.xml transactions.xml load_balancers.xml cursor.xml coll.xml monitoring.xml
 
 exit $CARGO_RESULT
