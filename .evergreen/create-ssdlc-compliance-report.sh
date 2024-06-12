90d788af2763d869b12f889eff8e681ecf7d1cec
Isabel Atkinson <isabel.atkinson@mongodb.com>
2024-06-12 15:35:15 -0400

    RUST-1924 Generate and upload SSDLC compliance report (#1133)

---
+++ .evergreen/create-ssdlc-compliance-report.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o errexit
+set -o xtrace
+
+REPORT_FILE=".evergreen/${CRATE_VERSION}-ssdlc-compliance-report.md"
+SED_REPLACE="s/RELEASE_VERSION/${CRATE_VERSION}/g"
+
+sed ${SED_REPLACE} .evergreen/ssdlc-compliance-report-template.md > ${REPORT_FILE}
