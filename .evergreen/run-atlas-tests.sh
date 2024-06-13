2380bfecfb8c37afba11e695b2478c79a77c9ebe
Abraham Egnor <abraham.egnor@mongodb.com>
2024-06-13 11:50:57 -0400

    RUST-1971 / RUST-1975 Update Atlas connectivity credentials (#1135)

--- .evergreen/run-atlas-tests.sh
+++ .evergreen/run-atlas-tests.sh
@@ -6,6 +6,10 @@ set -o pipefail
 source .evergreen/env.sh
 source .evergreen/cargo-test.sh
 
+export MONGO_ATLAS_TESTS=1
+
+source "${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh" drivers/atlas_connect
+
 set +o errexit
 
 cargo_test atlas_connectivity results.xml
