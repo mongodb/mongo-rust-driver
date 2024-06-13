04396c3e2736c666ea5a51eb0a1727def56d86c0
Abraham Egnor <abraham.egnor@mongodb.com>
2024-06-13 11:39:11 -0400

    RUST-1971 / RUST-1975 Update Atlas connectivity credentials

--- .evergreen/run-atlas-tests.sh
+++ .evergreen/run-atlas-tests.sh
@@ -8,20 +8,10 @@ source .evergreen/cargo-test.sh
 
 export MONGO_ATLAS_TESTS=1
 
-. "${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh" drivers/atlas_connect
-
-set +x
-export MONGO_ATLAS_FREE_TIER_REPL_URI="${atlas_free}"
-export MONGO_ATLAS_FREE_TIER_REPL_URI_SRV="${atlas_srv_free}"
-
-echo uri hash:
-echo "${MONGO_ATLAS_FREE_TIER_REPL_URI}" | shasum
-echo srv uri hash:
-echo "${MONGO_ATLAS_FREE_TIER_REPL_URI_SRV}" | shasum
-set -x
+source "${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh" drivers/atlas_connect
 
 set +o errexit
 
-cargo_test atlas_free_tier_repl_set results.xml
+cargo_test atlas_connectivity results.xml
 
 exit $CARGO_RESULT
--- src/test/atlas_connectivity.rs
+++ src/test/atlas_connectivity.rs
@@ -43,90 +43,66 @@ async fn run_test(uri_env_var: &str, resolver_config: Option<ResolverConfig>) {
 
 #[tokio::test]
 async fn atlas_free_tier_repl_set() {
-    run_test("MONGO_ATLAS_FREE_TIER_REPL_URI", None).await;
+    run_test("ATLAS_FREE", None).await;
 }
 
 #[tokio::test]
 async fn atlas_free_tier_repl_set_srv() {
-    run_test("MONGO_ATLAS_FREE_TIER_REPL_URI_SRV", None).await;
-    run_test(
-        "MONGO_ATLAS_FREE_TIER_REPL_URI_SRV",
-        Some(ResolverConfig::cloudflare()),
-    )
-    .await;
+    run_test("ATLAS_SRV_FREE", None).await;
+    run_test("ATLAS_SRV_FREE", Some(ResolverConfig::cloudflare())).await;
 }
 
 #[tokio::test]
 async fn atlas_serverless() {
-    run_test("MONGO_ATLAS_SERVERLESS_URI", None).await;
+    run_test("ATLAS_SERVERLESS", None).await;
 }
 
 #[tokio::test]
 async fn atlas_serverless_srv() {
-    run_test("MONGO_ATLAS_SERVERLESS_URI_SRV", None).await;
-    run_test(
-        "MONGO_ATLAS_SERVERLESS_URI_SRV",
-        Some(ResolverConfig::cloudflare()),
-    )
-    .await;
+    run_test("ATLAS_SRV_SERVERLESS", None).await;
+    run_test("ATLAS_SRV_SERVERLESS", Some(ResolverConfig::cloudflare())).await;
 }
 
 #[tokio::test]
 async fn atlas_repl_set() {
-    run_test("MONGO_ATLAS_REPL_URI", None).await;
+    run_test("ATLAS_REPL", None).await;
 }
 
 #[tokio::test]
 async fn atlas_repl_set_srv() {
-    run_test("MONGO_ATLAS_REPL_URI_SRV", None).await;
-    run_test(
-        "MONGO_ATLAS_REPL_URI_SRV",
-        Some(ResolverConfig::cloudflare()),
-    )
-    .await;
+    run_test("ATLAS_SRV_REPL", None).await;
+    run_test("ATLAS_SRV_REPL", Some(ResolverConfig::cloudflare())).await;
 }
 
 #[tokio::test]
 async fn atlas_sharded() {
-    run_test("MONGO_ATLAS_SHARDED_URI", None).await;
+    run_test("ATLAS_SHRD", None).await;
 }
 
 #[tokio::test]
 async fn atlas_sharded_srv() {
-    run_test("MONGO_ATLAS_SHARDED_URI_SRV", None).await;
-    run_test(
-        "MONGO_ATLAS_SHARDED_URI_SRV",
-        Some(ResolverConfig::cloudflare()),
-    )
-    .await;
+    run_test("ATLAS_SRV_SHRD", None).await;
+    run_test("ATLAS_SRV_SHRD", Some(ResolverConfig::cloudflare())).await;
 }
 
 #[tokio::test]
 async fn atlas_tls_11() {
-    run_test("MONGO_ATLAS_TLS11_URI", None).await;
+    run_test("ATLAS_TLS11", None).await;
 }
 
 #[tokio::test]
 async fn atlas_tls11_srv() {
-    run_test("MONGO_ATLAS_TLS11_URI_SRV", None).await;
-    run_test(
-        "MONGO_ATLAS_TLS11_URI_SRV",
-        Some(ResolverConfig::cloudflare()),
-    )
-    .await;
+    run_test("ATLAS_SRV_TLS11", None).await;
+    run_test("ATLAS_SRV_TLS11", Some(ResolverConfig::cloudflare())).await;
 }
 
 #[tokio::test]
 async fn atlas_tls_12() {
-    run_test("MONGO_ATLAS_TLS12_URI", None).await;
+    run_test("ATLAS_TLS12", None).await;
 }
 
 #[tokio::test]
 async fn atlas_tls12_srv() {
-    run_test("MONGO_ATLAS_TLS12_URI_SRV", None).await;
-    run_test(
-        "MONGO_ATLAS_TLS12_URI_SRV",
-        Some(ResolverConfig::cloudflare()),
-    )
-    .await;
+    run_test("ATLAS_SRV_TLS12", None).await;
+    run_test("ATLAS_SRV_TLS12", Some(ResolverConfig::cloudflare())).await;
 }
