8b01e9a0876fdf78aabf3722e5bd9f380cd1f0f5
Abraham Egnor <abraham.egnor@mongodb.com>
2024-06-13 10:55:07 -0400

    wip

--- .evergreen/run-atlas-tests.sh
+++ .evergreen/run-atlas-tests.sh
@@ -6,8 +6,22 @@ set -o pipefail
 source .evergreen/env.sh
 source .evergreen/cargo-test.sh
 
+export MONGO_ATLAS_TESTS=1
+
+. "${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh" drivers/atlas_connect
+
+set +x
+export MONGO_ATLAS_FREE_TIER_REPL_URI="${atlas_free}"
+export MONGO_ATLAS_FREE_TIER_REPL_URI_SRV="${atlas_srv_free}"
+
+echo uri hash:
+echo "${MONGO_ATLAS_FREE_TIER_REPL_URI}" | shasum
+echo srv uri hash:
+echo "${MONGO_ATLAS_FREE_TIER_REPL_URI_SRV}" | shasum
+set -x
+
 set +o errexit
 
-cargo_test atlas_connectivity results.xml
+cargo_test atlas_free_tier_repl_set results.xml
 
 exit $CARGO_RESULT
