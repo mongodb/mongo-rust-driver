c6ebc174dd1e95783b1a00e6f28f1a66afc3f1d8
John Vandenberg <jayvdb@gmail.com>
2024-08-16 19:38:57 +0800

    minor: bump derive_more to 1.0

--- src/event/sdam/topology_description.rs
+++ src/event/sdam/topology_description.rs
@@ -12,7 +12,7 @@ use crate::{
 /// A description of the most up-to-date information known about a topology. Further details can
 /// be found in the [Server Discovery and Monitoring specification](https://github.com/mongodb/specifications/blob/master/source/server-discovery-and-monitoring/server-discovery-and-monitoring.rst).
 #[derive(Clone, derive_more::Display)]
-#[display(fmt = "{}", description)]
+#[display("{}", description)]
 pub struct TopologyDescription {
     pub(crate) description: crate::sdam::TopologyDescription,
 }
