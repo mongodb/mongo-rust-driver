9b639bbbdb61b43a04a5bebbb4323956c3a62894
Abraham Egnor <abraham.egnor@mongodb.com>
2024-08-19 10:38:25 -0400

    backtrace for test only

--- src/error.rs
+++ src/error.rs
@@ -50,7 +50,8 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// [`ErrorKind`](enum.ErrorKind.html) is wrapped in an `Arc` to allow the errors to be
 /// cloned.
 #[derive(Clone, Debug, Error)]
-#[error("Kind: {kind}, labels: {labels:?}, backtrace: {bt}")]
+#[cfg_attr(test, error("Kind: {kind}, labels: {labels:?}, backtrace: {bt}"))]
+#[cfg_attr(not(test), error("Kind: {kind}, labels: {labels:?}"))]
 #[non_exhaustive]
 pub struct Error {
     /// The type of error that occurred.
@@ -59,6 +60,7 @@ pub struct Error {
     pub(crate) wire_version: Option<i32>,
     #[source]
     pub(crate) source: Option<Box<Error>>,
+    #[cfg(test)]
     bt: Arc<std::backtrace::Backtrace>,
 }
 
@@ -91,6 +93,7 @@ impl Error {
             labels,
             wire_version: None,
             source: None,
+            #[cfg(test)]
             bt: Arc::new(std::backtrace::Backtrace::capture()),
         }
     }
