b1002feec8e17ec219380485864ab98d7c522485
Abraham Egnor <abraham.egnor@mongodb.com>
2024-11-12 10:59:10 -0500

    revert to standard evergreen tools

--- .evergreen/fetch-drivers-tools.sh
+++ .evergreen/fetch-drivers-tools.sh
@@ -8,5 +8,4 @@ if [[ -z "$DRIVERS_TOOLS" ]]; then
 fi
 
 rm -rf $DRIVERS_TOOLS
-#git clone https://github.com/mongodb-labs/drivers-evergreen-tools.git $DRIVERS_TOOLS
-git clone -b DRIVERS-2949/happy-eyeballs-test https://github.com/abr-egn/drivers-evergreen-tools.git $DRIVERS_TOOLS
+git clone https://github.com/mongodb-labs/drivers-evergreen-tools.git $DRIVERS_TOOLS
