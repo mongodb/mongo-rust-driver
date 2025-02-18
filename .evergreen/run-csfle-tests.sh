90c9dc1e5b55d196fcc78c7512836c1629892762
Isabel Atkinson <isabel.atkinson@mongodb.com>
2025-02-18 14:24:20 -0700

    fix aws name

--- .evergreen/run-csfle-tests.sh
+++ .evergreen/run-csfle-tests.sh
@@ -33,12 +33,11 @@ cargo_test test::csfle
 cargo_test test::spec::client_side_encryption
 
 feature_flags+=("aws-auth")
-
-cargo_test test::csfle::on_demand_aws_auth::success
+cargo_test test::csfle::on_demand_aws::success
 
 # Unset variables for on-demand credential failure tests.
 unset AWS_ACCESS_KEY_ID
 unset AWS_SECRET_ACCESS_KEY
-cargo_test test::csfle::on_demand_aws_auth::failure
+cargo_test test::csfle::on_demand_aws::failure
 
 exit ${CARGO_RESULT}
