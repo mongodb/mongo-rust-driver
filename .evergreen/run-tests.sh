eca7fe6a5c3ade40fe8235187f463c1d0a5814ca
Abraham Egnor <abraham.egnor@mongodb.com>
2025-02-21 11:11:23 -0500

    RUST-2159 Fix evergreen windows openssl config

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -28,6 +28,11 @@ echo "cargo test options: $(cargo_test_options)"
 
 set +o errexit
 
+if [ "Windows_NT" == "$OS" ]; then
+  export SSL_CERT_FILE=$(cygpath /etc/ssl/certs/ca-bundle.crt --windows)
+  export SSL_CERT_DIR=$(cygpath /etc/ssl/certs --windows)
+fi
+
 cargo_test ""
 
 # cargo-nextest doesn't support doc tests
