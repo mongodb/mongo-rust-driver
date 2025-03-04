206b99e6b17b1b0ab9cb42ca70fa7a9f7c0f250f
Isabel Atkinson <isabel.atkinson@mongodb.com>
2025-03-04 16:33:45 -0500

    RUST-2055 Filter OIDC tests with `cargo nextest` (#1317)

--- .evergreen/run-mongodb-oidc-test.sh
+++ .evergreen/run-mongodb-oidc-test.sh
@@ -3,44 +3,23 @@
 set +x          # Disable debug trace
 set -o errexit  # Exit the script with error if any of the commands fail
 
+source .evergreen/env.sh
+source .evergreen/cargo-test.sh
+
+CARGO_OPTIONS+=("--ignore-default-filter")
+
 echo "Running MONGODB-OIDC authentication tests"
 
-OIDC_ENV=${OIDC_ENV:-"test"}
-
-export TEST_AUTH_OIDC=1
-export COVERAGE=1
-export AUTH="auth"
-
-if [ $OIDC_ENV == "test" ]; then
-
-    source .evergreen/env.sh
-    source .evergreen/cargo-test.sh
-    # Make sure DRIVERS_TOOLS is set.
-    if [ -z "$DRIVERS_TOOLS" ]; then
-        echo "Must specify DRIVERS_TOOLS"
-        exit 1
-    fi
-    source ${DRIVERS_TOOLS}/.evergreen/auth_oidc/secrets-export.sh
-
-    cargo nextest run test::spec::oidc::basic --no-capture --profile ci
-    RESULT=$?
-    cp target/nextest/ci/junit.xml results.xml
-elif [ $OIDC_ENV == "azure" ]; then
-    source ./env.sh
-
-    $TEST_FILE test::spec::oidc::azure --nocapture
-    RESULT=$?
-elif [ $OIDC_ENV == "gcp" ]; then
-    source ./secrets-export.sh
-
-    $TEST_FILE test::spec::oidc::gcp --nocapture
-    RESULT=$?
-elif [ $OIDC_ENV == "k8s" ]; then
-    $TEST_FILE test::spec::oidc::k8s --nocapture
-    RESULT=$?
-else
-    echo "Unrecognized OIDC_ENV '${OIDC_ENV}'"
+# Make sure DRIVERS_TOOLS is set.
+if [ -z "$DRIVERS_TOOLS" ]; then
+    echo "Must specify DRIVERS_TOOLS"
     exit 1
 fi
 
-exit $RESULT
+source ${DRIVERS_TOOLS}/.evergreen/auth_oidc/secrets-export.sh
+
+set +o errexit
+
+cargo_test test::spec::oidc_skip_ci::basic
+
+exit $CARGO_RESULT
