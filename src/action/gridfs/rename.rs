c72830c1a98c40ab23bb0d13894aeb2672a17ee3
Abraham Egnor <abraham.egnor@mongodb.com>
2025-04-02 10:21:38 -0400

    RUST-2090/gridfs helpers (#1351)

--- src/action/gridfs/rename.rs
+++ src/action/gridfs/rename.rs
@@ -1,6 +1,10 @@
 use bson::{doc, Bson};
 
-use crate::{action::action_impl, error::Result, gridfs::GridFsBucket};
+use crate::{
+    action::action_impl,
+    error::{ErrorKind, GridFsErrorKind, GridFsFileIdentifier, Result},
+    gridfs::GridFsBucket,
+};
 
 impl GridFsBucket {
     /// Renames the file with the given 'id' to the provided `new_filename`. This method returns an
@@ -14,6 +18,22 @@ impl GridFsBucket {
             new_filename: new_filename.into(),
         }
     }
+
+    /// Renames all revisions of the file with the given name to the provided `new_filename`. This
+    /// method returns an error if the name does not match any files in the bucket.
+    ///
+    /// `await` will return [`Result<()>`].
+    pub fn rename_by_name(
+        &self,
+        filename: impl Into<String>,
+        new_filename: impl Into<String>,
+    ) -> RenameByName {
+        RenameByName {
+            bucket: self,
+            filename: filename.into(),
+            new_filename: new_filename.into(),
+        }
+    }
 }
 
 #[cfg(feature = "sync")]
@@ -24,6 +44,18 @@ impl crate::sync::gridfs::GridFsBucket {
     /// [`run`](Rename::run) will return [`Result<()>`].
     pub fn rename(&self, id: Bson, new_filename: impl Into<String>) -> Rename {
         self.async_bucket.rename(id, new_filename)
+    }
+
+    /// Renames all revisions of the file with the given name to the provided `new_filename`. This
+    /// method returns an error if the name does not match any files in the bucket.
+    ///
+    /// [`run`](RenameByName::run) will return [`Result<()>`].
+    pub fn rename_by_name(
+        &self,
+        filename: impl Into<String>,
+        new_filename: impl Into<String>,
+    ) -> RenameByName {
+        self.async_bucket.rename_by_name(filename, new_filename)
     }
 }
 
@@ -47,6 +79,39 @@ impl<'a> Action for Rename<'a> {
                 doc! { "$set": { "filename": self.new_filename } },
             )
             .await?;
+
+        Ok(())
+    }
+}
+
+/// Renames a file selected by name.  Construct with [`GridFsBucket::rename_by_name`].
+#[must_use]
+pub struct RenameByName<'a> {
+    bucket: &'a GridFsBucket,
+    filename: String,
+    new_filename: String,
+}
+
+#[action_impl]
+impl<'a> Action for RenameByName<'a> {
+    type Future = RenameByNameFuture;
+
+    async fn execute(self) -> Result<()> {
+        let count = self
+            .bucket
+            .files()
+            .update_many(
+                doc! { "filename": self.filename.clone() },
+                doc! { "$set": { "filename": self.new_filename } },
+            )
+            .await?
+            .matched_count;
+        if count == 0 {
+            return Err(ErrorKind::GridFs(GridFsErrorKind::FileNotFound {
+                identifier: GridFsFileIdentifier::Filename(self.filename),
+            })
+            .into());
+        }
 
         Ok(())
     }
