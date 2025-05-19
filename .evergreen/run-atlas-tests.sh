49e936a4589e3affb544b6b0557ed2c0b3a86354
Kevin Albertson <kevin.albertson@mongodb.com>
2025-05-19 12:10:12 -0400

    RUST-2208 test Atlas with X509 auth (#1374)

--- .evergreen/run-atlas-tests.sh
+++ .evergreen/run-atlas-tests.sh
@@ -12,6 +12,13 @@ source "${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh" drivers/a
 
 set +o errexit
 
+# Create client certificate file from base64 encoded secret:
+mkdir -p .secrets
+chmod 700 .secrets
+echo "${ATLAS_X509_DEV_CERT_BASE64}" | base64 --decode > .secrets/clientcert.pem
+ATLAS_X509_DEV_WITH_CERT="${ATLAS_X509_DEV}&tlsCertificateKeyFile=.secrets/clientcert.pem"
+export ATLAS_X509_DEV_WITH_CERT
+
 cargo_test test::atlas_connectivity
 
 exit $CARGO_RESULT
