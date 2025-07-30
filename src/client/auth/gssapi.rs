9dda3b3e4e9a3cf2a7b0415a0386f86468e75bbe
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2025-07-30 11:33:54 -0400

    RUST-2245: Initial second attempt at GSSAPI on Windows

--- src/client/auth/gssapi.rs
+++ src/client/auth/gssapi.rs
@@ -1,12 +1,16 @@
 use cross_krb5::{ClientCtx, InitiateFlags, K5Ctx, PendingClientCtx, Step};
+use sspi::{
+    AcquireCredentialsHandleResult, BufferType, ClientRequestFlags, CredentialsBuffers,
+    DataRepresentation, InitializeSecurityContextResult, Kerberos, KerberosConfig, SecurityBuffer,
+    SecurityStatus, Sspi, SspiImpl, Username,
+};
 
 use crate::{
     bson::Bson,
     client::{
         auth::{
             sasl::{SaslContinue, SaslResponse, SaslStart},
-            Credential,
-            GSSAPI_STR,
+            Credential, GSSAPI_STR,
         },
         options::ServerApi,
     },
@@ -375,4 +379,200 @@ async fn canonicalize_hostname(
     let hostname = hostname.trim_end_matches(".");
 
     Ok(hostname.to_string())
+}
+
+async fn windows_sspi(
+    conn: &mut Connection,
+    credential: &Credential,
+    server_api: Option<&ServerApi>,
+    resolver_config: Option<&ResolverConfig>,
+) -> Result<()> {
+    let properties = GssapiProperties::from_credential(credential)?;
+
+    let conn_host = conn.address.host().to_string();
+    let hostname = properties.service_host.as_ref().unwrap_or(&conn_host);
+    let hostname = canonicalize_hostname(
+        hostname,
+        &properties.canonicalize_host_name,
+        resolver_config,
+    )
+    .await?;
+
+    // Configuration
+    let kerberos_config = KerberosConfig::new("", "".to_string());
+    let mut kerberos = Kerberos::new_client_from_config(kerberos_config).map_err(|e| {
+        Error::authentication_error(
+            GSSAPI_STR,
+            &format!("Failed to create SSPI Kerberos client: {e}"),
+        )
+    })?;
+
+    // Acquire Creds
+    let mut acq_creds_handle_result = get_cred_handle(&mut kerberos, credential)?;
+
+    let mut conversation_id = None;
+    let mut input_token: Vec<u8> = vec![];
+    let source = credential.source.as_deref().unwrap_or("$external");
+
+    loop {
+        let (output_token, status) = step(
+            &mut kerberos,
+            &mut acq_creds_handle_result.credentials_handle,
+            input_token.as_slice(),
+            credential.username.clone(),
+            properties.clone(),
+            &hostname,
+        )?;
+        if status == SecurityStatus::ContinueNeeded || status == SecurityStatus::Ok {
+            let command = if conversation_id.is_none() {
+                SaslStart::new(
+                    source.to_string(),
+                    crate::client::auth::AuthMechanism::Gssapi,
+                    output_token,
+                    server_api.cloned(),
+                )
+                .into_command()?
+            } else {
+                SaslContinue::new(
+                    source.to_string(),
+                    conversation_id.clone().unwrap(),
+                    output_token,
+                    server_api.cloned(),
+                )
+                .into_command()
+            };
+
+            let response_doc = conn.send_message(command).await?;
+            let sasl_response =
+                SaslResponse::parse(GSSAPI_STR, response_doc.auth_response_body(GSSAPI_STR)?)?;
+
+            conversation_id = Some(sasl_response.conversation_id);
+            input_token = sasl_response.payload;
+
+            if sasl_response.done {
+                return Ok(())
+            }
+        } else {
+            return Err(Error::authentication_error(
+                GSSAPI_STR,
+                &format!("problem authenticating; status = {status:?}"),
+            ));
+        }
+    }
+
+    Ok(())
+}
+
+pub(crate) fn get_cred_handle(
+    kerberos: &mut Kerberos,
+    credential: &Credential,
+) -> Result<AcquireCredentialsHandleResult<Option<CredentialsBuffers>>> {
+    let mut acq_creds_handle_result = kerberos
+        .acquire_credentials_handle()
+        .with_credential_use(sspi::CredentialUse::Outbound);
+
+    if let Some(pwd) = credential.password.as_ref() {
+        if let Some(username) = credential.username.as_ref() {
+            let identity = sspi::AuthIdentity {
+                username: Username::parse(username).map_err(|e| {
+                    Error::authentication_error(
+                        GSSAPI_STR,
+                        &format!("Failed to parse user principal: {e}"),
+                    )
+                })?,
+                password: pwd.into(),
+            };
+
+            acq_creds_handle_result = acq_creds_handle_result.with_auth_data(&identity.into());
+        } else {
+            return Err(Error::authentication_error(
+                GSSAPI_STR,
+                "Username required but not specified",
+            ));
+        }
+    }
+
+    acq_creds_handle_result.execute(kerberos).map_err(|e| {
+        Error::authentication_error(
+            GSSAPI_STR,
+            &format!("Failed to acquire credentials handle: {e:?}"),
+        )
+    })
+}
+
+fn step_helper(
+    kerberos: &mut Kerberos,
+    cred_handle: &mut <Kerberos as SspiImpl>::CredentialsHandle,
+    input_buffer: &mut [SecurityBuffer],
+    output_buffer: &mut [SecurityBuffer],
+    user_principal: Option<String>,
+    properties: GssapiProperties,
+    hostname: &str,
+) -> Result<InitializeSecurityContextResult> {
+    // todo: consider not only refactoring this duplicated code into a helper function,
+    //       but possibly updating GssapiProperties::from-credential to create the service principal for me
+    let service_name: &str = properties.service_name.as_ref();
+    let mut service_principal = format!("{service_name}/{hostname}");
+    if let Some(service_realm) = properties.service_realm.as_ref() {
+        service_principal = format!("{service_principal}@{service_realm}");
+    } else if let Some(user_principal) = user_principal.as_ref() {
+        if let Some(idx) = user_principal.find('@') {
+            // If no SERVICE_REALM was specified, use realm specified in the
+            // username. Note that `realm` starts with '@'.
+            let (_, realm) = user_principal.split_at(idx);
+            service_principal = format!("{service_principal}{realm}");
+        }
+    }
+    let mut builder = kerberos
+        .initialize_security_context()
+        .with_credentials_handle(cred_handle)
+        .with_context_requirements(ClientRequestFlags::MUTUAL_AUTH)
+        .with_target_data_representation(DataRepresentation::Native)
+        .with_target_name(&service_principal)
+        .with_input(input_buffer)
+        .with_output(output_buffer);
+
+    kerberos
+        .initialize_security_context_impl(&mut builder)
+        .map_err(|e| {
+            Error::authentication_error(
+                GSSAPI_STR,
+                &format!("Failed to initialize security context: {e}"),
+            )
+        })?
+        .resolve_to_result()
+        .map_err(|e| {
+            Error::authentication_error(GSSAPI_STR, &format!("Failed to resolve to result: {e}"))
+        })
+}
+
+pub fn step(
+    kerberos: &mut Kerberos,
+    cred_handle: &mut <Kerberos as SspiImpl>::CredentialsHandle,
+    input_token: &[u8],
+    user_principal: Option<String>,
+    properties: GssapiProperties,
+    hostname: &str,
+) -> Result<(Vec<u8>, SecurityStatus)> {
+    let mut secure_input_buffer =
+        vec![SecurityBuffer::new(input_token.to_vec(), BufferType::Token)];
+    let mut secure_output_buffer = vec![SecurityBuffer::new(Vec::new(), BufferType::Token)];
+    match step_helper(
+        kerberos,
+        cred_handle,
+        &mut secure_input_buffer,
+        &mut secure_output_buffer,
+        user_principal,
+        properties,
+        hostname,
+    ) {
+        Ok(result) => {
+            let output_buffer = secure_output_buffer[0].to_owned();
+            Ok((output_buffer.buffer, result.status))
+        }
+        Err(e) => Err(Error::authentication_error(
+            GSSAPI_STR,
+            &format!("error stepping: {e:?}"),
+        )),
+    }
 }
