6654548676284167e5c9c68ef67fefe5afc3614e
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2025-08-06 14:07:07 -0400

    RUST-2245: Update e2e tests to handle windows

--- .evergreen/run-gssapi-tests.sh
+++ .evergreen/run-gssapi-tests.sh
@@ -16,50 +16,56 @@ FEATURE_FLAGS+=("gssapi-auth")
 
 set +o errexit
 
-# Create a krb5 config file with relevant
-touch krb5.conf
-echo "[realms]
-  $SASL_REALM = {
-    kdc = $SASL_HOST
-    admin_server = $SASL_HOST
-  }
+if [ "Windows_NT" != "$OS" ]; then
+  # Create a krb5 config file with relevant
+  touch krb5.conf
+  echo "[realms]
+    $SASL_REALM = {
+      kdc = $SASL_HOST
+      admin_server = $SASL_HOST
+    }
 
-  $SASL_REALM_CROSS = {
-    kdc = $SASL_HOST
-    admin_server = $SASL_HOST
-  }
+    $SASL_REALM_CROSS = {
+      kdc = $SASL_HOST
+      admin_server = $SASL_HOST
+    }
 
-[domain_realm]
-  .$SASL_DOMAIN = $SASL_REALM
-  $SASL_DOMAIN = $SASL_REALM
-" > krb5.conf
+  [domain_realm]
+    .$SASL_DOMAIN = $SASL_REALM
+    $SASL_DOMAIN = $SASL_REALM
+  " > krb5.conf
 
-export KRB5_CONFIG=krb5.conf
+  export KRB5_CONFIG=krb5.conf
 
-# Authenticate the user principal in the KDC before running the e2e test
-echo "Authenticating $PRINCIPAL"
-echo "$SASL_PASS" | kinit -p $PRINCIPAL
-klist
+  # Authenticate the user principal in the KDC before running the e2e test
+  echo "Authenticating $PRINCIPAL"
+  echo "$SASL_PASS" | kinit -p $PRINCIPAL
+  klist
+fi
 
 # Run end-to-end auth tests for "$PRINCIPAL" user
 TEST_OPTIONS+=("--skip with_service_realm_and_host_options")
 cargo_test test::auth::gssapi_skip_local
 
-# Unauthenticate
-echo "Unauthenticating $PRINCIPAL"
-kdestroy
+if [ "Windows_NT" != "$OS" ]; then
+  # Unauthenticate
+  echo "Unauthenticating $PRINCIPAL"
+  kdestroy
 
-# Authenticate the alternative user principal in the KDC and run other e2e test
-echo "Authenticating $PRINCIPAL_CROSS"
-echo "$SASL_PASS_CROSS" | kinit -p $PRINCIPAL_CROSS
-klist
+  # Authenticate the alternative user principal in the KDC and run other e2e test
+  echo "Authenticating $PRINCIPAL_CROSS"
+  echo "$SASL_PASS_CROSS" | kinit -p $PRINCIPAL_CROSS
+  klist
+fi
 
 TEST_OPTIONS=()
 cargo_test test::auth::gssapi_skip_local::with_service_realm_and_host_options
 
-# Unauthenticate
-echo "Unauthenticating $PRINCIPAL_CROSS"
-kdestroy
+if [ "Windows_NT" != "$OS" ]; then
+  # Unauthenticate
+  echo "Unauthenticating $PRINCIPAL_CROSS"
+  kdestroy
+fi
 
 # Run remaining tests
 cargo_test spec::auth
