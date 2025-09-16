cb9d09bc2555634181203fabc14b768a9c88db0a
Abraham Egnor <abraham.egnor@mongodb.com>
2025-09-16 11:40:01 +0100

    skip test on sharded

--- src/test/spec/mongodb_handshake.rs
+++ src/test/spec/mongodb_handshake.rs
@@ -1,6 +1,18 @@
+use crate::test::topology_is_sharded;
+
 use super::unified_runner::run_unified_tests;
 
 #[tokio::test]
 async fn run_unified() {
-    run_unified_tests(&["mongodb-handshake", "unified"]).await;
+    let mut runner = run_unified_tests(&["mongodb-handshake", "unified"]);
+    if topology_is_sharded().await {
+        // This test is flaky on sharded deployments.
+        runner = runner.skip_tests(
+            &[
+                "metadata append does not create new connections or close existing ones and no \
+                 hello command is sent",
+            ],
+        );
+    }
+    runner.await;
 }
