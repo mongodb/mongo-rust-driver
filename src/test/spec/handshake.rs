ad34ea4a551e68fcc515eeac76c6b2c57c9539be
Abraham Egnor <abraham.egnor@mongodb.com>
2025-09-17 15:47:17 +0100

    test work

--- src/test/spec/handshake.rs
+++ src/test/spec/handshake.rs
@@ -50,98 +50,6 @@ async fn arbitrary_auth_mechanism() {
         .unwrap();
 }
 
-enum InitialMetadata {
-    InOptions,
-    Appended,
-}
-
-impl InitialMetadata {
-    async fn run_test(self) {
-        let test_info = [
-            DriverInfo {
-                name: "framework".to_owned(),
-                version: Some("2.0".to_owned()),
-                platform: Some("Framework Platform".to_owned()),
-            },
-            DriverInfo {
-                name: "framework".to_owned(),
-                version: Some("2.0".to_owned()),
-                platform: None,
-            },
-            DriverInfo {
-                name: "framework".to_owned(),
-                version: None,
-                platform: Some("Framework Platform".to_owned()),
-            },
-            DriverInfo {
-                name: "framework".to_owned(),
-                version: None,
-                platform: None,
-            },
-        ];
-        for addl_info in test_info {
-            let mut options = get_client_options().await.clone();
-            options.max_idle_time = Some(Duration::from_millis(1));
-            let initial_info = DriverInfo {
-                name: "library".to_owned(),
-                version: Some("1.2".to_owned()),
-                platform: Some("Library Platform".to_owned()),
-            };
-            let to_append = match self {
-                InitialMetadata::InOptions => {
-                    options.driver_info = Some(initial_info);
-                    None
-                }
-                InitialMetadata::Appended => Some(initial_info),
-            };
-            let hello = watch_hello(&mut options);
-            let client = Client::with_options(options).unwrap();
-
-            if let Some(append_info) = to_append {
-                client.append_metadata(append_info);
-            }
-            client.ping().await;
-            let initial_client_metadata = hello.lock().unwrap().client_metadata();
-            let initial_name = initial_client_metadata["driver"]["name"].as_str().unwrap();
-            let initial_version = initial_client_metadata["driver"]["version"]
-                .as_str()
-                .unwrap();
-            let initial_platform = initial_client_metadata["platform"].as_str().unwrap();
-
-            tokio::time::sleep(Duration::from_millis(5)).await;
-
-            client.append_metadata(addl_info.clone());
-            client.ping().await;
-            let test_client_metadata = hello.lock().unwrap().client_metadata();
-            let test_name = test_client_metadata["driver"]["name"].as_str().unwrap();
-            let test_version = test_client_metadata["driver"]["version"].as_str().unwrap();
-            let test_platform = test_client_metadata["platform"].as_str().unwrap();
-
-            // Compare updated metadata
-            assert_eq!(test_name, format!("{initial_name}|{}", addl_info.name));
-            if let Some(addl_version) = &addl_info.version {
-                assert_eq!(test_version, format!("{initial_version}|{addl_version}"));
-            } else {
-                assert_eq!(test_version, initial_version);
-            }
-            if let Some(addl_platform) = &addl_info.platform {
-                assert_eq!(test_platform, format!("{initial_platform}|{addl_platform}"));
-            } else {
-                assert_eq!(test_platform, initial_platform);
-            }
-
-            // Everything else should be the same
-            let mut initial_client_metadata = initial_client_metadata;
-            initial_client_metadata.remove("driver");
-            initial_client_metadata.remove("platform");
-            let mut test_client_metadata = test_client_metadata;
-            test_client_metadata.remove("driver");
-            test_client_metadata.remove("platform");
-            assert_eq!(initial_client_metadata, test_client_metadata);
-        }
-    }
-}
-
 fn watch_hello(options: &mut ClientOptions) -> Arc<Mutex<Command>> {
     let hello: Arc<Mutex<Command>> = Arc::new(Mutex::new(Command::default()));
     let cb_hello = hello.clone();
@@ -170,37 +78,151 @@ impl Client {
     }
 }
 
+impl DriverInfo {
+    fn new(name: &str, version: Option<&str>, platform: Option<&str>) -> Self {
+        Self {
+            name: name.to_owned(),
+            version: version.map(|v| v.to_owned()),
+            platform: platform.map(|v| v.to_owned()),
+        }
+    }
+}
+
+fn assert_other_fields_eq(metadata_a: &Document, metadata_b: &Document) {
+    let mut metadata_a = metadata_a.clone();
+    metadata_a.remove("driver");
+    metadata_a.remove("platform");
+    let mut metadata_b = metadata_b.clone();
+    metadata_b.remove("driver");
+    metadata_b.remove("platform");
+    assert_eq!(metadata_a, metadata_b);
+}
+
+fn extract_driver_info(metadata: &Document) -> (&str, &str, &str) {
+    (
+        metadata["driver"]["name"].as_str().unwrap(),
+        metadata["driver"]["version"].as_str().unwrap(),
+        metadata["platform"].as_str().unwrap(),
+    )
+}
+
 // Client Metadata Update Prose Test 1: Test that the driver updates metadata
 #[tokio::test]
 async fn append_metadata_driver_update() {
-    InitialMetadata::InOptions.run_test().await
+    let test_info = [
+        DriverInfo::new("framework", Some("2.0"), Some("Framework Platform")),
+        DriverInfo::new("framework", Some("2.0"), None),
+        DriverInfo::new("framework", None, Some("Framework Platform")),
+        DriverInfo::new("framework", None, None),
+    ];
+    for addl_info in test_info {
+        let mut options = get_client_options().await.clone();
+        options.max_idle_time = Some(Duration::from_millis(1));
+        options.driver_info = Some(DriverInfo::new(
+            "library",
+            Some("1.2"),
+            Some("Library Platform"),
+        ));
+        let hello = watch_hello(&mut options);
+        let client = Client::with_options(options).unwrap();
+
+        client.ping().await;
+        let initial_client_metadata = hello.lock().unwrap().client_metadata();
+        let (initial_name, initial_version, initial_platform) =
+            extract_driver_info(&initial_client_metadata);
+
+        tokio::time::sleep(Duration::from_millis(5)).await;
+
+        client.append_metadata(addl_info.clone());
+        client.ping().await;
+        let test_client_metadata = hello.lock().unwrap().client_metadata();
+        let (test_name, test_version, test_platform) = extract_driver_info(&test_client_metadata);
+
+        // Compare updated metadata
+        assert_eq!(test_name, format!("{initial_name}|{}", addl_info.name));
+        if let Some(addl_version) = &addl_info.version {
+            assert_eq!(test_version, format!("{initial_version}|{addl_version}"));
+        } else {
+            assert_eq!(test_version, initial_version);
+        }
+        if let Some(addl_platform) = &addl_info.platform {
+            assert_eq!(test_platform, format!("{initial_platform}|{addl_platform}"));
+        } else {
+            assert_eq!(test_platform, initial_platform);
+        }
+
+        assert_other_fields_eq(&initial_client_metadata, &test_client_metadata);
+    }
 }
 
 // Client Metadata Update Prose Test 2: Multiple Successive Metadata Updates
 #[tokio::test]
 async fn append_metadata_successive_updates() {
-    InitialMetadata::Appended.run_test().await
+    let test_info = [
+        DriverInfo::new("framework", Some("2.0"), Some("Framework Platform")),
+        DriverInfo::new("framework", Some("2.0"), None),
+        DriverInfo::new("framework", None, Some("Framework Platform")),
+        DriverInfo::new("framework", None, None),
+    ];
+    for addl_info in test_info {
+        let mut options = get_client_options().await.clone();
+        options.max_idle_time = Some(Duration::from_millis(1));
+        let hello = watch_hello(&mut options);
+        let client = Client::with_options(options).unwrap();
+
+        client.append_metadata(DriverInfo::new(
+            "library",
+            Some("1.2"),
+            Some("Library Platform"),
+        ));
+        client.ping().await;
+        let initial_client_metadata = hello.lock().unwrap().client_metadata();
+        let (initial_name, initial_version, initial_platform) =
+            extract_driver_info(&initial_client_metadata);
+
+        tokio::time::sleep(Duration::from_millis(5)).await;
+
+        client.append_metadata(addl_info.clone());
+        client.ping().await;
+        let test_client_metadata = hello.lock().unwrap().client_metadata();
+        let (test_name, test_version, test_platform) = extract_driver_info(&test_client_metadata);
+
+        // Compare updated metadata
+        assert_eq!(test_name, format!("{initial_name}|{}", addl_info.name));
+        if let Some(addl_version) = &addl_info.version {
+            assert_eq!(test_version, format!("{initial_version}|{addl_version}"));
+        } else {
+            assert_eq!(test_version, initial_version);
+        }
+        if let Some(addl_platform) = &addl_info.platform {
+            assert_eq!(test_platform, format!("{initial_platform}|{addl_platform}"));
+        } else {
+            assert_eq!(test_platform, initial_platform);
+        }
+
+        assert_other_fields_eq(&initial_client_metadata, &test_client_metadata);
+    }
 }
 
 // Client Metadata Update Prose Test 3: Multiple Successive Metadata Updates with Duplicate Data
 #[tokio::test]
 async fn append_metadata_duplicate_successive() {
-    let test_info = [DriverInfo {
-        name: "library".to_owned(),
-        version: Some("1.2".to_owned()),
-        platform: Some("Library Platform".to_owned()),
-    }];
+    let test_info = [
+        DriverInfo::new("library", Some("1.2"), Some("Library Platform")),
+        DriverInfo::new("framework", Some("1.2"), Some("Library Platform")),
+        DriverInfo::new("library", Some("2.0"), Some("Library Platform")),
+        DriverInfo::new("library", Some("1.2"), Some("Framework Platform")),
+        DriverInfo::new("framework", Some("2.0"), Some("Library Platform")),
+        DriverInfo::new("framework", Some("1.2"), Some("Framework Platform")),
+        DriverInfo::new("library", Some("2.0"), Some("Framework Platform")),
+    ];
     for info in test_info {
         let mut options = get_client_options().await.clone();
         options.max_idle_time = Some(Duration::from_millis(1));
         let hello = watch_hello(&mut options);
         let client = Client::with_options(options).unwrap();
 
-        let setup_info = DriverInfo {
-            name: "library".to_owned(),
-            version: Some("1.2".to_owned()),
-            platform: Some("Library Platform".to_owned()),
-        };
+        let setup_info = DriverInfo::new("library", Some("1.2"), Some("Library Platform"));
         client.append_metadata(setup_info.clone());
         client.ping().await;
         let updated_client_metadata = hello.lock().unwrap().client_metadata();
@@ -232,18 +254,47 @@ async fn append_metadata_duplicate_successive() {
             assert_eq!(test_name, format!("library|{}", info.name));
             assert_eq!(test_version, format!("1.2|{}", info.spec_version()));
             assert_eq!(
-                test_version,
+                test_platform,
                 format!("Library Platform|{}", info.spec_platform())
             );
         }
 
-        // Everything else should be the same
-        let mut updated_client_metadata = updated_client_metadata;
-        updated_client_metadata.remove("driver");
-        updated_client_metadata.remove("platform");
-        let mut test_client_metadata = test_client_metadata;
-        test_client_metadata.remove("driver");
-        test_client_metadata.remove("platform");
-        assert_eq!(updated_client_metadata, test_client_metadata);
+        assert_other_fields_eq(&updated_client_metadata, &test_client_metadata);
     }
+}
+
+// Client Metadata Update Prose Test 4: Multiple Metadata Updates with Duplicate Data
+#[tokio::test]
+async fn append_metadata_duplicate_multiple() {
+    let mut options = get_client_options().await.clone();
+    options.max_idle_time = Some(Duration::from_millis(1));
+    let hello = watch_hello(&mut options);
+    let client = Client::with_options(options).unwrap();
+
+    client.append_metadata(DriverInfo::new(
+        "library",
+        Some("1.2"),
+        Some("Library Platform"),
+    ));
+    client.ping().await;
+    tokio::time::sleep(Duration::from_millis(5)).await;
+
+    client.append_metadata(DriverInfo::new(
+        "framework",
+        Some("2.0"),
+        Some("Framework Platform"),
+    ));
+    client.ping().await;
+    let client_metadata = hello.lock().unwrap().client_metadata();
+    tokio::time::sleep(Duration::from_millis(5)).await;
+
+    client.append_metadata(DriverInfo::new(
+        "library",
+        Some("1.2"),
+        Some("Library Platform"),
+    ));
+    client.ping().await;
+    let updated_client_metadata = hello.lock().unwrap().client_metadata();
+
+    assert_eq!(client_metadata, updated_client_metadata);
 }
