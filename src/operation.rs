48727c06c2b5a7e574bc820cb0278df999f94c32
Isabel Atkinson <isabel.atkinson@mongodb.com>
2025-09-23 09:52:13 -0600

    feature

--- src/operation.rs
+++ src/operation.rs
@@ -18,6 +18,7 @@ mod insert;
 pub(crate) mod list_collections;
 pub(crate) mod list_databases;
 mod list_indexes;
+#[cfg(feature = "in-use-encryption")]
 pub(crate) mod raw_output;
 pub(crate) mod run_command;
 pub(crate) mod run_cursor_command;
