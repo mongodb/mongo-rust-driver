18c59b7497c0fb5385f0fa419925e060d431bfe6
Isabel Atkinson <isabel.atkinson@mongodb.com>
2025-11-20 13:40:23 -0500

    release v3.4.1 (#1536)

--- driver/src/lib.rs
+++ driver/src/lib.rs
@@ -15,7 +15,7 @@
 )]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(test, type_length_limit = "80000000")]
-#![doc(html_root_url = "https://docs.rs/mongodb/3.4.0")]
+#![doc(html_root_url = "https://docs.rs/mongodb/3.4.1")]
 
 #[macro_use]
 pub mod options;
