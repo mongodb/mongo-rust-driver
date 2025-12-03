cc5a3d8a4e9da0dc64ce2ba8939abd5ab3f4e6bc
Abraham Egnor <abraham.egnor@mongodb.com>
2025-12-03 12:22:54 +0000

    more fixes

--- .evergreen/build-nextest-archive.sh
+++ .evergreen/build-nextest-archive.sh
@@ -12,6 +12,7 @@ add_conditional_features
 
 if [[ "$ARCHIVE_IN_USE_ENCRYPTION" = true ]]; then
     FEATURE_FLAGS+=("in-use-encryption" "azure-kms" "text-indexes-unstable" "aws-auth")
+    .evergreen/install-dependencies.sh libmongocrypt
 fi
 
 cargo nextest archive --workspace $(features_option) --archive-file nextest-archive.tar.zst
