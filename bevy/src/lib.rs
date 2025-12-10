e63a6e70c870e80cc2b57c2d99418bcd4e49144b
Abraham Egnor <abraham.egnor@mongodb.com>
2025-12-10 12:25:16 +0000

    request-response

--- bevy/src/lib.rs
+++ bevy/src/lib.rs
@@ -1,39 +1,87 @@
-use std::path::Path;
-
-use bevy::{
-    asset::{
-        AssetApp as _,
-        io::{AssetReaderError, AsyncSeekForward, PathStream, Reader},
-    },
-    tasks::futures_lite::AsyncRead,
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
 };
 
-pub struct MongodbAssetPlugin(());
+use bevy::asset::{
+    AssetApp as _,
+    io::{AssetReaderError, PathStream, Reader, VecReader},
+};
+use tokio::sync::{mpsc, oneshot};
+
+pub struct MongodbAssetPlugin(mpsc::Sender<AssetMessage>);
+
+impl MongodbAssetPlugin {
+    pub async fn new(client: &mongodb::Client) -> Self {
+        let (tx, rx) = mpsc::channel::<AssetMessage>(1);
+        let client = client.clone();
+        tokio::spawn(async move {
+            let mut rx = rx;
+            while let Some(message) = rx.recv().await {
+                tokio::spawn(message.process(client.clone()));
+            }
+        });
+        Self(tx)
+    }
+}
+
+enum AssetRequest {
+    Read { path: PathBuf, is_meta: bool },
+}
+
+type AssetResponse = mongodb::error::Result<Vec<u8>>;
+
+struct AssetMessage {
+    request: AssetRequest,
+    response: oneshot::Sender<AssetResponse>,
+}
+
+impl AssetMessage {
+    fn new(request: AssetRequest) -> (Self, oneshot::Receiver<AssetResponse>) {
+        let (response, receiver) = oneshot::channel();
+        (Self { request, response }, receiver)
+    }
+
+    async fn process(self, client: mongodb::Client) {}
+}
 
 impl bevy::app::Plugin for MongodbAssetPlugin {
     fn build(&self, app: &mut bevy::app::App) {
+        let sender = self.0.clone();
         app.register_asset_source(
             "mongodb",
-            bevy::asset::io::AssetSource::build().with_reader(|| Box::new(MongodbAssetReader)),
+            bevy::asset::io::AssetSource::build()
+                .with_reader(move || Box::new(MongodbAssetReader(sender.clone()))),
         );
     }
 }
 
-struct MongodbAssetReader;
+struct MongodbAssetReader(mpsc::Sender<AssetMessage>);
 
 impl MongodbAssetReader {
-    async fn read_bson<'a>(&'a self, path: &'a Path) -> Result<impl Reader + 'a, AssetReaderError> {
-        Ok(BsonReader)
+    async fn send_request(&self, request: AssetRequest) -> Result<VecReader, AssetReaderError> {
+        let (message, response) = AssetMessage::new(request);
+        self.0.send(message).await.map_err(io_error)?;
+        let bytes = response.await.map_err(io_error)?.map_err(io_error)?;
+        Ok(VecReader::new(bytes))
     }
 }
 
 impl bevy::asset::io::AssetReader for MongodbAssetReader {
     async fn read<'a>(&'a self, path: &'a Path) -> Result<impl Reader + 'a, AssetReaderError> {
-        Ok(BsonReader)
+        self.send_request(AssetRequest::Read {
+            path: path.to_owned(),
+            is_meta: false,
+        })
+        .await
     }
 
     async fn read_meta<'a>(&'a self, path: &'a Path) -> Result<impl Reader + 'a, AssetReaderError> {
-        Ok(BsonReader)
+        self.send_request(AssetRequest::Read {
+            path: path.to_owned(),
+            is_meta: true,
+        })
+        .await
     }
 
     async fn is_directory<'a>(&'a self, _path: &'a Path) -> Result<bool, AssetReaderError> {
@@ -44,30 +92,13 @@ impl bevy::asset::io::AssetReader for MongodbAssetReader {
         &'a self,
         path: &'a Path,
     ) -> Result<Box<PathStream>, AssetReaderError> {
-        todo!()
+        Err(AssetReaderError::NotFound(path.to_owned()))
     }
 }
 
-struct BsonReader;
-
-impl AsyncRead for BsonReader {
-    fn poll_read(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-        buf: &mut [u8],
-    ) -> std::task::Poll<std::io::Result<usize>> {
-        todo!()
-    }
+fn io_error<E>(err: E) -> AssetReaderError
+where
+    E: Into<Box<dyn std::error::Error + Send + Sync>>,
+{
+    AssetReaderError::Io(Arc::new(std::io::Error::other(err)))
 }
-
-impl AsyncSeekForward for BsonReader {
-    fn poll_seek_forward(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-        offset: u64,
-    ) -> std::task::Poll<Result<u64, bevy::tasks::futures_lite::io::Error>> {
-        todo!()
-    }
-}
-
-impl Reader for BsonReader {}
