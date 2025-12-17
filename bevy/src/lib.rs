beb39a3b6cdd3b62ee248a7d820b3e224155c9aa
Abraham Egnor <abraham.egnor@mongodb.com>
2025-12-17 13:35:31 +0000

    more doc

--- bevy/src/lib.rs
+++ bevy/src/lib.rs
@@ -1,14 +1,19 @@
 #![doc = include_str!("../README.md")]
+#![warn(missing_docs)]
 
 use bevy::asset::AssetApp;
 
 mod reader;
 
+/// A Bevy plugin to allow reading assets from MongoDB.  See the crate documentation for more
+/// information.
 pub struct MongodbAssetPlugin {
     read_worker: reader::Worker,
 }
 
 impl MongodbAssetPlugin {
+    /// Construct a new instance of the plugin.  Must be run in the same Tokio runtime that was used
+    /// for construction of the [client][mongodb::Client].
     pub async fn new(client: &mongodb::Client) -> Self {
         Self {
             read_worker: reader::Worker::new(client).await,
