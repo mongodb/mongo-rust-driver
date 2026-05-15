9e2abe72aad22e5b8586a4fbb82157974eae6206
nickpoindexter <nick.poindexter@mongodb.com>
2026-05-15 10:40:58 -0500

    add asp commands

---
+++ driver/src/operation/stream_processing.rs
@@ -0,0 +1,10 @@
+//! Operation implementations for Atlas Stream Processing commands.
+
+pub(crate) mod create_stream_processor;
+pub(crate) mod drop_stream_processor;
+pub(crate) mod get_more_sample_stream_processor;
+pub(crate) mod get_stream_processor;
+pub(crate) mod get_stream_processor_stats;
+pub(crate) mod start_sample_stream_processor;
+pub(crate) mod start_stream_processor;
+pub(crate) mod stop_stream_processor;
