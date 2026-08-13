8a1bbe55afe0bf675e2107a0bae07e197fb91e5f
Isabel Atkinson <isabel.atkinson@mongodb.com>
2026-08-13 15:45:16 -0400

    test 9.0

--- .evergreen/generate-tasks/src/main.rs
+++ .evergreen/generate-tasks/src/main.rs
@@ -1,4 +1,6 @@
-static VERSIONS: &[&str] = &["4.2", "4.4", "5.0", "6.0", "7.0", "8.0", "rapid", "latest"];
+static VERSIONS: &[&str] = &[
+    "4.2", "4.4", "5.0", "6.0", "7.0", "8.0", "9.0", "rapid", "latest",
+];
 
 static TOPOLOGIES: &[(&str, &str)] = &[
     ("standalone", "server"),
