3023ab1521a128f977bcbc474b242ed100172619
Isabel Atkinson <isabel.atkinson@mongodb.com>
2026-09-03 14:57:11 -0400

    fixed?

--- .evergreen/set-up-azure-vm.sh
+++ .evergreen/set-up-azure-vm.sh
@@ -5,12 +5,20 @@ set -o errexit
 
 source ${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh azurekms
 
+AZUREKMS_PUBLICKEYPATH="/tmp/testazurekms_publickey"
+AZUREKMS_PRIVATEKEYPATH="/tmp/testazurekms_privatekey"
+
 set +o xtrace
-echo ${AZUREKMS_PUBLICKEY} > "/tmp/testazurekms_publickey"
-echo ${AZUREKMS_PRIVATEKEY} > "/tmp/testazurekms_privatekey"
+echo ${AZUREKMS_PUBLICKEY} > ${AZUREKMS_PUBLICKEYPATH}
+echo ${AZUREKMS_PRIVATEKEY} > ${AZUREKMS_PRIVATEKEYPATH}
 set -o xtrace
 
 # Set 600 permissions on private key file. Otherwise ssh / scp may error with permissions "are too open".
-chmod 600 /tmp/testazurekms_privatekey
+chmod 600 ${AZUREKMS_PRIVATEKEYPATH}
+
+cat <<EOT > azure-keys.yml
+AZUREKMS_PUBLICKEYPATH: ${AZUREKMS_PUBLICKEYPATH}
+AZUREKMS_PRIVATEKEYPATH: ${AZUREKMS_PRIVATEKEYPATH}
+EOT
 
 $DRIVERS_TOOLS/.evergreen/csfle/azurekms/create-and-setup-vm.sh
