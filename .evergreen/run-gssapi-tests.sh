7bd19323a532a5e9bf93fd356d49ce570e32758f
Isabel Atkinson <isabel.atkinson@mongodb.com>
2026-09-14 12:05:49 -0400

    RUST-2489 Fix GSSAPI tests (#1795)

--- .evergreen/run-gssapi-tests.sh
+++ .evergreen/run-gssapi-tests.sh
@@ -1,85 +1,40 @@
 #!/bin/bash
 
 set -o xtrace
-set -o errexit # Exit the script with error if any of the commands fail
+set -o errexit
 
 echo "Running MONGODB-GSSAPI authentication tests"
 
-cd ${PROJECT_DIRECTORY}
+cd "$PROJECT_DIRECTORY"
 source .evergreen/env.sh
 source .evergreen/cargo-test.sh
 
-# Source the drivers/atlas_connect secrets, where GSSAPI test values are held
-source "${DRIVERS_TOOLS}/.evergreen/secrets_handling/setup-secrets.sh" drivers/atlas_connect
+source "$DRIVERS_TOOLS/.evergreen/secrets_handling/setup-secrets.sh" drivers/enterprise_auth
 
-FEATURE_FLAGS+=("gssapi-auth")
+# Windows authenticates via SSPI with a username/password pair in the URI.
+if [ "$OSTYPE" != "cygwin" ]; then
+  export KRB5_CONFIG="krb5.conf.empty" # a nonexistent file is equivalent to an empty config
+  KEYTAB_FILE="drivers.keytab"
+
+  set +o xtrace
+  echo "$KEYTAB_BASE64" | base64 --decode > "$KEYTAB_FILE"
+  set -o xtrace
+
+  kdestroy -A
+  kinit -k -t "$KEYTAB_FILE" -p "$PRINCIPAL"
+fi
 
 set +o errexit
 
-# On Windows, `kinit`/`kdestroy` and other krb5 config settings are
-# not available, nor are they required steps. Windows uses SSPI which
-# is similar to but distinct from (KRB5) GSSAPI. Therefore, we only
-# run the following steps if we are not on Windows.
-if [[ "cygwin" != "$OSTYPE" ]]; then
-  pushd driver
-  # Create a krb5 config file with relevant
-  touch krb5.conf
-  echo "[realms]
-    $SASL_REALM = {
-      kdc = $SASL_HOST
-      admin_server = $SASL_HOST
-    }
-
-    $SASL_REALM_CROSS = {
-      kdc = $SASL_HOST
-      admin_server = $SASL_HOST
-    }
-
-  [domain_realm]
-    .$SASL_DOMAIN = $SASL_REALM
-    $SASL_DOMAIN = $SASL_REALM
-  " > krb5.conf
-
-  export KRB5_CONFIG=krb5.conf
-
-  # Authenticate the user principal in the KDC before running the e2e test
-  echo "Authenticating $PRINCIPAL"
-  echo "$SASL_PASS" | kinit -p $PRINCIPAL
-  klist
-  popd
-fi
-
-# Run end-to-end auth tests for "$PRINCIPAL" user
-TEST_OPTIONS+=("--skip with_service_realm_and_host_options")
-cargo_test test::auth::gssapi_skip_local
-
-if [[ "cygwin" != "$OSTYPE" ]]; then
-  pushd driver
-  # Unauthenticate
-  echo "Unauthenticating $PRINCIPAL"
-  kdestroy
-
-  # Authenticate the alternative user principal in the KDC and run other e2e test
-  echo "Authenticating $PRINCIPAL_CROSS"
-  echo "$SASL_PASS_CROSS" | kinit -p $PRINCIPAL_CROSS
-  klist
-  popd
-fi
-
-TEST_OPTIONS=()
-cargo_test test::auth::gssapi_skip_local::with_service_realm_and_host_options
-
-if [[ "cygwin" != "$OSTYPE" ]]; then
-  pushd driver
-  # Unauthenticate
-  echo "Unauthenticating $PRINCIPAL_CROSS"
-  kdestroy
-  popd
-fi
-
-# Run remaining tests
+FEATURE_FLAGS+=("gssapi-auth")
+cargo_test gssapi
 cargo_test spec::auth
 cargo_test uri_options
 cargo_test connection_string
+
+if [ "$OSTYPE" != "cygwin" ]; then
+  rm "$KEYTAB_FILE"
+  kdestroy -A
+fi
 
 exit $CARGO_RESULT
